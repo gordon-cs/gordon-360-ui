7ad36a1a4019dacd490f2f6cad82422f20fad7c1
Adam Vigneaux <adamvigneaux@gmail.com>
2017-10-27 15:52:46 -0400

    Add HTTP, Auth, and Storage services

---
+++ src/services/http.js
@@ -0,0 +1,96 @@
+/**
+ * Handle HTTP requests to the API
+ *
+ * @module http
+ */
+
+import storage from './storage';
+
+import { checkToken } from './auth';
+
+const base = process.env.REACT_APP_API_URL;
+
+/**
+ * @global
+ * @typedef Auth
+ * @property {String} token Bearer token
+ * @property {Date} expires Date/time when token expires
+ */
+
+/**
+ * Make a headers object for use with the API
+ * @description Provides the correct authorization for API requests.
+ * @return {Headers}
+ */
+const makeHeaders = () => {
+  let token;
+
+  try {
+    const auth = storage.get('auth');
+    token = auth.token;
+  } catch (err) {
+    console.error('User is not authenticated.');
+  }
+
+  return new Headers({
+    Authorization: `Bearer ${token}`,
+  });
+};
+
+/**
+ * Make a request object with the correct URL and headers for the API
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @param {String} method HTTP method
+ * @param {object|array} body data to send with request
+ * @return {Request}
+ */
+const makeRequest = (url, method, body) => new Request(`${base}api/${url}`, {
+  method,
+  body,
+  headers: makeHeaders(),
+});
+
+/**
+ * Parse an HTTP response
+ * @param {Response} res HTTP response
+ * @return {object|array} Body of response
+ */
+const parseResponse = res => res.json()
+  .catch(() => console.error(`Got non-JSON response from request to ${res.url}`));
+
+/**
+ * Get
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @return {Promise.<Object>} Response body
+ */
+const get = url => checkToken().then(() => fetch(makeRequest(url, 'get'))).then(parseResponse);
+
+/**
+ * Put
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @param {object|array} body data to send with request
+ * @return {Promise.<Object>} Response body
+ */
+const put = (url, body) => checkToken().then(() => fetch(makeRequest(url, 'put', body)).then(parseResponse));
+
+/**
+ * Post
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @param {object|array} body data to send with request
+ * @return {Promise.<Object>} Response body
+ */
+const post = (url, body) => checkToken().then(() => fetch(makeRequest(url, 'post', body)).then(parseResponse));
+
+/**
+ * Delete
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @return {Promise.<Object>} Response body
+ */
+const del = url => checkToken().then(() => fetch(makeRequest(url, 'delete')).then(parseResponse));
+
+export default {
+  del,
+  get,
+  post,
+  put,
+};
