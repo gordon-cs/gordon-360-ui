3870f637b78ed454fcae80a314ea17934914e09c
mFelgate <m.felgate55@gmail.com>
2017-10-29 17:43:06 -0400

    Created two nav bars based on authentication status

--- src/components/Header/components/Nav/index.js
+++ src/components/Header/components/Nav/index.js
@@ -5,6 +5,10 @@ import {
   Navbar,
   NavbarToggler,
   NavItem,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Button,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -13,7 +17,6 @@ import './nav.css';
 export default class GordonNav extends Component {
   constructor(props) {
     super(props);
-
     this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: false,
@@ -24,7 +27,7 @@ export default class GordonNav extends Component {
       isOpen: !this.state.isOpen,
     });
   }
-  render() {
+  authenticatedNav() {
     return (
       <Navbar className="gordon-nav" color="faded" light expand="sm">
         <NavbarToggler onClick={this.toggle} />
@@ -45,6 +48,50 @@ export default class GordonNav extends Component {
           </Nav>
         </Collapse>
       </Navbar>
+    );
+  }
+
+  loginNav() {
+    return (
+      <Navbar className="gordon-nav" color="faded" light expand="sm">
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <InputGroup>
+                <InputGroupAddon>@gordon.edu</InputGroupAddon>
+                <Input placeholder="username" />
+              </InputGroup>
+            </NavItem>
+            <NavItem>
+              <InputGroup>
+                <InputGroupAddon>@gordon.edu</InputGroupAddon>
+                <Input placeholder="Password" />
+              </InputGroup>
+            </NavItem>
+            <NavItem>
+              <Button color="primary">Login</Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+
+  rendernav(authenticated) {
+    let nav;
+    if (authenticated) {
+      nav = this.authenticatedNav();
+    } else {
+      nav = this.loginNav();
+    }
+    return (nav);
+  }
+
+  render() {
+    const authenticated = true;
+    return (
+      this.rendernav(authenticated)
     );
   }
 }
