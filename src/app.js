1f900cfcbe2c071d045fb8ceec5f61d94666b6c0
Adam Vigneaux <adamvigneaux@gmail.com>
2017-11-03 12:18:58 -0400

    Add Material-UI theme

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
@@ -5,23 +7,25 @@ import {
 } from 'react-router-dom';
 
 import './app.css';
-
 import ActivityEdit from './views/ActivityEdit';
 import GordonHeader from './components/Header';
 import Home from './views/Home';
+import theme from './theme';
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <section className="app-wrapper">
-          <GordonHeader />
-          <main className="app-main">
-            <Route path="/" component={Home} />
-            <Route path="/activity/:activityId/edit" component={ActivityEdit} />
-          </main>
-        </section>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <section className="app-wrapper">
+            <GordonHeader />
+            <main className="app-main">
+              <Route path="/" component={Home} />
+              <Route path="/activity/:activityId/edit" component={ActivityEdit} />
+            </main>
+          </section>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
