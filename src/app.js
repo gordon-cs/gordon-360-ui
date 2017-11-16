fd4fd708de8c2c668870feb99fc7d79bea9ef7fc
Adam Vigneaux <adamvigneaux@gmail.com>
2017-11-15 21:57:21 -0500

    Add login page
    Whenever the user navigates to a view and their token is missing from `localStorage`, they will see the login view instead. When they log in, they will then see whichever view they were trying to see originally.
    
    The login page displays loading progress and errors.

--- src/app.js
+++ src/app.js
@@ -9,9 +9,11 @@ import {
 import './app.css';
 import ActivitiesAll from './views/ActivitiesAll';
 import ActivityEdit from './views/ActivityEdit';
+import { isAuthenticated } from './services/auth';
 import GordonHeader from './components/Header';
 import GordonNav from './components/Nav';
 import Home from './views/Home';
+import Login from './views/Login';
 import theme from './theme';
 
 export default class App extends Component {
@@ -19,6 +21,7 @@ export default class App extends Component {
     super(props);
 
     this.onDrawerToggle = this.onDrawerToggle.bind(this);
+    this.onLogIn = this.onLogIn.bind(this);
 
     this.state = {
       drawerOpen: false,
@@ -27,19 +30,31 @@ export default class App extends Component {
   onDrawerToggle() {
     this.setState({ drawerOpen: !this.state.drawerOpen });
   }
+  onLogIn() {
+    // Force this component to re-render, login view -> main app view
+    this.forceUpdate();
+  }
   render() {
+    let content = (
+      <section className="app-wrapper">
+        <GordonHeader onDrawerToggle={this.onDrawerToggle} />
+        <GordonNav onDrawerToggle={this.onDrawerToggle} drawerOpen={this.state.drawerOpen} />
+        <main className="app-main">
+          <Route exact path="/" component={Home} />
+          <Route path="/activities" component={ActivitiesAll} />
+          <Route path="/activity/:activityId/edit" component={ActivityEdit} />
+        </main>
+      </section>
+    );
+
+    if (!isAuthenticated()) {
+      content = <Login onLogIn={this.onLogIn} />;
+    }
+
     return (
       <MuiThemeProvider theme={theme}>
         <Router>
-          <section className="app-wrapper">
-            <GordonHeader onDrawerToggle={this.onDrawerToggle} />
-            <GordonNav onDrawerToggle={this.onDrawerToggle} drawerOpen={this.state.drawerOpen} />
-            <main className="app-main">
-              <Route exact path="/" component={Home} />
-              <Route path="/activities" component={ActivitiesAll} />
-              <Route path="/activity/:activityId/edit" component={ActivityEdit} />
-            </main>
-          </section>
+          { content }
         </Router>
       </MuiThemeProvider>
     );
