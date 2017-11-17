be4a3fd6386caaa1bfbd24a44014b0cd4891ccf5
Adam Vigneaux <adamvigneaux@gmail.com>
2017-11-17 11:06:30 -0500

    Add method to get current term code
    The term code has the same purpose as the session code and is therefore redundant, but the API expects it for the time being.

--- src/services/session.js
+++ src/services/session.js
@@ -27,7 +27,31 @@ const getAll = () => http.get('sessions').then(sessions => sessions.reverse());
  */
 const getCurrent = () => http.get('sessions/current');
 
+/**
+ * Get current term code
+ * @return {Promise.<String>}
+ */
+const getTermCode = () => {
+  const now = new Date();
+  const terms = {
+    spring: 'SP',
+    fall: 'FA',
+  };
+
+  // Decide what term it is, defaulting to fall
+  let term = terms.fall;
+  let year = now.getFullYear();
+  if (now.getMonth() <= 6) {
+    term = terms.spring;
+    // If spring term, decrement current year to get current academic year
+    year -= 1;
+  }
+
+  return `${year.toString().substr(-2)}${term}`;
+};
+
 export default {
   getAll,
   getCurrent,
+  getTermCode,
 };
