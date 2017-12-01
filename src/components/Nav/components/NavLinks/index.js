e96538175d7b90a005079574170a262ff4c79d08
Adam Vigneaux <adamvigneaux@gmail.com>
2017-12-01 09:57:40 -0500

    Add selected state to navigation drawer links
    Now, when the route matching a navigation link is active, that navigation link will be highlighted.

--- src/components/Nav/components/NavLinks/index.js
+++ src/components/Nav/components/NavLinks/index.js
@@ -7,37 +7,39 @@ import HomeIcon from 'material-ui-icons/Home';
 import LocalActivityIcon from 'material-ui-icons/LocalActivity';
 import EventIcon from 'material-ui-icons/Event';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import './nav-links.css';
 
 export default class GordonNavLinks extends Component {
   render() {
     return (
       <List className="gordon-nav-links">
-        <Link to="/" onClick={this.props.onLinkClick}>
+        <NavLink exact to="/" onClick={this.props.onLinkClick}>
           <ListItem button>
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
             <ListItemText primary="Home" />
           </ListItem>
-        </Link>
-        <Link to="/activities" onClick={this.props.onLinkClick}>
+        </NavLink>
+        <NavLink exact to="/activities" onClick={this.props.onLinkClick}>
           <ListItem button>
             <ListItemIcon>
               <LocalActivityIcon />
             </ListItemIcon>
             <ListItemText primary="Activities" />
           </ListItem>
-        </Link>
-        <Link to="/events" onClick={this.props.onLinkClick}>
+        </NavLink>
+        <NavLink exact to="/events" onClick={this.props.onLinkClick}>
           <ListItem button>
             <ListItemIcon>
               <EventIcon />
             </ListItemIcon>
             <ListItemText primary="Events" />
           </ListItem>
-        </Link>
+        </NavLink>
       </List>
     );
   }
