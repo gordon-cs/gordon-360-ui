e458f20cb5fbe0ed459bb3b61858435de4b63b31
Adam Vigneaux <adamvigneaux@gmail.com>
2017-12-02 22:31:21 -0500

    Add user profile preview to nav drawer

--- src/components/Nav/components/NavAvatar/index.js
+++ src/components/Nav/components/NavAvatar/index.js
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import './nav-avatar.css';
+import user from '../../../../services/user';
 
 const styles = theme => ({
   drawerHeader: theme.mixins.toolbar,
@@ -19,36 +20,64 @@ class GordonNavAvatar extends Component {
     this.getInitials = this.getInitials.bind(this);
 
     this.state = {
-      user: {
-        name: 'Test User',
-        email: 'test.user@gordon.edu',
-        username: 'test.user',
-      },
+      email: null,
+      image: null,
+      name: null,
+      username: null,
     };
   }
+  async componentWillMount() {
+    const { name, user_name: username } = user.getLocalInfo();
+    this.setState({ name, username });
+    const [
+      { Email: email },
+      { def: defaultImage, pref: preferredImage },
+    ] = await Promise.all([
+      await user.getProfileInfo(),
+      await user.getImage(),
+    ]);
+
+    const image = preferredImage || defaultImage;
+
+    this.setState({ email, image });
+  }
   getInitials() {
-    return this.state.user.name
-      .split(' ') // Split name into separate words
-      .map(name => name[0]) // Get first letter of each part of name
-      .join(''); // Join initials back into a string
+    if (this.state.username) {
+      return this.state.username
+        .split('.') // Split name into separate words
+        .map(name => name[0]) // Get first letter of each part of name
+        .join(''); // Join initials back into a string
+    }
+    return '';
   }
   render() {
     const { classes } = this.props;
 
+    let avatar = <Avatar className="avatar placeholder">{this.getInitials()}</Avatar>;
+    if (this.state.image) {
+      avatar = <Avatar className="avatar image" src={`data:image/jpg;base64,${this.state.image}`} />;
+    }
+
+    // Link component to be used with Button component
+    const buttonLink = ({ ...props }) =>
+      <Link {...props} to={`/profile/${this.state.username}`} onClick={this.props.onLinkClick} />;
+
     return (
       <Button
         className={`${classes.drawerHeader} gordon-nav-avatar`}
-        classes={{ root: 'gordon-nav-avatar button' }}
+        classes={{
+          root: 'gordon-nav-avatar button',
+          label: 'label',
+        }}
+        component={buttonLink}
       >
-        <Link to={`/profile/${this.state.user.username}`} onClick={this.props.onLinkClick}>
-          <Avatar className="image">{this.getInitials()}</Avatar>
-          <Typography type="body2" className="text" align="left" gutterBottom>
-            {this.state.user.name}
-          </Typography>
-          <Typography type="body1" className="text" align="left" gutterBottom>
-            {this.state.user.email}
-          </Typography>
-        </Link>
+        {avatar}
+        <Typography type="body2" className="text" align="left" gutterBottom>
+          {this.state.name}
+        </Typography>
+        <Typography type="caption" className="text" align="left" gutterBottom>
+          {this.state.email}
+        </Typography>
       </Button>
     );
   }
