9cf0fdab280fdb84f8d7ba4a4c03a839ac6290c1
mFelgate <m.felgate55@gmail.com>
2017-12-03 19:53:47 -0500

    Add user stats to home view (#12)
    * making the graphs into componenets
    
    * forgot to add my changes in the pull request
    
    * Add React ChartJS dependency
    
    * Add development proxy to get around CORS issues
    
    This proxy enables the frontend to make requests to the API server hosted on the `gordon.edu` domain when running on `localhost`, which would normally be disallowed by CORS.
    
    * Add method to get current term code
    
    The term code has the same purpose as the session code and is therefore redundant, but the API expects it for the time being.
    
    * Fix getting attended events and chapel credits
    
    * finished graphed componenets
    
    * finished chart componenets
    
    * remove the event service
    
    * merging with master
    
    * delete uneeded code and improve the bad practices
    
    * changed the import of ChapelProgress to match the changed file name
    
    * Fix JSDoc return type for getTermCode
    
    * fix varible names and put the loader inside the card
    
    * Fix capitalization of ChapelProgress folder name
    
    * added typography and JSDocs
    
    * add typography and JSDocs
    
    * fix JSDocs
    
    * merged with master
    
    * fixed merge conflicts
    
    * fix grammer in strings and variable names in JSDocs
    
    * fix grammer in strings and variable names in JSDocs

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,40 +1,17 @@
 import Grid from 'material-ui/Grid';
 import React, { Component } from 'react';
-
-import chapelEventsForUser from './events-chapel-user-session.json';
-import daysLeft from './sessions-days-left.json';
+import DaysLeft from './components/DaysLeft';
+import ChapelProgress from './components/ChapelProgress';
 
 export default class Home extends Component {
   render() {
     return (
       <Grid container justify="center">
-        <Grid item xs={12}>
-          <figure>
-            <figcaption>
-              <h3>Days Left in Semester</h3>
-              {daysLeft[0]} of {daysLeft[1]}
-            </figcaption>
-            <progress
-              className="progress-indicator"
-              color="primary"
-              value={daysLeft[0]}
-              max={daysLeft[1]}
-            />
-          </figure>
+        <Grid item xs={12} md={3}>
+          <DaysLeft />
         </Grid>
-        <Grid item xs={12}>
-          <figure>
-            <figcaption>
-              <h3>Christian Life and Worship Credits</h3>
-              {chapelEventsForUser.length} of {chapelEventsForUser[0].Required}
-            </figcaption>
-            <progress
-              className="progress-indicator"
-              color="primary"
-              value={chapelEventsForUser.length}
-              max={chapelEventsForUser[0].Required || chapelEventsForUser.length}
-            />
-          </figure>
+        <Grid item xs={12} md={3}>
+          <ChapelProgress />
         </Grid>
       </Grid>
     );
