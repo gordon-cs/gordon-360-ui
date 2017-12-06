40fbad42c62f563b03efd0f2ee8cdefa2b05e863
mFelgate <m.felgate55@gmail.com>
2017-12-06 16:45:24 -0500

    add discription, dropdowns, and multiple occurrances

--- src/views/Events/components/EventItem/index.js
+++ src/views/Events/components/EventItem/index.js
@@ -1,37 +1,96 @@
-import { ListItem, ListItemText } from 'material-ui/List';
-import Divider from 'material-ui/Divider';
+
+import Card, { CardActions, CardContent, CardHeader } from 'material-ui/Card';
+import List, { ListItem, ListSubheader, ListItemText } from 'material-ui/List';
 import PropTypes from 'prop-types';
+import Typography from 'material-ui/Typography';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
+import Chip from 'material-ui/Chip';
+import Button from 'material-ui/Button';
+import Divider from 'material-ui/Divider';
+import Collapse from 'material-ui/transitions/Collapse';
 import React, { Component } from 'react';
 import { DateTime } from 'luxon';
 
+import './event-item.css';
 
 export default class GordonEventItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleExpandClick = this.handleExpandClick.bind(this);
+
+    this.state = { open: false };
+  }
+  handleExpandClick() {
+    this.setState({ open: !this.state.open });
+  }
   render() {
     const { event } = this.props;
 
-    let time;
-    if (event.Occurrences[0] && event.Occurrences[0][0]) {
-      time = DateTime.fromISO(event.Occurrences[0][0]).toFormat('LLL dd yy');
+    let beginTime;
+    let endTime;
+    if (event.Occurrences[0] && event.Occurrences[0][0] && event.Occurrences[0][1]) {
+      beginTime = DateTime.fromISO(event.Occurrences[0][0]).toFormat('t');
+      endTime = DateTime.fromISO(event.Occurrences[0][1]).toFormat('t');
     }
+    const timeRange = ` from ${beginTime} to ${endTime}`;
+
+    let date;
+    if (event.Occurrences[0] && event.Occurrences[0][0]) {
+      date = DateTime.fromISO(event.Occurrences[0][0]).toFormat('LLL dd yy');
+    }
+
     let title;
     if (event.Event_Title === '') {
       title = event.Event_Name;
     } else {
       title = event.Event_Title;
     }
-    let subInfo;
+
+    let location;
     if (event.Occurrences[0] && event.Occurrences[0][2]) {
-      subInfo = `${time}, ${event.Occurrences[0][2]}`;
-    } else {
-      subInfo = `${time}`;
+      location = `${event.Occurrences[0][2]} `;
+    }
+
+    let discription = 'No description available';
+    if (event.Description) {
+      if (event.Description === '' || event.Description.substring(0, 4) === '<res') {
+        event.Description = 'No description available';
+      }
+      event.Description = event.Description.replace(/&(#[0-9]+|[a-zA-Z]+);/g, ' ').replace(/<\/?[^>]+(>|$)/g, ' ');
+
+      discription = `${event.Description} `;
+    }
+    const subheader = `${date}, ${timeRange} | ${location}`;
+
+    let content;
+    if (event.Occurrences.length > 1) {
+      content = event.Occurrences
+        .map(locations => (
+          <ListItem >
+            <ListItemText primary={DateTime.fromISO(locations[0]).toFormat('ff')} secondary={locations[2]} />
+          </ListItem>
+        ));
     }
     return (
-      <div>
-        <ListItem>
-          <ListItemText primary={title} secondary={subInfo} />
-        </ListItem>
-        <Divider />
-      </div>
+      <Card >
+        <CardHeader
+          action={
+            <Button dense color="primary" onClick={this.handleExpandClick}>
+              <ExpandMoreIcon />
+            </Button>}
+          title={title}
+          subheader={subheader}
+        />
+        <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+          <List>
+            <ListItem >
+              <ListItemText primary={event.Organization} secondary={discription} />
+            </ListItem>
+            {content}
+          </List>
+        </Collapse>
+      </Card>
     );
   }
 }
