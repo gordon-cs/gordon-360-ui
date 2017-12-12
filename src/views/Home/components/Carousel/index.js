d76a137d7965abd69d8400e11737d09079763e25
ajabbot <ajabbot@gmail.com>
2017-12-12 15:03:36 -0500

    Add initial carousel to home view

---
+++ src/views/Home/components/Carousel/index.js
@@ -0,0 +1,62 @@
+import Card, { CardContent, CardHeader } from 'material-ui/Card';
+import React, { Component } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+import cms from '../../../../services/cms';
+import '../../../../../node_modules/react-responsive-carousel/lib/styles/carousel.css';
+import GordonLoader from '../../../../components/Loader';
+import './carousel.scss';
+
+export default class GordonCarousel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.loadCarousel = this.loadCarousel.bind(this);
+
+    this.state = {
+      error: null,
+      loading: true,
+      carouselContent: {},
+    };
+  }
+  componentWillMount() {
+    this.loadCarousel();
+  }
+  async loadCarousel() {
+    this.setState({ loading: true });
+    try {
+      const carouselContent = await cms.getSlides();
+      this.setState({ loading: false, carouselContent });
+    } catch (error) {
+      this.setState({ error });
+    }
+  }
+  render() {
+    if (this.state.error) {
+      throw this.state.error;
+    }
+
+    let content;
+    let subheader;
+    if (this.state.loading === true) {
+      content = <GordonLoader />;
+    } else {
+      content = (
+        <Carousel showThumbs={false} autoPlay={true} infiniteLoop transitionTime={1500}>
+          {this.state.carouselContent.map(slide => (
+            <div>
+              <img src={slide.ImagePath} alt={slide.AltTag} />
+            </div>
+          ))}
+        </Carousel>
+      );
+    }
+    return (
+      <Card>
+        <CardContent>
+          {content}
+        </CardContent >
+      </Card>
+    );
+  }
+}
