1ef7d9ebbdf48a9139abb4d6c72008b5e3f3fa29
Nathaniel Rudenberg <Nathaniel.Rudenberg@gordon.edu>
2018-06-13 15:46:22 -0400

    Extend student name + pic across screen / some formatting changes

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -117,10 +117,14 @@ export default class Profile extends Component {
     return (
       <div>
         <Grid container>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
             <Card>
               <CardContent>
                 <Grid container justify="center">
+                  <Grid item xs={6} sm={6} md={6} lg={4}>
+                    <img src={`data:image/jpg;base64,${this.state.image}`} alt="" style={style} />
+                  </Grid>
+
                   <Grid item xs={6} sm={6} md={6} lg={4}>
                     <CardHeader
                       title={this.state.profile.fullName}
@@ -160,23 +164,25 @@ export default class Profile extends Component {
                       </DialogActions>
                     </Dialog>
                   </Grid>
-                  <Grid item xs={6} sm={6} md={6} lg={4}>
-                    <img src={`data:image/jpg;base64,${this.state.image}`} alt="" style={style} />
-                  </Grid>
                 </Grid>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
+
+          {/* <Grid item xs={12} sm={12} md={6} lg={6}>
             <Card>
               <CardContent>
                 <CardHeader title="Home Address" />
+
                 <List>
                   <Divider />
+
                   <ListItem>
                     <Typography>Street Number: {this.state.profile.HomeStreet2}</Typography>
                   </ListItem>
+
                   <Divider />
+
                   <ListItem>
                     <Typography>
                       Home Town: {this.state.profile.HomeCity}, {this.state.profile.HomeState}
@@ -185,43 +191,76 @@ export default class Profile extends Component {
                 </List>
               </CardContent>
             </Card>
-          </Grid>
+          </Grid> */}
+
           <Grid item xs={12} sm={12} md={6} lg={6}>
             <Card>
               <CardContent>
                 <CardHeader title="Personal Information" />
+
                 <List>
                   <ListItem>
                     <Typography>Major: {this.state.profile.Major1Description}</Typography>
                   </ListItem>
+
                   <Divider />
+
                   <ListItem>
                     <Grid item xs={6} sm={7} md={8} lg={10}>
                       <Typography>Cell Phone: {this.state.profile.MobilePhone}</Typography>
                     </Grid>
+
                     <Grid item xs={6} sm={5} md={4} lg={1}>
                       <Button onClick={this.handleExpandClick} raised style={button}>
                         {this.state.button}
                       </Button>
                     </Grid>
                   </ListItem>
+
                   <Divider />
+
                   <ListItem>
                     <Typography>Student ID: {this.state.profile.ID}</Typography>
                   </ListItem>
+
                   <Divider />
+
                   <ListItem>
                     <Typography>Email: {this.state.profile.Email}</Typography>
                   </ListItem>
+
                   <Divider />
+
                   <ListItem>
                     <Typography>On/Off Campus: {this.state.profile.OnOffCampus}</Typography>
                   </ListItem>
+
+                  <Divider />
+                </List>
+
+                <CardHeader title="Home Address" />
+
+                <List>
+                  <Divider />
+
+                  <ListItem>
+                    <Typography>Street Number: {this.state.profile.HomeStreet2}</Typography>
+                  </ListItem>
+
+                  <Divider />
+
+                  <ListItem>
+                    <Typography>
+                      Home Town: {this.state.profile.HomeCity}, {this.state.profile.HomeState}
+                    </Typography>
+                  </ListItem>
+
                   <Divider />
                 </List>
               </CardContent>
             </Card>
           </Grid>
+
           <Grid item xs={12} sm={12} md={6} lg={6}>
             <Card>
               <CardContent>
