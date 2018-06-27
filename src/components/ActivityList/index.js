a80680b7d27ff4df5d86d919de864f8893ac944f
softwary <dominiqueadevai@gmail.com>
2018-06-27 14:05:34 -0400

    Update ActivityList UI
    	- Privacy toggle not yet functional

--- src/components/ActivityList/index.js
+++ src/components/ActivityList/index.js
@@ -4,6 +4,8 @@ import Divider from '@material-ui/core/Divider';
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
+import Switch from '@material-ui/core/Switch';
+import membership from './../../services/membership';
 
 export default class Activities extends Component {
   constructor(props) {
@@ -15,31 +17,51 @@ export default class Activities extends Component {
     this.loadProfile();
   }
   async loadProfile() {}
+
+  handleChangeMembershipPrivacy(userMembership) {
+    userMembership.toggleMembershipPrivacy;
+  }
+
   render() {
     const { Activity } = this.props;
     const imgStyle = {
       width: '90%',
     };
-    const textStyle = {
-      textIndent: '12pt',
+    const toggleTextStyle = {
+      fontSize: '10pt',
     };
 
     return (
       <div>
-        <Link to={`/activity/${Activity.SessionCode}/${Activity.ActivityCode}`}>
-          <Grid container alignItems="center">
-            <Grid item xs={10} sm={10} md={10} lg={10} style={textStyle}>
+        <Grid container alignItems="center">
+          <Grid item xs={8} sm={8} md={8} lg={8}>
+            <Link to={`/activity/${Activity.SessionCode}/${Activity.ActivityCode}`}>
               <Typography>
                 <b>{Activity.ActivityDescription}</b>
               </Typography>
               <Typography>{Activity.SessionDescription}</Typography>
               <Typography>{Activity.ParticipationDescription}</Typography>
-            </Grid>
-            <Grid item xs={2} sm={2} md={2} lg={2}>
-              <img src={Activity.ActivityImagePath} alt="" style={imgStyle} />
+            </Link>
+          </Grid>
+          <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+            <Grid container>
+              <Grid item xs={12} align="center">
+                <Switch
+                  onClick={this.handleChangeMembershipPrivacy(Activity)}
+                  checked={Activity.Privacy}
+                />
+              </Grid>
+              <Grid item xs={12} align="center">
+                <Typography style={toggleTextStyle}>Privacy</Typography>
+              </Grid>
             </Grid>
           </Grid>
-        </Link>
+          <Grid item xs={2} sm={2} md={2} lg={2}>
+            <Link to={`/activity/${Activity.SessionCode}/${Activity.ActivityCode}`}>
+              <img src={Activity.ActivityImagePath} alt="" style={imgStyle} />
+            </Link>
+          </Grid>
+        </Grid>
         <Divider />
       </div>
     );
