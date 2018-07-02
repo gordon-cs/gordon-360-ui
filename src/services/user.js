47beee34acda309c9c80e74c61ce3cf1742d641d
softwary <dominiqueadevai@gmail.com>
2018-07-02 14:43:35 -0400

    Alphabetize the public profile involvements

--- src/services/user.js
+++ src/services/user.js
@@ -409,6 +409,7 @@ const getMemberships = async id => {
 const getPublicMemberships = async username => {
   let memberships;
   memberships = await http.get(`/memberships/student/username/${username}/`);
+  memberships.sort(compareByTitle);
   return memberships;
 };
 
