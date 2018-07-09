d425fcf66fd14ca240a96f636ea59a4ac678732b
Emily Bishop <Emily.Bishop@gordon.edu>
2018-07-09 17:53:10 -0400

    Took out search bar on attended events page

--- src/views/EventsAttended/index.js
+++ src/views/EventsAttended/index.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import user from './../../services/user';
-import gordonEvent from './../../services/event';
 import GordonLoader from '../../components/Loader';
 import EventList from './../../components/EventList';
-
-const styles = {
-  searchBar: {
-    margin: '0 auto',
-  },
-};
 
 export default class EventsAttended extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: '',
       events: [],
-      filteredEvents: [],
       loading: true,
     };
   }
@@ -28,20 +18,10 @@ export default class EventsAttended extends Component {
     this.loadEvents();
   }
 
-  search(name) {
-    return async event => {
-      await this.setState({
-        [name]: event.target.value,
-      });
-      const events = await gordonEvent.getFilteredEvents(this.state);
-      this.setState({ filteredEvents: events, loading: false });
-    };
-  }
-
   async loadEvents() {
     this.setState({ loading: true });
     const events = await user.getAttendedChapelEventsFormatted();
-    this.setState({ events, loading: false, filteredEvents: events });
+    this.setState({ events, loading: false });
   }
   render() {
     let content;
@@ -59,20 +39,6 @@ export default class EventsAttended extends Component {
     return (
       <section>
         <Grid container justify="center">
-          <Grid item xs={12} md={12} lg={8}>
-            <Grid container alignItems="baseline" style={styles.searchBar}>
-              <Grid item xs={8} sm={9} md={10} lg={10}>
-                <TextField
-                  id="search"
-                  label="Search"
-                  value={this.state.search}
-                  onChange={this.search('search')}
-                  margin="none"
-                  fullWidth
-                />
-              </Grid>
-            </Grid>
-          </Grid>
           <Grid item xs={12} md={12} lg={8}>
             <List>{content}</List>
           </Grid>
