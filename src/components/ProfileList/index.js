fcf94a742399420b1f65b53a7ec57d923253a5bb
Max Moniz <max.moniz@gordon.edu>
2018-07-11 15:28:09 -0400

    Update index.js

--- src/components/ProfileList/index.js
+++ src/components/ProfileList/index.js
@@ -13,7 +13,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import LockIcon from '@material-ui/icons/Lock';
-import './profilelist.css';
+import './profileList.css';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 
