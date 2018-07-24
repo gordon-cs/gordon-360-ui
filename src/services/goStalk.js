8a7e1196494398cf10c6e975c6eddc698a022307
softwary <dominiqueadevai@gmail.com>
2018-07-24 16:34:01 -0400

    Fix GoStalk case sensitivity

--- src/services/goStalk.js
+++ src/services/goStalk.js
@@ -18,18 +18,26 @@ const search = (firstName, lastName, homeCity, zipCode) => {
   if (firstName === '' || firstName === null) {
     // eslint-disable-next-line
     firstName = 'C' + '\u266F';
+  } else {
+    firstName = firstName.toLowerCase();
   }
   if (lastName === '' || lastName === null) {
     // eslint-disable-next-line
     lastName = 'C' + '\u266F';
+  } else {
+    lastName = lastName.toLowerCase();
   }
   if (homeCity === '' || homeCity === null) {
     // eslint-disable-next-line
     homeCity = 'C' + '\u266F';
+  } else {
+    homeCity = homeCity.toLowerCase();
   }
   if (zipCode === '' || zipCode === null) {
     // eslint-disable-next-line
     zipCode = 'C' + '\u266F';
+  } else {
+    zipCode = zipCode.toLowerCase();
   }
   return http.get(
     `accounts/advanced-people-search/${firstName}/${lastName}/${homeCity}/${zipCode}`,
