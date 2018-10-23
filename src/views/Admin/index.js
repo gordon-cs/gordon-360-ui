cf684aa0a5bfeb3085c0898658aac7aa3952ddc1
Addison <ajabbot@gmail.com>
2018-10-23 08:28:33 -0400

    Started adding UI to admin page

--- src/views/Admin/index.js
+++ src/views/Admin/index.js
@@ -1,7 +1,48 @@
-import { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+import { gordonColors } from '../../theme';
 
 export default class Admin extends Component {
   render() {
-    return 'Admin Page';
+    const headerStyle = {
+      backgroundColor: gordonColors.primary.blue,
+      color: '#FFF',
+      padding: '10px',
+    };
+
+    return (
+      <Grid container align="center" spacing={16}>
+        <Grid item xs={12}>
+          <Card>
+            <div style={headerStyle}>
+              <Typography variant="body2" style={headerStyle}>
+                Open Involvements
+              </Typography>
+            </div>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <div style={headerStyle}>
+              <Typography variant="body2" style={headerStyle}>
+                Closed Involvements
+              </Typography>
+            </div>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <div style={headerStyle}>
+              <Typography variant="body2" style={headerStyle}>
+                Super Admins
+              </Typography>
+            </div>
+          </Card>
+        </Grid>
+      </Grid>
+    );
   }
 }
