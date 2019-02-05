978eb0a134e1f8003334077012422ff563cc9bcb
Addison <ajabbot@gmail.com>
2019-02-05 12:58:25 -0500

    Fixed link to Involvements in Open/Closed Involvement list

--- src/views/Admin/components/InvolvementStatusList/index.js
+++ src/views/Admin/components/InvolvementStatusList/index.js
@@ -3,32 +3,46 @@ import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import session from '../../../../services/session';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import activity from '../../../../services/activity';
 import Button from '@material-ui/core/Button';
 
 export default class InvolvementStatusList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { currentSession: '' };
+  }
+
+  async componentWillMount() {
+    const { SessionCode: sessionCode } = await session.getCurrent();
+    this.setState({ currentSession: sessionCode });
+  }
+
   render() {
     const { Activity } = this.props;
-    console.log('Activity prop: ', Activity);
     const imgStyle = {
       width: '20%',
     };
-    // Link created should look like: /activity/201809/COMPS
-    // We are not getting the Activity.sessionCode, just general activities, without a time period
+
     return (
       <div>
         <Grid container>
           <Grid item xs={12} md={10} lg={12}>
             <Grid container alignItems="center">
               <Grid item xs={4}>
-                <Link to={`/activity/${Activity.sessionCode}/${Activity.ActivityCode}`}>
+                <Link
+                  to={`/activity/${this.state.currentSession}/${this.props.Activity.ActivityCode}`}
+                >
                   <img src={Activity.ActivityImagePath} alt="" style={imgStyle} />
                 </Link>
               </Grid>
               <Grid item xs={8}>
-                <Link to={`/activity/${Activity.sessionCode}/${Activity.activityCode}`}>
+                <Link
+                  to={`/activity/${this.state.currentSession}/${this.props.Activity.ActivityCode}`}
+                >
                   <Typography>
                     <b>{Activity.ActivityDescription}</b>
                   </Typography>
