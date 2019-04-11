a3419ae6633e0bff81de91a622a5f7383b02fe35
Dominique Adevai <dominiqueadevai@gmail.com>
2019-04-11 12:55:18 -0400

    Add postIDImage

--- src/services/user.js
+++ src/services/user.js
@@ -315,6 +315,20 @@ const resetImage = () => {
 };
 
 /**
+ * upload an ID image for a user to the __ folder
+ * @param {String} dataURI of the image being uploaded
+ * @return {Response} response of http request
+ */
+const postIDImage = dataURI => {
+  let imageData = new FormData();
+  let blob = dataURItoBlob(dataURI);
+  let type = blob.type.replace('image/', '');
+  let headerOptions = {};
+  imageData.append('canvasImage', blob, 'canvasImage.' + type);
+  return http.post('profiles/IDimage', imageData, headerOptions);
+};
+
+/**
  * upload a photo to user's profile, which is then used as the preferred photo
  * @param {String} dataURI of the image being uploaded
  * @return {Response} response of http request
