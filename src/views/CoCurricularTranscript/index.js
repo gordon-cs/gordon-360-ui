725e41ffd38aa09e530bc1b1bd38c192c63d1794
Joshua Rogers <joshua.rogers@gordon.edu>
2019-06-12 11:09:44 -0400

    Cleaned up index.js and .scss, switched to Grid layout from flexbox

--- src/views/CoCurricularTranscript/index.js
+++ src/views/CoCurricularTranscript/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 import { gordonColors } from '../../theme';
@@ -12,8 +11,8 @@ import user from './../../services/user';
 import GordonLoader from './../../components/Loader';
 import './coCurricularTranscript.css';
 
-//This component creates the overall interface for the CoCurricularTranscript (card, heading, download button),
-//and contains a InvolvementsList object for displaying the content
+//This component creates the overall interface for the CoCurricularTranscript (card, heading,
+//download button), and contains a InvolvementsList object for displaying the content
 
 export default class Transcript extends Component {
   constructor(props) {
@@ -23,13 +22,14 @@ export default class Transcript extends Component {
       loading: true,
       profile: {},
     };
-    //Variable used by groupActivityBySession to determine when activies in the list are part of a new session
-    //Necessary in order to remember session info about previous item in list while looping though
+    //Variable used by groupActivityBySession to determine when activies in the list are part of a
+    //new session necessary in order to remember session info about previous item in list while
+    //looping though
     this.prevSessionVal = '';
   }
 
   handleDownload() {
-    window.print();
+    window.print(); //look for somethig that sends straight to download
   }
 
   componentWillMount() {
@@ -54,7 +54,7 @@ export default class Transcript extends Component {
   //isUnique value is passed as a prop, along with Activity object, to TranscriptActivity Component
   //Returns TranscriptActivity component which should be passed into activityList
   groupActivityBySession = activity => {
-    //bool to keep track of when an zctivity is part of a new session, passed to TranscriptActivity
+    //bool to keep track of when an activity is part of a new session, passed to TranscriptActivity
     let isUniqueSession = true;
 
     if (activity.SessionDescription === this.prevSessionVal) {
@@ -67,6 +67,7 @@ export default class Transcript extends Component {
   };
 
   render() {
+    /* bloated? */
     let activityList;
 
     if (!this.state.activities) {
@@ -75,51 +76,39 @@ export default class Transcript extends Component {
       activityList = this.state.activities.map(this.groupActivityBySession);
     }
 
-    const button = {
+    const buttonColors = {
+      /* not in style sheet so that gordonColors is accessible */
       background: gordonColors.primary.cyan,
       color: 'white',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fullWidth: true,
     };
 
     return (
-      <Grid container className="co-curricular-transcript" alignItems="center" justify="center" spacing="16">
-        <Grid xs={12} sm={12} md={8} lg={6}>
-          <Card elevation="10">
-            <CardContent>
-              <Grid item xs={12} className="print-only">
-                {/* <img src={require('./logo.png')} alt="" /> */}
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  variant="contained"
-                  raised
-                  style={button}
-                  justify="center"
-                  onClick={this.handleDownload}
-                >
-                  Print Co-Curricular Transcript
-                </Button>
-              </Grid>
-              <Grid item xs={12} margin="normal" className="heading">
-                <div>
-                  <Typography variant="headline">
-                    <b> Co-Curricular Transcript - {this.state.profile.fullName} </b>
-                  </Typography>
-                </div>
-              </Grid>
-              <Grid item xs={12} class="print">
-                <Grid container spacing={0}>
-                  <Grid item xs={12}>
-                    {activityList}
-                  </Grid>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+      <div className="co-curricular-transcript">
+        <Card className="transcript-item" elevation="10">
+          <CardContent className="card-content">
+            <div className="print-only">{/* <img src={require('./logo.png')} alt="" /> */}</div>
+            <div>
+              <Button
+                className="button"
+                onClick={this.handleDownload}
+                raised
+                style={buttonColors}
+                variant="contained"
+              >
+                Print Co-Curricular Transcript
+              </Button>
+            </div>
+            <div>
+              <Typography variant="headline">
+                <b> Co-Curricular Transcript - {this.state.profile.fullName} </b>
+              </Typography>
+            </div>
+            <div className="involvements" class="print">
+              <div className="full-length">{activityList}</div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     );
   }
 }
