c40de1327684c7e30f5df8b1bebc8eaf681cebb0
Yerang Lim <yerang.lim@malachi.gordon.edu>
2019-06-26 14:07:28 -0400

    Schedule service added

--- src/components/SchedulePanel/components/CourseEvents/index.js
+++ src/components/SchedulePanel/components/CourseEvents/index.js
@@ -1,41 +1,4 @@
 import moment, { now } from 'moment';
+import schedule from './../../../../services/schedule';
 
-var startTime = moment();
-startTime.add(1, 'h');
-startTime.set('m', 0);
-
-var endTime = moment().clone(startTime);
-endTime.add(2, 'h');
-endTime.set('m', 0);
-
-export default [
-  {
-    id: 0,
-    title: 'Board meeting',
-    start: startTime.toDate(),
-    end: endTime.toDate(),
-    resourceId: 2,
-  },
-  {
-    id: 1,
-    title: 'MS training',
-    allDay: true,
-    start: startTime.toDate(),
-    end: endTime.toDate(),
-    resourceId: 2,
-  },
-  {
-    id: 2,
-    title: 'Team lead meeting',
-    start: startTime.toDate(),
-    end: endTime.toDate(),
-    resourceId: 3,
-  },
-  {
-    id: 11,
-    title: 'Birthday Party',
-    start: startTime.toDate(),
-    end: endTime.toDate(),
-    resourceId: 4,
-  },
-];
+export default schedule.getScheduleCourses();
