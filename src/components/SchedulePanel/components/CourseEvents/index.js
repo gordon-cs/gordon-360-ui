2d5f960c54ef27600f3815814ddcd6feeb9c60df
Yerang Lim <yerang.lim@malachi.gordon.edu>
2019-06-27 10:29:19 -0400

    Minor Fixes, on test

--- src/components/SchedulePanel/components/CourseEvents/index.js
+++ src/components/SchedulePanel/components/CourseEvents/index.js
@@ -2,3 +2,10 @@ import moment, { now } from 'moment';
 import schedule from './../../../../services/schedule';
 
 export default schedule.getScheduleCourses();
+// [{
+//   id: 0,
+//   title: "courseTitle",
+//   start: new Date(),
+//   end: new Date(),
+//   resourceId: 1,
+// }]
