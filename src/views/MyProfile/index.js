ee3e3b8bb6314cd936307dcc3f820b036a10777a
Joshua Rogers <joshua.rogers@gordon.edu>
2019-07-10 11:55:31 -0400

    Fixed Dropzones

--- src/views/MyProfile/index.js
+++ src/views/MyProfile/index.js
@@ -246,6 +246,7 @@ export default class Profile extends Component {
       const [{ def: defaultImage, pref: preferredImage }] = await Promise.all([
         await user.getImage(),
       ]);
+      console.log(preferredImage);
       const memberships = await user.getMembershipsAlphabetically(profile.ID);
       const involvementsAndTheirPrivacy = await this.getInvolvementAndPrivacyDictionary(
         memberships,
