47cdfc87c6a0f9e6a43e943a2c427eea17b06969
jahnueldorelus <jahnueldorelus@gmail.com>
2019-07-16 22:58:16 -0400

    Fixed the current session for the involvements offline page

--- src/views/ActivitiesAll/index.js
+++ src/views/ActivitiesAll/index.js
@@ -216,7 +216,7 @@ export default class GordonActivitiesAll extends Component {
     if (networkStatus === 'online') {
       SessionList = sessionOptions;
     } else {
-      SessionList = sessionOptions[2];
+      SessionList = sessionOptions[0];
     }
 
     return (
