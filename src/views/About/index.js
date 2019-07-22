eb1ee5040d06bf2b4ab3ce35073ff93e38073663
SeHee Hyung <SeHee.Hyung@gordon.edu>
2019-07-22 13:39:27 -0400

    Reflect Summer '19 work

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -156,6 +156,18 @@ export default class About extends Component {
                 Computer Science (2018)
               </Typography>
               <Typography variant="subheading" gutterBottom>
+                <strong>Software Development Team, Summer 2019</strong>
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Evan Platzer&nbsp;&apos;20, Isaac Bleecker&nbsp;&apos;20, Yi (Edward) Zhou
+                &nbsp;&apos;20, Hyeok Chin (Jake) Moon&nbsp;&apos;20, Phil Lee&nbsp;&apos;20, Joshua
+                Rogers&nbsp;&apos;21, Yerang Lim&nbsp;&apos;21, Jahnuel Dorelus&nbsp;&apos;21, and
+                SeHee Hyung&nbsp;&apos;22
+                <br />
+                Dr.&nbsp;Russ Tuck, Dr.&nbsp;Jonathan Senning&nbsp;&apos;85, Summer Practicum in
+                Computer Science (2019)
+              </Typography>
+              <Typography variant="subheading" gutterBottom>
                 <strong>Beneficent Philanthropes</strong>
               </Typography>
               <Typography variant="body1" paragraph>
