9447be1c17ffe8d71c66ef82c9379a6c92ee8c63
Philip Lee <phil.lee@zephaniah.gordon.edu>
2019-07-25 15:41:46 -0400

    Changed size of DaysLeft title

--- src/views/Home/components/DaysLeft/index.js
+++ src/views/Home/components/DaysLeft/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -109,9 +110,12 @@ export default class DaysLeft extends Component {
     return (
       <Card>
         <CardContent>
-          <Typography variant="headline" style={{ textAlign: 'center', paddingTop: 5 }}>
+          <Grid container direction="row" alignItems="center">
+            <CardHeader title={this.state.currSessionDescription} />
+          </Grid>
+          {/* <Typography variant="headline" style={{ textAlign: 'left', paddingTop: 5 }}>
             {this.state.currSessionDescription}
-          </Typography>
+          </Typography> */}
           {content}
         </CardContent>
       </Card>
