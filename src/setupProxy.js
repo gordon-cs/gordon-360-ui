80a978187a73a3748b9e53efef82faff8196cf0b
Evan Platzer <evan.platzer@gordon.edu>
2019-07-25 11:24:52 -0400

    Changed variable for setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use(proxy('/api', { target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
-  app.use(proxy('/token', { target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
+  app.use(proxy('/api', { target: 'https://360Api.gordon.edu/', changeOrigin: true }));
+  app.use(proxy('/token', { target: 'https://360Api.gordon.edu/', changeOrigin: true }));
 };
