1ef41d91ebf319a4c1c49cd992d1d9ed109cf115
Jacob Bradley <jacobbradley090298@gmail.com>
2019-11-02 16:58:16 -0400

    Add date and time selectors to jobs page. Pair programmed with Adam and Natou.

--- src/views/Timesheets/index.js
+++ src/views/Timesheets/index.js
@@ -1,16 +1,71 @@
 import React, { Component } from 'react';
+import 'date-fns';
+import { Grid, Card, CardContent, CardActions } from '@material-ui/core/';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 import Typography from '@material-ui/core/Typography';
 
-export default class Timesheets extends Component {
-  constructor(props) {
-    super(props);
+export default function Timesheets() {
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2019-01-18T21:11:54'));
 
-    this.state = {
-      network: 'online',
-    };
-  }
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
 
-  render() {
-    return <Typography>Heyyyyy</Typography>;
-  }
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Card>
+        <CardContent
+          style={{
+            marginLeft: 8,
+            marginTop: 8,
+          }}
+        >
+          <Grid container spacing={2} alignItems="center" justify="center">
+            <Grid item xs={12} md={3}>
+              <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                label="Date"
+                format="MM/dd/yyyy"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <KeyboardTimePicker
+                margin="normal"
+                id="time-picker-in"
+                label="Time In"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <KeyboardTimePicker
+                margin="normal"
+                id="time-picker-out"
+                label="Time Out"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </MuiPickersUtilsProvider>
+  );
 }
