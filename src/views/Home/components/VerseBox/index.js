ea12f43fc12a78d2aaf832e0224a83892b56ab55
isaacbleecker <isaacbleecker@gmail.com>
2019-12-03 19:03:43 -0500

    Added Jahnuel and Ari's work on adding verse

---
+++ src/views/Home/components/VerseBox/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import { gordonColors } from '../../../../theme';
+import GordonLoader from '../../../../components/Loader';
+
+export default class VerseOfTheDay extends Component {
+  constructor(props) {
+    super(props);
+
+    this.loadVerse = this.loadVerse.bind(this);
+
+    this.state = {
+      error: null,
+      loading: true,
+      verse: 'This is a test of a verse',
+    };
+  }
+  componentDidMount() {
+    console.log('Made it here boss');
+  }
+
+  async loadVerse() {
+    this.setState({ loading: true });
+    console.log('TRUE');
+    fetch(
+      'https://ajith-holy-bible.p.rapidapi.com/GetVerses?Book=Luke&chapter=1&VerseFrom=5&VerseTo=8',
+      {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'ajith-holy-bible.p.rapidapi.com',
+          'x-rapidapi-key': '680f1d67bfmshf998753267a5dd6p150946jsn6e2c7cd6e30f',
+        },
+      },
+    )
+      .then(response => {
+        console.log(response.json());
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    let verseOfTheDay = this.state.verse;
+    return (
+      <Card>
+        <CardContent>
+          <CardHeader title="Verse Of The Day" />
+          {'verseOfTheDay'}
+        </CardContent>
+      </Card>
+    );
+  }
+}
