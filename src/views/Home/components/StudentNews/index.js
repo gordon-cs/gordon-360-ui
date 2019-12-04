9e7a3746c4bf4a959a9fb85a2f088266acd648db
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2019-12-04 13:59:20 -0500

    Add date submitted to dummy data

--- src/views/Home/components/StudentNews/index.js
+++ src/views/Home/components/StudentNews/index.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import { Doughnut, defaults } from 'react-chartjs-2';
 import Button from '@material-ui/core/Button';
-import Collapse from '@material-ui/core/Collapse';
 import NewsItem from './components/NewsItem';
 import { gordonColors } from '../../../../theme';
-import user from '../../../../services/user';
-import session from '../../../../services/session';
-import GordonLoader from '../../../../components/Loader';
-import List from '@material-ui/core/List';
 import './student-news.css';
 
 export default class StudentNews extends Component {
@@ -62,21 +54,24 @@ export default class StudentNews extends Component {
     );
 
     const content = (
-      <div className="event-list">
+      <div className="news-list">
         <NewsItem
           subject="Selling Christmas Gala Ticket"
           submittedBy="SeHee Hyung"
           description="If you want to buy Christmas Gala ticket, email SeHee Hyung"
+          dateSubmitted="12/2/2019"
         />
         <NewsItem
           subject="Need more meme videos"
           submittedBy="Michael Xiao"
           description="Plz send me your meme videos so I can make meme compilation thx bye"
+          dateSubmitted="12/1/2019"
         />
         <NewsItem
           subject="We need help"
           submittedBy="Student News"
           description="Who do we talk to so we can pull student news items"
+          dateSubmitted="11/28/2019"
         />
       </div>
     );
