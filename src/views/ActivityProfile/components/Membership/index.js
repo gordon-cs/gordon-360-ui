aacb7ca30bd8b4644a64ea3aed336e19a4e802d2
Ben Abbett <36084988+babbett@users.noreply.github.com>
2019-12-05 16:48:50 -0500

    Update index.js
    Removed mailbox location from activity profile

--- src/views/ActivityProfile/components/Membership/index.js
+++ src/views/ActivityProfile/components/Membership/index.js
@@ -467,19 +467,14 @@ export default class Membership extends Component {
           header = (
             <div style={headerStyle}>
               <Grid container direction="row">
-                <Grid item xs={4}>
+                <Grid item xs={6/*4*/}>
                   <Typography variant="body2" className="header" style={headerStyle}>
                     NAME
                   </Typography>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={6/*4*/}>
                   <Typography variant="body2" className="header" style={headerStyle}>
                     PARTICIPATION
-                  </Typography>
-                </Grid>
-                <Grid item xs={4}>
-                  <Typography variant="body2" className="header" style={headerStyle}>
-                    MAILBOX
                   </Typography>
                 </Grid>
               </Grid>
