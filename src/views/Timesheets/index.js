969e7c84a1cb37c8a8ca176e7aa970a310999248
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2019-12-06 15:58:46 -0500

    WIP: Add dropdown to display jobs for 50193229
    Pair programmed with Adam Princiotta and Jacob Bradley

--- src/views/Timesheets/index.js
+++ src/views/Timesheets/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'date-fns';
 import { Grid, Card, CardContent } from '@material-ui/core/';
 import DateFnsUtils from '@date-io/date-fns';
+import jobs from '../../services/jobs';
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
@@ -11,6 +12,13 @@ import ScheduleIcon from '@material-ui/icons/Schedule';
 import './timesheets.css';
 
 export default function Timesheets() {
+  const [userJobs, setUserJobs] = useState([]);
+  useEffect(() => {
+    jobs.getActiveJobsForUser('50193229').then(result => {
+      setUserJobs(result);
+    });
+  }, []);
+
   const [selectedDate, setSelectedDate] = React.useState(new Date());
   const [selectedDate2, setSelectedDate2] = React.useState(new Date());
   const clockIcon = <ScheduleIcon />;
@@ -23,65 +31,73 @@ export default function Timesheets() {
     setSelectedDate2(date);
   };
 
+  const jobsMenuItems = userJobs.map(job => (
+    <option value={job.EML_DESCRIPTION} key={job.ID}>
+      {job.EML_DESCRIPTION}
+    </option>
+  ));
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Card>
-        <CardContent
-          style={{
-            marginLeft: 8,
-            marginTop: 8,
-          }}
-        >
-          <Grid
-            container
-            spacing={2}
-            justify="space-around"
-            alignItems="center"
-            alignContent="center"
+    <>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Card>
+          <CardContent
+            style={{
+              marginLeft: 8,
+              marginTop: 8,
+            }}
           >
-            <Grid item xs={12} sm={3}>
-              <KeyboardDatePicker
-                margin="normal"
-                id="date-picker-dialog"
-                label="Date"
-                format="MM/dd/yyyy"
-                value={selectedDate}
-                onChange={handleDateChange1}
-                KeyboardButtonProps={{
-                  'aria-label': 'change date',
-                }}
-              />
+            <select>{jobsMenuItems}</select>
+            <Grid
+              container
+              spacing={2}
+              justify="space-around"
+              alignItems="center"
+              alignContent="center"
+            >
+              <Grid item xs={12} sm={3}>
+                <KeyboardDatePicker
+                  margin="normal"
+                  id="date-picker-dialog"
+                  label="Date"
+                  format="MM/dd/yyyy"
+                  value={selectedDate}
+                  onChange={handleDateChange1}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <KeyboardTimePicker
+                  margin="normal"
+                  id="time-picker-in"
+                  label="Time In"
+                  value={selectedDate}
+                  onChange={handleDateChange1}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                  }}
+                  keyboardIcon={clockIcon}
+                />
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <KeyboardTimePicker
+                  ampm={false}
+                  margin="normal"
+                  id="time-picker-out"
+                  label="Time Out"
+                  value={selectedDate2}
+                  onChange={handleDateChange2}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                  }}
+                  keyboardIcon={clockIcon}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={3}>
-              <KeyboardTimePicker
-                margin="normal"
-                id="time-picker-in"
-                label="Time In"
-                value={selectedDate}
-                onChange={handleDateChange1}
-                KeyboardButtonProps={{
-                  'aria-label': 'change time',
-                }}
-                keyboardIcon={clockIcon}
-              />
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <KeyboardTimePicker
-                ampm={false}
-                margin="normal"
-                id="time-picker-out"
-                label="Time Out"
-                value={selectedDate2}
-                onChange={handleDateChange2}
-                KeyboardButtonProps={{
-                  'aria-label': 'change time',
-                }}
-                keyboardIcon={clockIcon}
-              />
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </MuiPickersUtilsProvider>
+          </CardContent>
+        </Card>
+      </MuiPickersUtilsProvider>
+    </>
   );
 }
