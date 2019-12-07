94bc4d8d9e26e42a86000b8282284b0eeee0da87
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2019-12-07 13:19:53 -0500

    Add second date picker for MVP
    Disable day out selections that are earlier than / two days after day in
    
    Pair programmed by the whole group

--- src/views/Timesheets/index.js
+++ src/views/Timesheets/index.js
@@ -22,9 +22,22 @@ import './timesheets.css';
 
 export default function Timesheets() {
   const [userJobs, setUserJobs] = useState([]);
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [selectedDate1, setSelectedDate1] = React.useState(new Date());
   const [selectedDate2, setSelectedDate2] = React.useState(new Date());
   const [selectedJob, setSelectedJob] = React.useState('');
+  // const [day2PushedForward, setDay2CPushedForward] = React.useState(false);
+
+  // const handleHourDifference = (timeIn, timeOut) => {
+  //   let timeDiff = timeOut.getTime() - timeIn.getTime();
+  //   if(timeDiff < 0){
+  //     selectedDate2.setTime(selectedDate2.getTime() + 86400000);
+  //     console.log("Day In = " + selectedDate.getDay());
+  //     console.log("Day Out = " + selectedDate2.getDay());
+  //     console.log("overnightHourDiff = " + (selectedDate2.getTime() - selectedDate.getTime()) / 1000 / 60 / 60 )
+  //   } else {
+  //     console.log("It's a regular hour difference my dude", (selectedDate2.getTime() - selectedDate.getTime()) / 1000 / 60 / 60);
+  //   }
+  // }
 
   useEffect(() => {
     jobs.getActiveJobsForUser('50193229').then(result => {
@@ -35,10 +48,15 @@ export default function Timesheets() {
   const clockIcon = <ScheduleIcon />;
 
   const handleDateChange1 = date => {
-    setSelectedDate(date);
+    // let timeDiff = selectedDate2.getTime() - selectedDate.getTime();
+    // handleHourDifference(date, selectedDate2);
+    setSelectedDate1(date);
+    setSelectedDate2(date);
   };
 
   const handleDateChange2 = date => {
+    // let timeDiff = selectedDate2.getTime() - selectedDate.getTime();
+    // handleHourDifference(selectedDate, date);
     setSelectedDate2(date);
   };
 
@@ -47,6 +65,12 @@ export default function Timesheets() {
       {job.EML_DESCRIPTION}
     </MenuItem>
   ));
+
+  const disableDisallowedDays = date => {
+    let dayIn = selectedDate1.getDate();
+    let shouldDisableDate = !(date.getDate() === dayIn || date.getDate() === dayIn + 1);
+    return shouldDisableDate;
+  };
 
   const jobDropdown = (
     <FormControl
@@ -87,25 +111,25 @@ export default function Timesheets() {
               alignItems="center"
               alignContent="center"
             >
-              <Grid item xs={12} sm={3}>
+              <Grid item xs={12} sm={6} md={3}>
                 <KeyboardDatePicker
                   margin="normal"
-                  id="date-picker-dialog"
-                  label="Date"
+                  id="date-picker-in-dialog"
+                  label="Date In"
                   format="MM/dd/yyyy"
-                  value={selectedDate}
+                  value={selectedDate1}
                   onChange={handleDateChange1}
                   KeyboardButtonProps={{
                     'aria-label': 'change date',
                   }}
                 />
               </Grid>
-              <Grid item xs={12} sm={3}>
+              <Grid item xs={12} sm={6} md={3}>
                 <KeyboardTimePicker
                   margin="normal"
                   id="time-picker-in"
                   label="Time In"
-                  value={selectedDate}
+                  value={selectedDate1}
                   onChange={handleDateChange1}
                   KeyboardButtonProps={{
                     'aria-label': 'change time',
@@ -113,7 +137,22 @@ export default function Timesheets() {
                   keyboardIcon={clockIcon}
                 />
               </Grid>
-              <Grid item xs={12} sm={3}>
+              <Grid item xs={12} sm={6} md={3}>
+                <KeyboardDatePicker
+                  hintText="Weekends disabled"
+                  shouldDisableDate={disableDisallowedDays}
+                  margin="normal"
+                  id="date-picker-out-dialog"
+                  label="Date Out"
+                  format="MM/dd/yyyy"
+                  value={selectedDate2}
+                  onChange={handleDateChange2}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
                 <KeyboardTimePicker
                   margin="normal"
                   id="time-picker-out"
@@ -126,7 +165,7 @@ export default function Timesheets() {
                   keyboardIcon={clockIcon}
                 />
               </Grid>
-              <Grid item xs={12} lg={3}>
+              <Grid item xs={12} sm={6} md={3}>
                 {jobDropdown}
               </Grid>
             </Grid>
