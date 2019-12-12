b119cad7717039ab4a1a4361c1e538757f6f3e6a
Jacob Bradley <jacob.bradley@gordon.edu>
2019-12-12 00:51:49 -0500

    Save shift to database

--- src/services/jobs.js
+++ src/services/jobs.js
@@ -22,4 +22,30 @@ const getActiveJobsForUser = userID => {
   return http.get(`jobs/getJobs/${userID}`);
 };
 
-export default { getE2eTestResult, getActiveJobsForUser };
+/**
+ * Get active jobs for current user
+ * @param {String} userID The Gordon id of the user whose jobs to fetch
+ * @return {Promise.<String>} User's active jobs
+ */
+const submitShiftForUser = async (
+  studentID,
+  eml,
+  shiftStart,
+  shiftEnd,
+  hoursWorked,
+  shiftNotes,
+  lastChangedBy,
+) => {
+  let shiftDetails = {
+    ID_NUM: studentID,
+    EML: eml,
+    SHIFT_START_DATETIME: shiftStart,
+    SHIFT_END_DATETIME: shiftEnd,
+    HOURS_WORKED: hoursWorked,
+    SHIFT_NOTES: shiftNotes,
+    LAST_CHANGED_BY: lastChangedBy,
+  };
+  return await http.post(`jobs/submitShift/`, shiftDetails);
+};
+
+export default { getE2eTestResult, getActiveJobsForUser, submitShiftForUser };
