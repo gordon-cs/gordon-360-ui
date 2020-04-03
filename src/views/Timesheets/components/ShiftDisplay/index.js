9ab7285ddfed06ea28990d26e7a1966ccc48be1a
Adam Princiotta <adamprinciotta@gmail.com>
2020-04-03 16:09:16 -0400

    Begin to move shift fetch to shift display

--- src/views/Timesheets/components/ShiftDisplay/index.js
+++ src/views/Timesheets/components/ShiftDisplay/index.js
@@ -24,14 +24,22 @@ export default class ShiftDisplay extends Component {
         this.rejectedShiftListComponent = null;
     }
 
-    loadSavedShifts = () => {
-        this.props.getSavedShiftsForUser().then(data => {
+    componentDidMount() {
+        console.log()
+        this.loadSavedShifts();
+    }
+
+    loadSavedShifts() {
+        const {userId, getSavedShiftsForUser} = this.props; 
+        console.log('user ID:', userId);
+        getSavedShiftsForUser(userId).then(data => {
             this.setState({
                 shifts: data,
                 jobNames: [...new Set(this.state.shifts.EML_DESCRIPTION)],
             });
             console.log(data, this.state.shifts);
-        });
+            console.log('job names:', this.state.jobNames);
+        }).catch(error => {console.log('error:', error)});
     };
 
     handleTabChange = (event, value) => {
@@ -39,87 +47,89 @@ export default class ShiftDisplay extends Component {
     }
 
     render() {
-        const {userId, getSavedShiftsForUser, setSavedShiftListComponent} = this.props;
-        let savedShiftsList =  userId !== '' ? (
-            <SavedShiftsList ref={setSavedShiftListComponent} submittedList={this.submittedShiftListComponent} getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Saved Shifts" />
-        ) : (
-            <>
-                <CardContent>
-                    <GordonLoader />
-                </CardContent>
-            </>
-        );
+        // const {userId, getSavedShiftsForUser, setSavedShiftListComponent} = this.props;
+        // let savedShiftsList =  userId !== '' ? (
+        //     <SavedShiftsList ref={setSavedShiftListComponent} submittedList={this.submittedShiftListComponent} getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Saved Shifts" />
+        // ) : (
+        //     <>
+        //         <CardContent>
+        //             <GordonLoader />
+        //         </CardContent>
+        //     </>
+        // );
 
-        let submittedShiftsList = userId !== '' ? (
-            <SavedShiftsList ref={comp => {this.submittedShiftListComponent = comp}} getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Submitted Shifts" />
-        ) : (
-            <>
-                <CardContent>
-                    <GordonLoader />
-                </CardContent>
-            </>
-        );
+        // let submittedShiftsList = userId !== '' ? (
+        //     <SavedShiftsList ref={comp => {this.submittedShiftListComponent = comp}} getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Submitted Shifts" />
+        // ) : (
+        //     <>
+        //         <CardContent>
+        //             <GordonLoader />
+        //         </CardContent>
+        //     </>
+        // );
 
-        let approvedShiftsList = userId !== '' ? (
-            <SavedShiftsList getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Approved Shifts" />
-        ) : (
-            <>
-                <CardContent>
-                    <GordonLoader />
-                </CardContent>
-            </>
-        );
+        // let approvedShiftsList = userId !== '' ? (
+        //     <SavedShiftsList getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Approved Shifts" />
+        // ) : (
+        //     <>
+        //         <CardContent>
+        //             <GordonLoader />
+        //         </CardContent>
+        //     </>
+        // );
 
-        let rejectedShiftsList = userId !== '' ? (
-            <SavedShiftsList ref={comp => {this.rejectedShiftListComponent = comp}} getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Rejected Shifts" />
-        ) : (
-            <>
-                <CardContent>
-                    <GordonLoader />
-                </CardContent>
-            </>
-        );
+        // let rejectedShiftsList = userId !== '' ? (
+        //     <SavedShiftsList ref={comp => {this.rejectedShiftListComponent = comp}} getShifts={getSavedShiftsForUser} userID={userId} cardTitle="Rejected Shifts" />
+        // ) : (
+        //     <>
+        //         <CardContent>
+        //             <GordonLoader />
+        //         </CardContent>
+        //     </>
+        // );
 
-        return (
-            <>
-                <Grid item xs={12}>
-                    <Card>
-                        <CardContent>
-                            <CardHeader title="Display shifts for:" />
-                            <Tabs centered value={this.state.tabValue} onChange={this.handleTabChange} fullWidth={false} className="job-tabs">
-                                <Tab
-                                    className="job-tab"
-                                    label="code nerd"
-                                />
-                                <Tab
-                                    className="job-tab"
-                                    label="media nerd"
-                                />
-                                <Tab
-                                    className="job-tab"
-                                    label="DML"
-                                />
-                            </Tabs>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            {savedShiftsList}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {submittedShiftsList}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {rejectedShiftsList}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {approvedShiftsList}
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </>
-        )
+        // return (
+        //     <>
+        //         <Grid item xs={12}>
+        //             <Card>
+        //                 <CardContent>
+        //                     <CardHeader title="Display shifts for:" />
+        //                     <Tabs centered value={this.state.tabValue} onChange={this.handleTabChange} fullWidth={false} className="job-tabs">
+        //                         <Tab
+        //                             className="job-tab"
+        //                             label="code nerd"
+        //                         />
+        //                         <Tab
+        //                             className="job-tab"
+        //                             label="media nerd"
+        //                         />
+        //                         <Tab
+        //                             className="job-tab"
+        //                             label="DML"
+        //                         />
+        //                     </Tabs>
+        //                 </CardContent>
+        //             </Card>
+        //         </Grid>
+        //         <Grid item xs={12}>
+        //             <Grid container spacing={2}>
+        //                 <Grid item xs={12}>
+        //                     {savedShiftsList}
+        //                 </Grid>
+        //                 <Grid item xs={12}>
+        //                     {submittedShiftsList}
+        //                 </Grid>
+        //                 <Grid item xs={12}>
+        //                     {rejectedShiftsList}
+        //                 </Grid>
+        //                 <Grid item xs={12}>
+        //                     {approvedShiftsList}
+        //                 </Grid>
+        //             </Grid>
+        //         </Grid>
+        //     </>
+        // )
+
+        return null;
     }
 }
