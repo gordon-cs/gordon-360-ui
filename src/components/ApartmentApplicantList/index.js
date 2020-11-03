ac35f83141fea65a0060cebdf1ddc2337495970a
Christian Kunis <ckunis98@gmail.com>
2020-11-02 22:17:38 -0500

    Added save button

--- src/components/ApartmentApplicantList/index.js
+++ src/components/ApartmentApplicantList/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import 'date-fns';
-import { Grid, Card, CardHeader, CardContent, List } from '@material-ui/core';
+import { Grid, Card, CardHeader, CardContent, List, Button, Typography } from '@material-ui/core';
 import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import GordonPeopleSearch from '../Header/components/PeopleSearch';
 import ApplicantListItem from './components/ApplicantListItem';
 import '../../views/ApartmentApp/apartmentApp.css';
 import '../../views/PeopleSearch/components/PeopleSearchResult/peopleSearchResult.css';
+const MIN_NUM_APPLICANTS = 3;
 
 // Create a list of applicants, displayed by name, username, and class standing.
 export default class ApplicantList extends Component {
@@ -13,6 +14,10 @@ export default class ApplicantList extends Component {
     super(props);
     this.handleSelection = this.handleSelection.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleSaveButtonClick = this.handleSaveButtonClick.bind(this);
+    this.state = {
+      saveButtonClicked: false,
+    };
   }
 
   handleSelection = (theChosenOne) => {
@@ -20,6 +25,8 @@ export default class ApplicantList extends Component {
     if (theChosenOne) {
       // Send the selected username to the parent component
       this.props.onSearchSubmit(theChosenOne);
+      // Reset the save button
+      this.setState({ saveButtonClicked: false });
     }
   };
 
@@ -28,7 +35,14 @@ export default class ApplicantList extends Component {
     if (profile) {
       // Send the selected profile to the parent component
       this.props.onApplicantRemove(profile);
+      // Reset the save button
+      this.setState({ saveButtonClicked: false });
     }
+  };
+
+  handleSaveButtonClick = () => {
+    this.props.onSaveButtonClick();
+    this.setState({ saveButtonClicked: true });
   };
 
   render() {
@@ -49,7 +63,7 @@ export default class ApplicantList extends Component {
           className="card-header"
         />
         <CardContent>
-          <Grid container direction="column" spacing={8}>
+          <Grid container justify="flex-end">
             <Grid item xs={12}>
               <List className="applicant-list" aria-label="apartment applicants">
                 {this.props.applicants ? (
@@ -70,6 +84,23 @@ export default class ApplicantList extends Component {
                   />
                 )}
               </List>
+            </Grid>
+            <Grid item>
+              {this.props.applicants.length < MIN_NUM_APPLICANTS && this.state.saveButtonClicked ? (
+                <Typography variant="overline" color="error">
+                  Must have at least {MIN_NUM_APPLICANTS} applicants
+                </Typography>
+              ) : null}
+            </Grid>
+            <Grid item xs={2}>
+              <Button
+                // disabled={this.props.applicants.length < MIN_NUM_APPLICANTS}
+                variant="contained"
+                color="primary"
+                onClick={this.handleSaveButtonClick}
+              >
+                Save
+              </Button>
             </Grid>
           </Grid>
         </CardContent>
