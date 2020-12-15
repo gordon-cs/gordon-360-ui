666e26d7b406d2d65c43075dcaab93318504c079
Evan Platzer <evan.platzer@gmail.com>
2020-12-15 10:52:05 -0500

    Add network context
    This commit introduces the NetworkContext, which uses React's Context API to track the online/offline status of the user's network. The goal is to have network-aware component's subscribe to this context provider so that the network is tracked in only one place, rather than having eventListeners in every component that needs to know the network status.
    
    This may result in minimal performace gains (having only one event listener attached to the window instead of >10 at once), but I would guess that the rerendering takes significantly more processing than the eventlisteners themselves. The real advantage to this technique is the separation of concerns. Now, the NetworkContextProvider is the only component responsible for the logic of tracking network changes. Every ther network-aware component simply subscribes to these changes with the very minmial hook useNetworkStatus - e.g. `const networkStatus = useNetworkStatus`, rather than having a large block of logic duplicated across many components with a useEffect hook and window listeners that obscures the component's more natural responsibilities.

---
+++ src/contexts/NetworkContext.js
@@ -0,0 +1,61 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import storage from '../services/storage';
+
+const NetworkContext = createContext();
+
+/**
+ * Custom hook to subscribe to network status.
+ *
+ * Value is retrieved from local storage initially and updated by service worker firing events
+ *
+ * Can be used by any functional component under the NetworkContextProvider in App.js
+ *
+ * @returns {boolean} true if connected to the network, false otherwise.
+ */
+export const useNetworkStatus = () => {
+  const context = useContext(NetworkContext);
+  if (context === undefined) {
+    throw new Error(`useNetworkStatus must be called within NetworkContextProvider`);
+  }
+
+  return context;
+};
+
+const NetworkContextProvider = ({ children }) => {
+  const [isOnline, setIsOnline] = useState('online');
+
+  useEffect(() => {
+    // Retrieve network status from local storage or default to online
+    try {
+      const networkStatus = storage.get('network-status');
+      setIsOnline(networkStatus === 'online');
+    } catch (error) {
+      console.error(error);
+      setIsOnline(true);
+    }
+
+    /* Used to re-render the page when the network connection changes.
+     * The origin of the message is checked to prevent cross-site scripting attacks
+     */
+    const updateNetworkStatus = (event) => {
+      setIsOnline((prevStatus) => {
+        if (
+          event.origin === window.location.origin &&
+          (event.data === 'online' || event.data === 'offline')
+        ) {
+          return event.data === 'online';
+        } else {
+          return prevStatus;
+        }
+      });
+    };
+
+    window.addEventListener('message', updateNetworkStatus);
+
+    return () => window.removeEventListener('message', updateNetworkStatus);
+  }, []);
+
+  return <NetworkContext.Provider value={isOnline}>{children}</NetworkContext.Provider>;
+};
+
+export default NetworkContextProvider;
