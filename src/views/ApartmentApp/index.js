5f5c40bf9dffd8269129f5fc0eea58a4f00532e3
Christian Kunis <ckunis98@gmail.com>
2021-01-26 17:06:18 -0500

    Changes to ApartApp landing page requested by review

--- src/views/ApartmentApp/index.js
+++ src/views/ApartmentApp/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import 'date-fns';
 import { Grid, Card, CardContent, Button } from '@material-ui/core/';
 import GordonLoader from '../../components/Loader';
 import StudentApplication from './components/StudentApplication';
@@ -14,15 +13,11 @@ export default class ApartApp extends Component {
     this.peopleSearch = React.createRef();
     this.state = {
       isStu: Boolean,
-      isFac: Boolean,
-      isAlu: Boolean,
+      isHousingStaff: Boolean,
       loading: true,
       network: 'online',
       userProfile: {},
     };
-    this.snackbarText = '';
-    this.snackbarSeverity = '';
-    this.saveButtonAlertTimeout = null;
   }
 
   componentDidMount() {
@@ -38,8 +33,6 @@ export default class ApartApp extends Component {
       const profile = await user.getProfileInfo();
       this.setState({ userProfile: profile });
       this.setState({ isStu: String(profile.PersonType).includes('stu') });
-      this.setState({ isFac: String(profile.PersonType).includes('fac') });
-      this.setState({ isAlu: String(profile.PersonType).includes('alu') });
       this.setState({ loading: false });
     } catch (error) {
       // Do Nothing
@@ -75,92 +68,82 @@ export default class ApartApp extends Component {
        */
       const networkStatus = JSON.parse(localStorage.getItem('network-status')) || 'online';
 
-      if (networkStatus === 'online' && this.state.isStu && this.props.authentication) {
-        return (
-          <div>
-            {this.state.loading ? (
-              <GordonLoader />
-            ) : (
-              <div className="student-apartment-application">
-                <StudentApplication
-                  userProfile={this.state.userProfile}
-                  authentication={this.props.authentication}
-                />
-              </div>
-            )}
-          </div>
-        );
-      } else if (networkStatus === 'online' && !this.state.isStu && this.props.authentication) {
-        return (
-          <div>
-            {this.state.loading ? (
-              <GordonLoader />
-            ) : (
-              <div className="staff-apartment-application">
-                <StaffMenu authentication={this.props.authentication} />
-              </div>
-            )}
-          </div>
-        );
-      } else {
-        // If the network is offline or the user type is non-student
-        if (networkStatus === 'offline' || !this.state.isStu) {
+      if (networkStatus === 'online') {
+        if (this.state.loading) {
+          return <GordonLoader />;
+        } else if (this.state.isStu) {
           return (
-            <Grid container justify="center" spacing="16">
-              <Grid item xs={12} md={8}>
-                <Card>
-                  <CardContent
-                    style={{
-                      margin: 'auto',
-                      textAlign: 'center',
-                    }}
-                  >
-                    {networkStatus === 'offline' && (
-                      <Grid
-                        item
-                        xs={2}
-                        alignItems="center"
-                        style={{
-                          display: 'block',
-                          marginLeft: 'auto',
-                          marginRight: 'auto',
-                        }}
-                      >
-                        <img
-                          src={require(`${'../../NoConnection.svg'}`)}
-                          alt="Internet Connection Lost"
-                        />
-                      </Grid>
-                    )}
-                    <br />
-                    <h1>
-                      {networkStatus === 'offline'
-                        ? 'Please re-establish connection'
-                        : 'Apartment application Unavailable'}
-                    </h1>
-                    <h4>
-                      {networkStatus === 'offline'
-                        ? 'Apartment application entry has been disabled due to loss of network.'
-                        : 'Apartment application is currently available for students only. Support for staff will come soon!'}
-                    </h4>
-                    <br />
-                    <br />
-                    <Button
-                      color="primary"
-                      backgroundColor="white"
-                      variant="outlined"
-                      onClick={() => {
-                        window.location.pathname = '';
-                      }}
-                    >
-                      Back To Home
-                    </Button>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
+            <div className="student-apartment-application">
+              <StudentApplication
+                userProfile={this.state.userProfile}
+                authentication={this.props.authentication}
+              />
+            </div>
+          );
+        } else if (this.state.isHousingStaff) {
+          return (
+            <div className="staff-apartment-application">
+              <StaffMenu authentication={this.props.authentication} />
+            </div>
           );
         }
+      } else if (networkStatus === 'offline' || !this.state.isStu) {
+        // If the network is offline or the user type is non-student
+        return (
+          <Grid container justify="center" spacing="16">
+            <Grid item xs={12} md={8}>
+              <Card>
+                <CardContent
+                  style={{
+                    margin: 'auto',
+                    textAlign: 'center',
+                  }}
+                >
+                  {networkStatus === 'offline' && (
+                    <Grid
+                      item
+                      xs={2}
+                      alignItems="center"
+                      style={{
+                        display: 'block',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                      }}
+                    >
+                      <img
+                        src={require(`${'../../NoConnection.svg'}`)}
+                        alt="Internet Connection Lost"
+                      />
+                    </Grid>
+                  )}
+                  <br />
+                  <h1>
+                    {networkStatus === 'offline'
+                      ? 'Please re-establish connection'
+                      : 'Apartment application Unavailable'}
+                  </h1>
+                  <h4>
+                    {networkStatus === 'offline'
+                      ? 'Apartment application entry has been disabled due to loss of network.'
+                      : 'Apartment application is available for students only.'}
+                  </h4>
+                  <br />
+                  <br />
+                  <Button
+                    color="primary"
+                    backgroundColor="white"
+                    variant="outlined"
+                    onClick={() => {
+                      window.location.pathname = '';
+                    }}
+                  >
+                    Back To Home
+                  </Button>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        );
       }
     } else {
       // The user is not logged in
