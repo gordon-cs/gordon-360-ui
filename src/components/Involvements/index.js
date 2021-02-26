cb87ec2872d49cbb6dbd5db7616cf9a06765dbcb
Evan Platzer <Evan.Platzer@gmail.com>
2021-02-26 09:09:40 -0500

    Refactor MyProfileActivityList to useNetworkStatus

--- src/components/Involvements/index.js
+++ src/components/Involvements/index.js
@@ -1,22 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import activity from '../../services/activity';
-import storage from '../../services/storage';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Grid from '@material-ui/core/Grid';
+
+import { Button, Card, CardContent, CardHeader, Grid, List, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import List from '@material-ui/core/List';
+import activity from '../../services/activity';
+import membershipService from '../../services/membership';
+import SimpleSnackbar from '../Snackbar';
 import MyProfileActivityList from '../MyProfileActivityList/index';
 import ProfileActivityList from '../ProfileActivityList/index';
-import Typography from '@material-ui/core/Typography';
 import './index.css';
 
 export const Involvements = (props) => {
   const [involvementsAndTheirPrivacy, setInvolvementsAndTheirPrivacy] = useState([]);
+  const [snackbar, setSnackbar] = useState({ message: '', severity: '', open: false });
   const [publicMemberships, setPublicMemberships] = useState([]);
-  const [network, setNetwork] = useState();
+  console.log(snackbar);
 
   /**
    * Loads the user's profile info only once (at start)
@@ -41,46 +38,6 @@ export const Involvements = (props) => {
     loadInvolvements();
   }, [props.memberships, props.myProf]);
 
-  useEffect(() => {
-    let networkStatus;
-    /* Attempts to get the network status from local storage.
-     * If not found, the default value is online
-     */
-    try {
-      networkStatus = storage.get('network-status');
-    } catch (error) {
-      // Defaults the network to online if not found in local storage
-      networkStatus = 'online';
-    }
-
-    // Saves the network's status to this component's state
-    setNetwork(networkStatus);
-  }, [network]);
-
-  useEffect(() => {
-    /* Used to re-render the page when the network connection changes.
-     *  The state's network variable is compared to the message received to prevent
-     *  multiple re-renders that creates extreme performance lost.
-     *  The origin of the message is checked to prevent cross-site scripting attacks
-     */
-    window.addEventListener('message', (event) => {
-      if (
-        event.data === 'online' &&
-        network === 'offline' &&
-        event.origin === window.location.origin
-      ) {
-        setNetwork('online');
-      } else if (
-        event.data === 'offline' &&
-        network === 'online' &&
-        event.origin === window.location.origin
-      ) {
-        setNetwork('offline');
-      }
-    });
-    return window.removeEventListener('message', () => {});
-  }, [network]);
-
   // Creates the Involvements list
   function createInvolvementsList() {
     // Creates the Involvements list for the My Profile page
@@ -101,9 +58,9 @@ export const Involvements = (props) => {
       else {
         return involvementsAndTheirPrivacy.map((involvementPrivacyKeyValuePair) => (
           <MyProfileActivityList
-            Membership={involvementPrivacyKeyValuePair.key}
-            InvolvementPrivacy={involvementPrivacyKeyValuePair.value}
-            network={network}
+            membership={involvementPrivacyKeyValuePair.key}
+            isPrivateInvolvement={involvementPrivacyKeyValuePair.value}
+            onTogglePrivacy={toggleMembershipPrivacy}
           />
         ));
       }
@@ -125,6 +82,20 @@ export const Involvements = (props) => {
       }
     }
   }
+
+  const toggleMembershipPrivacy = async (membership) => {
+    try {
+      await membershipService.toggleMembershipPrivacy(membership);
+      createSnackbar(membership.Privacy ? 'Membership Hidden' : 'Membership Visible', 'Success');
+      membership.Privacy = !membership.Privacy;
+    } catch {
+      createSnackbar('Privacy Change Failed', 'Error');
+    }
+  };
+
+  const createSnackbar = (message, severity) => {
+    setSnackbar({ message, severity, open: true });
+  };
 
   // Gets the privacy data of each membership of the user
   async function getInvolvementAndPrivacyDictionary(membershipsList) {
@@ -154,24 +125,38 @@ export const Involvements = (props) => {
   }
 
   return (
-    <Grid item xs={12} className="involvements">
-      <Grid container className="involvements-header">
-        <CardHeader title="Involvements" />
+    <>
+      <Grid item xs={12} className="involvements">
+        <Grid container className="involvements-header">
+          <CardHeader title="Involvements" />
+        </Grid>
+        <Card className="involvements-card">
+          <CardContent className="involvements-card-content">
+            {props.myProf && (
+              <Grid container justify="center">
+                <Link className="gc360-link" to="/transcript">
+                  <Button variant="contained" className="involvements-card-content-button">
+                    Experience Transcript
+                  </Button>
+                </Link>
+              </Grid>
+            )}
+            <List>{createInvolvementsList()}</List>
+          </CardContent>
+        </Card>
       </Grid>
-      <Card className="involvements-card">
-        <CardContent className="involvements-card-content">
-          {props.myProf && (
-            <Grid container justify="center">
-              <Link className="gc360-link" to="/transcript">
-                <Button variant="contained" className="involvements-card-content-button">
-                  Experience Transcript
-                </Button>
-              </Link>
-            </Grid>
-          )}
-          <List>{createInvolvementsList()}</List>
-        </CardContent>
-      </Card>
-    </Grid>
+      {props.myProf && (
+        <SimpleSnackbar
+          open={snackbar.open}
+          text={snackbar.message}
+          severity={'success'}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          onClose={() => setSnackbar((s) => ({ ...s, open: false }))}
+        />
+      )}
+    </>
   );
 };
--- src/components/MyProfileActivityList/index.js
+++ src/components/MyProfileActivityList/index.js
@@ -1,365 +1,105 @@
-import Grid from '@material-ui/core/Grid';
-import PropTypes from 'prop-types';
-import Divider from '@material-ui/core/Divider';
-import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import classnames from 'classnames';
+import { Divider, Grid, List, ListItem, Switch, Typography } from '@material-ui/core/';
 import { Link } from 'react-router-dom';
-import Switch from '@material-ui/core/Switch';
-import membership from './../../services/membership';
-import List from '@material-ui/core/List';
 import LockIcon from '@material-ui/icons/Lock';
-import ListItem from '@material-ui/core/ListItem';
-import Snackbar from '@material-ui/core/Snackbar';
-import ErrorIcon from '@material-ui/icons/Error';
-import CloseIcon from '@material-ui/icons/Close';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import IconButton from '@material-ui/core/IconButton';
-import '../ProfileList/profileList.css';
-import '../../app.css';
+import useNetworkStatus from '../../hooks/useNetworkStatus';
 import './index.css';
 
-export default class MyProfileActivityList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSnackbarOpen: false,
-      snackbarMessage: '',
-      snackbarType: '',
-      snackbarKey: 0,
-      switchChecked: !props.Membership.Privacy,
-    };
+const MyProfileActivityList = ({ membership, isPrivateInvolvement, onTogglePrivacy }) => {
+  const isOnline = useNetworkStatus();
 
-    this.createSnackbar = this.createSnackbar.bind(this);
-  }
-
-  async handleChangeMembershipPrivacy(userMembership) {
-    try {
-      let result = await membership.toggleMembershipPrivacy(userMembership);
-      // If no error occured above, then changing the activity's privacy was successful
-      if (result) {
-        this.createSnackbar(
-          userMembership.Privacy ? 'Membership Hidden' : 'Membership Visible',
-          'Success',
-        );
-        this.setState({ switchChecked: !this.state.switchChecked });
-      }
-      // Changing the activity's privacy failed
-      else {
-        this.createSnackbar('Privacy Change Failed', 'Error');
-      }
-    } catch {
-      // Changing the activity's privacy failed
-      this.createSnackbar('Privacy Change Failed', 'Error');
-    }
-  }
-
-  /**
-   * Displays the snackbar to the user.
-   * @param {String} message The message to display to the user
-   * @param {String} messageType The message's type. Either a success or error
-   */
-  createSnackbar(message, messageType) {
-    // Sets the snackbar key as either 0 or 1. This prevents a high number being made.
-    this.setState({
-      snackbarMessage: message,
-      snackbarType: messageType,
-      snackbarKey: (this.state.snackbarKey + 1) % 2,
-      isSnackbarOpen: true,
+  const OnlineOnlyLink = ({ children }) => {
+    const linkClass = classnames({
+      'gc360-link': isOnline,
+      'private-membership': isPrivateInvolvement || membership.Privacy,
+      'public-membership': !(isPrivateInvolvement || membership.Privacy),
     });
-  }
-
-  render() {
-    // Gets the membership and involvement privacy
-    const { Membership, InvolvementPrivacy } = this.props;
-    // Style of privacy text
-    const toggleTextStyle = {
-      fontSize: '12pt',
-    };
-    // Creates the opacity of the card to reflect the membership's privacy
-    const membershipItemStyle = {
-      opacity: Membership.Privacy || InvolvementPrivacy ? '0.5' : '1',
-    };
-    // The Grid lengths for each content inside of the card based on Material-UI's breakpoints
-    const cardContentLengths = {
-      contentOne: {
-        xs: 8,
-        sm: 9,
-        md: 9,
-        lg: 8,
-        xl: 9,
-      },
-      contentTwo: {
-        xs: 4,
-        sm: 3,
-        md: 3,
-        lg: 4,
-        xl: 3,
-      },
-    };
-
-    let myProfileInvolvementsList;
-
-    // If the Involvement is a regular (non-special/secret group - AKA Public) it is False.
-    if (!InvolvementPrivacy) {
-      myProfileInvolvementsList = (
-        <div>
-          <Grid container alignItems="center" justify="center" className="my-profile-info-card">
-            {/* CONTENT ONE (Activity Text and Privacy) */}
-            <Grid
-              container
-              xs={cardContentLengths.contentOne.xs}
-              sm={cardContentLengths.contentOne.sm}
-              md={cardContentLengths.contentOne.md}
-              lg={cardContentLengths.contentOne.lg}
-              xl={cardContentLengths.contentOne.xl}
-              justify="center"
-              alignItems="center"
-              className="my-profile-info-card-info-one"
-            >
-              <Grid container xs={8} alignItem="center">
-                <List>
-                  <ListItem className="my-profile-info-card-info-one-text">
-                    {/* A link to the activity is only available if the user is online */}
-                    {this.props.network === 'online' ? (
-                      <Link
-                        className="gc360-link"
-                        to={`/activity/${Membership.SessionCode}/${Membership.ActivityCode}`}
-                        style={membershipItemStyle}
-                      >
-                        <Typography>
-                          <b>{Membership.ActivityDescription}</b>
-                        </Typography>
-                        <Typography>{Membership.SessionDescription}</Typography>
-                        <Typography>{Membership.ParticipationDescription}</Typography>
-                      </Link>
-                    ) : (
-                      <div style={membershipItemStyle}>
-                        <Typography>
-                          <b>{Membership.ActivityDescription}</b>
-                        </Typography>
-                        <Typography>{Membership.SessionDescription}</Typography>
-                        <Typography>{Membership.ParticipationDescription}</Typography>
-                      </div>
-                    )}
-                  </ListItem>
-                </List>
-              </Grid>
-              <Grid container xs={4} alignItems="center">
-                <Grid container direction="column">
-                  <Grid item align="center">
-                    {this.props.network === 'online' && (
-                      <Switch
-                        onChange={() => {
-                          this.handleChangeMembershipPrivacy(Membership);
-                        }}
-                        checked={this.state.switchChecked}
-                      />
-                    )}
-                  </Grid>
-
-                  <Grid item align="center">
-                    <Typography style={toggleTextStyle}>
-                      {Membership.Privacy ? 'Private' : 'Public'}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-            {/* CONTENT TWO (Activity Picture) */}
-            <Grid
-              container
-              xs={cardContentLengths.contentTwo.xs}
-              sm={cardContentLengths.contentTwo.sm}
-              md={cardContentLengths.contentTwo.md}
-              lg={cardContentLengths.contentTwo.lg}
-              xl={cardContentLengths.contentTwo.xl}
-              className="my-profile-info-card-info-two"
-              alignItems="center"
-            >
-              {/* A link to the activity is only available if the user is online */}
-              {this.props.network === 'online' ? (
-                <Link
-                  className="gc360-link"
-                  to={`/activity/${Membership.SessionCode}/${Membership.ActivityCode}`}
-                  style={membershipItemStyle}
-                >
-                  <img src={Membership.ActivityImagePath} alt="" className="active" />
-                </Link>
-              ) : (
-                <div style={membershipItemStyle}>
-                  <img src={Membership.ActivityImagePath} alt="" />
-                </div>
-              )}
-            </Grid>
-          </Grid>
-          <Divider />
-        </div>
+    if (isOnline) {
+      return (
+        <Link
+          className={linkClass}
+          to={`/activity/${membership.SessionCode}/${membership.ActivityCode}`}
+        >
+          {children}
+        </Link>
       );
-      // If the Involvement is some kind of Private group (ejj.g. scholarship group etc. - AKA Private) it is False.
     } else {
-      myProfileInvolvementsList = (
-        <div>
-          <Grid container alignItems="center" justify="center" className="my-profile-info-card">
-            {/* CONTENT ONE (Activity Text and Privacy) */}
-            <Grid
-              container
-              xs={cardContentLengths.contentOne.xs}
-              sm={cardContentLengths.contentOne.sm}
-              md={cardContentLengths.contentOne.md}
-              lg={cardContentLengths.contentOne.lg}
-              xl={cardContentLengths.contentOne.xl}
-              justify="center"
-              alignItems="center"
-              className="my-profile-info-card-info-one"
-            >
-              <Grid container xs={8} alignItem="center">
-                <List>
-                  <ListItem className="my-profile-info-card-info-one-text">
-                    {/* A link to the activity is only available if the user is online */}
-                    {this.props.network === 'online' ? (
-                      <Link
-                        className="gc360-link"
-                        to={`/activity/${Membership.SessionCode}/${Membership.ActivityCode}`}
-                        style={membershipItemStyle}
-                      >
-                        <Typography>
-                          <b>{Membership.ActivityDescription}</b>
-                        </Typography>
-                        <Typography>{Membership.SessionDescription}</Typography>
-                        <Typography>{Membership.ParticipationDescription}</Typography>
-                      </Link>
-                    ) : (
-                      <div style={membershipItemStyle}>
-                        <Typography>
-                          <b>{Membership.ActivityDescription}</b>
-                        </Typography>
-                        <Typography>{Membership.SessionDescription}</Typography>
-                        <Typography>{Membership.ParticipationDescription}</Typography>
-                      </div>
-                    )}
-                  </ListItem>
-                </List>
-              </Grid>
+      return <div className={linkClass}>{children}</div>;
+    }
+  };
 
-              <Grid container xs={4} alignItems="center">
-                {this.props.network === 'online' && (
-                  <Grid container>
-                    <Grid item xs={12} align="center">
-                      <Grid container justify="center">
-                        <Grid item>
-                          <LockIcon className="lock-icon" />
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={12} align="center">
-                      <Typography style={toggleTextStyle}>
-                        {InvolvementPrivacy ? 'Private' : 'Public'}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                )}
+  return (
+    <>
+      <Grid container alignItems="center" justify="center" className="my-profile-info-card">
+        {/* CONTENT ONE (Activity Text and Privacy) */}
+        <Grid
+          container
+          xs={8}
+          sm={9}
+          md={9}
+          lg={8}
+          xl={9}
+          justify="center"
+          alignItems="center"
+          className="my-profile-info-card-description"
+        >
+          <Grid container xs={8} alignItem="center">
+            <List>
+              <ListItem className="my-profile-info-card-description-text">
+                <OnlineOnlyLink>
+                  <Typography>
+                    <b>{membership.ActivityDescription}</b>
+                  </Typography>
+                  <Typography>{membership.SessionDescription}</Typography>
+                  <Typography>{membership.ParticipationDescription}</Typography>
+                </OnlineOnlyLink>
+              </ListItem>
+            </List>
+          </Grid>
+
+          <Grid container xs={4} alignItems="center">
+            <Grid container>
+              <Grid item xs={12} align="center">
+                {isOnline &&
+                  (isPrivateInvolvement ? (
+                    <LockIcon className="lock-icon" />
+                  ) : (
+                    <Switch
+                      onChange={() => {
+                        onTogglePrivacy(membership);
+                      }}
+                      checked={!membership.Privacy}
+                    />
+                  ))}
               </Grid>
-            </Grid>
-            {/* CONTENT TWO (Activity Picture) */}
-            <Grid
-              container
-              xs={cardContentLengths.contentTwo.xs}
-              sm={cardContentLengths.contentTwo.sm}
-              md={cardContentLengths.contentTwo.md}
-              lg={cardContentLengths.contentTwo.lg}
-              xl={cardContentLengths.contentTwo.xl}
-              className="my-profile-info-card-info-two"
-              alignItems="center"
-            >
-              {/* A link to the activity is only available if the user is online */}
-              {this.props.network === 'online' ? (
-                <Link
-                  className="gc360-link"
-                  to={`/activity/${Membership.SessionCode}/${Membership.ActivityCode}`}
-                  style={membershipItemStyle}
-                >
-                  <img src={Membership.ActivityImagePath} alt="" className="active" />
-                </Link>
-              ) : (
-                <div style={membershipItemStyle}>
-                  <img src={Membership.ActivityImagePath} alt="" />
-                </div>
-              )}
+              <Grid item xs={12} align="center">
+                <Typography>
+                  {membership.Privacy || isPrivateInvolvement ? 'Private' : 'Public'}
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
-          <Divider />
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        {myProfileInvolvementsList}
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          // Makes every snackbar unique to prevent the same snackbar from being updated
-          key={this.state.snackbarKey.toString()}
-          open={this.state.isSnackbarOpen}
-          autoHideDuration={6000}
-          onClose={() => {
-            this.setState({ isSnackbarOpen: false });
-          }}
-          ContentProps={{
-            'aria-describedby': 'message-id',
-          }}
-          message={
-            // If the message type is Success
-            this.state.snackbarType === 'Success' ? (
-              <span id="message-id">
-                <CheckCircleIcon
-                  style={{
-                    marginBottom: '-4.5pt',
-                    marginRight: '0.5rem',
-                  }}
-                />
-                {this.state.snackbarMessage}
-              </span>
-            ) : (
-              <span id="message-id">
-                <ErrorIcon
-                  style={{
-                    marginBottom: '-4.5pt',
-                    marginRight: '0.5rem',
-                  }}
-                />
-                {this.state.snackbarMessage}
-              </span>
-            )
-          }
-          action={[
-            <IconButton
-              key="close"
-              aria-label="Close"
-              color="inherit"
-              onClick={() => {
-                this.setState({ isSnackbarOpen: false });
-              }}
-            >
-              <CloseIcon />
-            </IconButton>,
-          ]}
-        />
-      </div>
-    );
-  }
-}
-
-MyProfileActivityList.propTypes = {
-  Membership: PropTypes.shape({
-    ActivityDescription: PropTypes.string.isRequired,
-    ActivityImagePath: PropTypes.string.isRequired,
-    SessionDescription: PropTypes.string.isRequired,
-    ActivityCode: PropTypes.string,
-    Participation: PropTypes.string,
-    ParticipationDescription: PropTypes.string,
-    GroupAdmin: PropTypes.bool,
-  }).isRequired,
+        </Grid>
+        {/* CONTENT TWO (Activity Picture) */}
+        <Grid
+          container
+          xs={4}
+          sm={3}
+          md={3}
+          lg={4}
+          xl={3}
+          className="my-profile-info-card-image"
+          alignItems="center"
+        >
+          <OnlineOnlyLink>
+            <img src={membership.ActivityImagePath} alt="" className={isOnline ? 'active' : ''} />
+          </OnlineOnlyLink>
+        </Grid>
+      </Grid>
+      <Divider />
+    </>
+  );
 };
+
+export default MyProfileActivityList;
