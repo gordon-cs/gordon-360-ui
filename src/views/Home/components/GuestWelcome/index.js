dd1905e607fcdaac4fbd2a74ef29404f21cc30ee
Hudson Finn <Hudson.Finn@gordon.edu>
2021-03-02 16:04:13 -0500

    Working on styling Gordon's logo for the welcome message;

--- src/views/Home/components/GuestWelcome/index.js
+++ src/views/Home/components/GuestWelcome/index.js
@@ -1,12 +1,17 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
-import React  from 'react';
+import { Card, CardMedia, CardContent, Typography } from '@material-ui/core';
+import React from 'react';
 import './guest-welcome.css';
-
+import gordonLogo from './gordon-logo-vertical-white.svg';
 
 const GuestWelcome = () => {
   return (
     <Card>
       <CardContent className='guest-welcome-card'>
+        <CardMedia className='guest-welcome-logo'
+          component='img'
+          alt='GordonLogo'
+          image={gordonLogo}
+        />
         <Typography className='guest-welcome-message'>
           Welcome to the Guest view of Gordon360. Gordon login required for full access.
         </Typography>
