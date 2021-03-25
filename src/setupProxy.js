f65103d71e178d19896df4d392a61e1436d0210a
Evan Platzer <Evan.Platzer@gmail.com>
2021-03-25 07:41:59 -0400

    Point master to prod API

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,12 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = (app) => {
   // @PROD
-  // app.use('/api', createProxyMiddleware({ target: 'https://360Api.gordon.edu/', changeOrigin: true }));
-  // app.use('/token', createProxyMiddleware({ target: 'https://360Api.gordon.edu/', changeOrigin: true }));
+  app.use('/api', createProxyMiddleware({ target: 'https://360Api.gordon.edu/', changeOrigin: true }));
+  app.use('/token', createProxyMiddleware({ target: 'https://360Api.gordon.edu/', changeOrigin: true }));
 
   // @TRAIN
-  app.use('/api', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
-  app.use('/token', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
+  // app.use('/api', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
+  // app.use('/token', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
 
   // @LOCALHOST
   // app.use('/api', createProxyMiddleware({ target: 'http://localhost:2626/', changeOrigin: true }));
