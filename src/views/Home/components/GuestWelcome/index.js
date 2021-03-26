918bbc2697edc9f4c320491d621f8a5ee1109d65
Evan Platzer <Evan.Platzer@gmail.com>
2021-03-26 16:17:29 -0400

    Clarify wording of welcome message

--- src/views/Home/components/GuestWelcome/index.js
+++ src/views/Home/components/GuestWelcome/index.js
@@ -17,8 +17,7 @@ const GuestWelcome = ({ onLogIn }) => {
           <CardHeader title="Welcome to Gordon360!" />
           <CardContent>
             <Typography>
-              As a guest, you have access to a limited view of the site. Login is required for full
-              access.
+              As a guest, you have access to a limited view of the site. Login for full access.
             </Typography>
           </CardContent>
         </Card>
