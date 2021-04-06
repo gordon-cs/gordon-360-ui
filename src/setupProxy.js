5c7c484b354b24206edc1d6e49a89144cf4e3224
Christian Kunis <ckunis98@gmail.com>
2021-04-06 11:10:55 -0400

    Configure for testing with API

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,10 +6,10 @@ module.exports = (app) => {
   // app.use('/token', createProxyMiddleware({ target: 'https://360Api.gordon.edu/', changeOrigin: true }));
 
   // @TRAIN
-  app.use('/api', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
-  app.use('/token', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
+  // app.use('/api', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
+  // app.use('/token', createProxyMiddleware({ target: 'https://360ApiTrain.gordon.edu/', changeOrigin: true }));
 
   // @LOCALHOST
-  // app.use('/api', createProxyMiddleware({ target: 'http://localhost:2477/', changeOrigin: true }));
-  // app.use('/token', createProxyMiddleware({ target: 'http://localhost:2477/', changeOrigin: true }));
+  app.use('/api', createProxyMiddleware({ target: 'http://localhost:2477/', changeOrigin: true }));
+  app.use('/token', createProxyMiddleware({ target: 'http://localhost:2477/', changeOrigin: true }));
 };
