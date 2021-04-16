3814d377b53e117699b7f8d974e715062dc98d13
Evan Platzer <Evan.Platzer@gmail.com>
2021-04-16 15:20:11 -0400

    Remove commented-out old code from MyProfile

--- src/views/MyProfile/index.js
+++ src/views/MyProfile/index.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import GordonLoader from 'components/Loader';
-// import GordonSchedulePanel from 'components/Profile/SchedulePanel';
-// import Identification from 'components/Profile/Identification';
-// import MembershipsList from 'components/Profile/MembershipsList';
-// import OfficeInfoList from 'components/Profile/OfficeInfoList';
-// import PersonalInfoList from 'components/Profile/PersonalInfoList';
 import user from 'services/user';
-// import VictoryPromiseDisplay from '../../components/Profile/VictoryPromiseDisplay/index.js';
 import Profile from 'components/Profile';
 import './myProfile.css';
 
@@ -16,9 +10,6 @@ const MyProfile = ({ authentication }) => {
   const [loading, setLoading] = useState(true);
   const [profile, setProfile] = useState(null);
 
-  /**
-   * Loads the user's profile info only once (at start)
-   */
   useEffect(() => {
     async function loadProfile() {
       setLoading(true);
@@ -43,37 +34,6 @@ const MyProfile = ({ authentication }) => {
       return <GordonLoader />;
     } else {
       return <Profile profile={profile} myProf />;
-      // (
-      //   <Grid container justify="center" spacing={2}>
-      //     <Grid
-      //       item
-      //       xs={12}
-      //       md={profile?.PersonType?.includes('stu') ? 8 : 12}
-      //       lg={profile?.PersonType?.includes('stu') ? 6 : 10}
-      //     >
-      //       <Identification profile={profile} network={network} myProf={true} />
-      //     </Grid>
-
-      //     {profile?.PersonType?.includes('stu') && (
-      //       <Grid item xs={12} md={4}>
-      //         <VictoryPromiseDisplay network={network} />
-      //       </Grid>
-      //     )}
-
-      //     <Grid item xs={12} lg={10}>
-      //       <GordonSchedulePanel profile={profile} myProf={true} network={network} />
-      //     </Grid>
-
-      //     <Grid item xs={12} lg={5}>
-      //       <OfficeInfoList profile={profile} />
-      //       <PersonalInfoList profile={profile} myProf={true} network={network} />
-      //     </Grid>
-
-      //     <Grid item xs={12} lg={5}>
-      //       <MembershipsList user={profile?.ID} myProf={true} />
-      //     </Grid>
-      //   </Grid>
-      // );
     }
   }
   // NOT AUTHENTICATED
