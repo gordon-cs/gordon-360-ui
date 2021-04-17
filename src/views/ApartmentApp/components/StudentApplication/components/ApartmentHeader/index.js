67ba62fbec44882e6044358f5f8fd48723e5523a
Christian Kunis <ckunis98@gmail.com>
2021-04-17 01:52:33 -0400

    Simplified logic of header text

--- src/views/ApartmentApp/components/StudentApplication/components/ApartmentHeader/index.js
+++ src/views/ApartmentApp/components/StudentApplication/components/ApartmentHeader/index.js
@@ -8,83 +8,47 @@ const ApartmentHeader = ({
   userProfile,
   onShowApplication,
 }) => {
-  const newApplicationContent = (
-    <React.Fragment>
-      <Grid item xs={6} sm={8}>
-        <Typography variant="body1">
-          You are not on any applications for the current semester.
-          <br />
-          Click the button to begin a new application.
-        </Typography>
-      </Grid>
-      <Grid item xs={6} sm={4}>
-        <Button
-          variant="contained"
-          onClick={onShowApplication}
-          color="primary"
-          fullWidth
-          disabled={applicationCardsOpen}
-        >
-          Create a new application
-        </Button>
-      </Grid>
-    </React.Fragment>
-  );
-
-  const editApplicationContent = (
-    <React.Fragment>
-      <Grid item xs={6} sm={8}>
-        <Typography variant="body1">
-          You are on an existing application for this semester.
-        </Typography>
-      </Grid>
-      <Grid item xs={6} sm={4}>
-        <Button
-          variant="contained"
-          onClick={onShowApplication}
-          color="primary"
-          fullWidth
-          disabled={applicationCardsOpen}
-        >
-          Edit your application
-        </Button>
-      </Grid>
-    </React.Fragment>
-  );
-
-  const viewApplicationContent = (
-    <React.Fragment>
-      <Grid item xs={6} sm={8}>
-        <Typography variant="body1">
-          You are on an existing application for this semester, but you are not assigned as the
-          editor.
-          <br />
-          Only the application editor may edit the application.
-        </Typography>
-      </Grid>
-      <Grid item xs={6} sm={4}>
-        <Button
-          variant="contained"
-          onClick={onShowApplication}
-          color="primary"
-          fullWidth
-          disabled={applicationCardsOpen}
-        >
-          View your application
-        </Button>
-      </Grid>
-    </React.Fragment>
-  );
+  const headerContent = !applicationID
+    ? {
+        primaryText: 'You are not on any applications for the current semester.',
+        secondaryText: 'Click the button to begin a new application.',
+        buttonLabel: 'Create a new application',
+      }
+    : userProfile.AD_Username === editorUsername
+    ? {
+        primaryText: 'You are on an existing application for this semester.',
+        secondaryText: "Click the button to edit your group's application.",
+        buttonLabel: 'Edit your application',
+      }
+    : {
+        primaryText:
+          'You are on an existing application for this semester, but you are not assigned as the editor.',
+        secondaryText: 'Only the application editor may edit the application.',
+        buttonLabel: 'View your application',
+      };
 
   return (
     <Card>
       <CardContent>
         <Grid container direction="row" justify="flex-end" spacing={2}>
-          {!applicationID
-            ? newApplicationContent
-            : userProfile.AD_Username === editorUsername
-            ? editApplicationContent
-            : viewApplicationContent}
+          <Grid item xs={6} sm={8}>
+            <Typography variant="body1">
+              {headerContent.primaryText}
+              <br />
+              {headerContent.secondaryText}
+            </Typography>
+          </Grid>
+          <Grid item xs={6} sm={4}>
+            <Button
+              variant="contained"
+              onClick={onShowApplication}
+              color="primary"
+              fullWidth
+              disabled={applicationCardsOpen}
+            >
+              {headerContent.buttonLabel}
+            </Button>
+          </Grid>
         </Grid>
       </CardContent>
     </Card>
