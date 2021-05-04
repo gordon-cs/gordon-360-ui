4da35e931e3484a11d88f543a3d70cd5a896bfa5
Bennett Forkner <bennett.forkner@gordon.edu>
2021-05-04 18:01:32 -0400

    change content according to @ChrisImagineer 's specification on #1110; Rmoved and consolidated styling where needed.

--- src/views/Help/index.js
+++ src/views/Help/index.js
@@ -20,13 +20,13 @@ export default class Help extends Component {
                 <Card>
                   <CardHeader
                     className="help-header"
-                    title={`LOGIN INSTRUCTIONS`}
+                    title="LOGIN INSTRUCTIONS"
                     titleTypographyProps={{ variant: 'body1' }}
                   />
                   <CardContent>
-                    <Typography variant="body1" component="ul" style={{ textAlign: 'start' }}>
-                      <li>Please use your 'firstname.lastname' username or your Gordon email address</li>
-                      <li>Use your current Gordon College account password here as well.</li>
+                    <Typography variant="body1" component="ul">
+                      <li>Username: 'firstname.lastname' or your Gordon email address</li>
+                      <li>Password: Your Gordon College account password</li>
                     </Typography>
                   </CardContent>
                 </Card>
@@ -34,27 +34,34 @@ export default class Help extends Component {
                 <Card>
                   <CardHeader
                     className="help-header"
-                    title={`SITE NAVIGATION`}
+                    title="SITE NAVIGATION"
                     titleTypographyProps={{ variant: 'body1' }}
                   />
                   <CardContent>
-                    <Typography variant="body1" component="ul" style={{ textAlign: 'start' }}>
+                    <Typography variant="body1" component="ul">
                       <li>
-                        <strong>Home</strong> &ndash; Contains a chart displaying the number of days left in
-                        the current semester, alongside the number of Christian Life &amp; Worship Credits
-                        you have earned this semester. This chart can be clicked to view the CL&amp;W events
-                        you have attended. You can also view your pending requests to join Involvements, as
-                        well as requests to join your club, if you are a leader.
+                        <strong>Home</strong> &ndash; Contains various tiles with charts or information.
+                        <Typography variant="body1" component="ul">
+                          <li>
+                            <strong>Student:</strong> Includes your Dining “Meal Wheel’ tracker synced to days remaining in the
+                            semester and your Christian Life & Worship (CL&W) credits. You can click on the
+                            CL&W tracker to see which events are recorded for you.
+                          </li>
+                          <li>
+                            <strong>Faculty/Staff:</strong> Includes days remaining wheel and a separate Dining Dollars tracker.
+                            Also view your pending join requests for Involvements you’ve requested, as well
+                            as the requests that are pending for you approve if you are a Group Leader/Advisor.
+                          </li>
+                        </Typography>
                       </li>
                       <li>
-                        <strong>Involvements</strong> &ndash; Is a list of all the Current Involvements for this
-                        Academic Session. Pick the Academic Session in the drop down menu to view
-                        Involvements from other times of year.
+                        <strong>Involvements</strong> &ndash; A list of available groups, clubs and organizations for the current
+                        Academic Session. Change Academic Sessions in the drop down menu to view involvements in other sessions.
                       </li>
                       <li>
-                        <strong>Events</strong> &ndash; Displays a table of upcoming events on campus. You
-                        can search for specific events by name, location, or time. You can also filter by
-                        event type including those the offer CL&amp;W credit.
+                        <strong>Events</strong> &ndash; A live list of events drawn from the campus master calendar including
+                        CL&W events. You can search by event name, location and/or time. Use the filters to narrow your
+                        search (including CL&W events if you are looking for more).
                       </li>
                       <li>
                         <strong>People</strong> &ndash; Allows searching the campus community with more
@@ -62,18 +69,27 @@ export default class Help extends Component {
                         faculty by name or hall and by academic, home, and office info.
                       </li>
                       <li>
-                        <strong>Search Bar</strong> &ndash; Allows basic searching for Gordon students,
-                        faculty, and staff by name. (Just search by first or last name, don't include
-                        titles like Dr.)
+                        <strong>Quick Search</strong> &ndash; Quick searches by name of Gordon students, faculty and staff.
+                        Just first name or last name is needed.
                       </li>
                       <li>
-                        <strong>My Profile</strong> &ndash; Shows your personal information (such as your Student ID, and any
-                        Involvements you have been a part of) and your co-curricular transcript. This page is accessible from the profile image
-                        on the top right (on Desktop), or in the Navigation Drawer to the left (on Mobile). You
-                        can toggle what information is made visible to other students, as well as update or
-                        hide your profile photo (click on your photo for photo options). Anything with a
-                        lock is permanently private to only be seen by you. There is also an option to
-                        link your personal social media accounts which will be visible to other users.
+                        <strong>My Profile</strong> &ndash; Shows your own personal information – Student ID, current official
+                        ID photo and current personal photo (changeable), current and past involvements, other trackers and
+                        information, and your Experience Transcript (ET). Your ET displays a collection of your Honors and Leadership,
+                        Experience (work/jobs), Service Learning, and your Activities and Clubs in a resume-like format. You can
+                        also add links to your Social Media for others to connect with you in the My Profile photo section.
+                        On Mobile, access My Profile through the Navigation Drawer (three-line stack) or on Desktop by clicking your
+                        profile navitar picture on the top right of the navbar. Some settings have toggles to leave information public,
+                        or make it private. To view what others see about you, use the View My Public Profile feature near your photo
+                        on your My Profile view. For students, some key institutional contact information will remain visible to
+                        faculty and staff even if you make it private for student viewers. If you have a situation that requires
+                        additional privacy or security, please contact CTS,
+                        <a
+                          href='mailto:360@gordon.edu'
+                          style={{ color: gordonColors.primary.cyan }}>
+                          360@gordon.edu
+                        </a>
+                        , or the Registrar.
                       </li>
                     </Typography>
                   </CardContent>
@@ -82,24 +98,24 @@ export default class Help extends Component {
                 <Card>
                   <CardHeader
                     className="help-header"
-                    title={`INVOLVEMENT USER LEVELS`}
+                    title="INVOLVEMENT USER LEVELS"
                     titleTypographyProps={{ variant: 'body1' }}
                   />
                   <CardContent>
-                    <Typography variant="body1" component="ul" style={{ textAlign: 'start' }}>
+                    <Typography variant="body1" component="ul">
                       <li>
                         <strong>Subscriber</strong> &ndash; Subscribers will receive Group emails. The
-                        Involvement will not appear on their Co-Curricular Transcript.
+                        Involvement will not appear on their Experience Transcript.
                       </li>
                       <li>
                         <strong>Member</strong> &ndash; Members appear on the roster and can see other
                         Members and Subscribers (Guests). Members receive group emails and have their role
-                        recorded in Memberships on the Co-Curricular Transcript, with a position title, if added.
+                        recorded in Memberships on the Experience Transcript, with a position title, if added.
                       </li>
                       <li>
                         <strong>Leader</strong> &ndash; Leaders appear on the roster with a title and with all
-                        the same privileges and Members. Leadership appears on Co-Curricular Transcript view
-                        and Official Co-Curricular Transcript, with their position title.
+                        the same privileges and Members. Leadership appears on Experience Transcript view
+                        and Official Experience Transcript, with their position title.
                       </li>
                       <li>
                         <strong>Advisor</strong> &ndash; Faculty or Staff Advisor have the same privileges
@@ -124,11 +140,11 @@ export default class Help extends Component {
                 <Card>
                   <CardHeader
                     className="help-header"
-                    title={`MANAGEMENT & EDITING INVOLVEMENTS`}
+                    title="MANAGEMENT & EDITING INVOLVEMENTS"
                     titleTypographyProps={{ variant: 'body1' }}
                   />
                   <CardContent>
-                    <Typography variant="body1" component="ul" style={{ textAlign: 'start' }}>
+                    <Typography variant="body1" component="ul">
                       <li>
                         <strong>Group Email</strong> &ndash; Leaders/Advisors can email the full roster of
                         Members, plus any Subscribers to their Communications feed. Allows direct connection
@@ -149,9 +165,9 @@ export default class Help extends Component {
                       </li>
                       <li>
                         <strong>Title/Comment</strong> &ndash; Short but descriptive job or role title that
-                        will appear on the Roster and the Co-Curricular Transcript. Helps manage roles in
+                        will appear on the Roster and the Experience Transcript. Helps manage roles in
                         large rosters. Recommended for Leader and Advisors, can be used for anyone. These
-                        are public and appear as a detail for the Official Co-Curricular Transcript!
+                        are public and appear as a detail for the Official Experience Transcript!
                       </li>
                       <li>
                         <strong>Web link</strong> &ndash; Link to an Involvement&apos;s web page for more
@@ -174,11 +190,11 @@ export default class Help extends Component {
                 <Card>
                   <CardHeader
                     className="help-header"
-                    title={`ISSUES & TROUBLESHOOTING`}
+                    title="ISSUES & TROUBLESHOOTING"
                     titleTypographyProps={{ variant: 'body1' }}
                   />
                   <CardContent>
-                    <Typography variant="body1" component="ul" style={{ textAlign: 'start' }}>
+                    <Typography variant="body1" component="ul">
                       <li>
                         <a
                           href="mailto:cts@gordon.edu?Subject=Gordon 360 Bug"
@@ -196,11 +212,11 @@ export default class Help extends Component {
                 <Card>
                   <CardHeader
                     className="help-header"
-                    title={`SUPPORTED PLATFORMS`}
+                    title="SUPPORTED PLATFORMS"
                     titleTypographyProps={{ variant: 'body1' }}
                   />
                   <CardContent>
-                    <Typography variant="body1" component="ul" style={{ textAlign: 'start' }}>
+                    <Typography variant="body1" component="ul">
                       <li>
                         360 has been successfully tested on Windows (Firefox, Chrome), Mac (Safari,
                         Firefox, Chrome), and on Android and iPhone default browsers.
@@ -215,16 +231,16 @@ export default class Help extends Component {
                 <Card>
                   <CardHeader
                     className="help-header"
-                    title={`FAQ`}
+                    title="FAQ"
                     titleTypographyProps={{ variant: 'body1' }}
                   />
                   <CardContent>
-                    <Typography variant="body1" component="ul" style={{ textAlign: 'start' }}>
+                    <Typography variant="body1" component="ul">
                       <li>
                         <strong>Do I have to rejoin an Involvement each semester?</strong> Yes. This allows
-                        each student to control their participation in Involvements for their Co-Curricular
-                        Transcript. Student Leaders and Advisors can ADD members and guests too! Some
-                        may do this to help maintain their team roster.
+                        each student to control their participation in Involvements for their Experience Transcript.
+                        Student Leaders and Advisors can ADD members and guests too! Some may do this to help
+                        maintain their team roster.
                       </li>
                       <li>
                         <strong>
@@ -239,7 +255,7 @@ export default class Help extends Component {
                         <strong>What if I leave an Involvement?</strong> Subscribership and Membership are
                         fluid during an academic session, and are confirmed at the conclusion of a session.
                         You can come and go over the semester, and your Membership will move to your
-                        official Co-Curricular Transcript only at the end of the semester.
+                        official Experience Transcript only at the end of the semester.
                       </li>
                       <li>
                         <strong>What if I am in an Involvement I don&apos;t want to be part of? </strong>
