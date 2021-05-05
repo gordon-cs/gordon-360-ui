0ca1a44bb39c4ec4e3890d06264721f520397306
Christian Kunis <ckunis98@gmail.com>
2021-05-04 21:29:26 -0400

    Remove unused imports

--- src/views/ApartmentApp/components/StudentApplication/components/OffCampusList/index.js
+++ src/views/ApartmentApp/components/StudentApplication/components/OffCampusList/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { Grid, Card, CardHeader, CardContent, List } from '@material-ui/core';
 import OffCampusListItem from './components/OffCampusListItem';
 import goStalk from 'services/goStalk';
