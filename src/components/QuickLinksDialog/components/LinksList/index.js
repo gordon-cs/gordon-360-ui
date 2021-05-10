901155eb56fb37d5e07bf3dcdada00605339cf70
Evan Platzer <Evan.Platzer@gmail.com>
2021-05-10 13:55:49 -0400

    Fix Links Dialog errors

--- src/components/QuickLinksDialog/components/LinksList/index.js
+++ src/components/QuickLinksDialog/components/LinksList/index.js
@@ -1,14 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LinkIcon from '@material-ui/icons/InsertLink';
 
-import {
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-  List,
-  ListItem,
-  Typography,
-} from '@material-ui/core';
+import { ListItemIcon, ListItemText, ListSubheader, List, ListItem } from '@material-ui/core';
 
 import CanvasIcon from './images/Canvas.ico';
 import GordonIcon from './images/GordonFavicon.ico';
@@ -19,108 +12,108 @@ function ListItemLink(props) {
   return <ListItem component="a" {...props} />;
 }
 
-// Currently all the URLs in Links tab are hardcoded in this file
+const GordonLinksList = () => (
+  <>
+    <List
+      component="nav"
+      subheader={
+        <ListSubheader component="div" disableSticky>
+          Academics
+        </ListSubheader>
+      }
+    >
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="https://www.gordon.edu"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <img src={GordonIcon} alt="gordon" width="16" height="16" />
+        </ListItemIcon>
+        <ListItemText primary="Gordon College" />
+      </ListItemLink>
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="https://my.gordon.edu"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <img src={MyGordonIcon} alt="my gordon" width="16" height="16" />
+        </ListItemIcon>
+        <ListItemText primary="My Gordon" />
+      </ListItemLink>
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="https://go.gordon.edu"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <img src={GOIcon} alt="go gordon" width="16" height="16" />
+        </ListItemIcon>
+        <ListItemText primary="Go Gordon" />
+      </ListItemLink>
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="https://canvas.gordon.edu"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <img src={CanvasIcon} alt="canvas" width="16" height="16" />
+        </ListItemIcon>
+        <ListItemText primary="Canvas" />
+      </ListItemLink>
+    </List>
+    <List component="nav" subheader={<ListSubheader component="div">Information</ListSubheader>}>
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="http://athletics.gordon.edu/"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <LinkIcon style={{ fontSize: 16 }} />
+        </ListItemIcon>
+        <ListItemText primary="Fighting Scots" />
+      </ListItemLink>
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="http://stories.gordon.edu/"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <LinkIcon style={{ fontSize: 16 }} />
+        </ListItemIcon>
+        <ListItemText primary="The Bell" />
+      </ListItemLink>
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="https://www.gordon.edu/titleix"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <LinkIcon style={{ fontSize: 16 }} />
+        </ListItemIcon>
+        <ListItemText primary="Sexual Discrimination and Harassment" />
+      </ListItemLink>
+      <ListItemLink
+        target="_blank"
+        rel="noopener"
+        href="https://www.gordon.edu/map"
+        className="gc360-text-link"
+      >
+        <ListItemIcon>
+          <LinkIcon style={{ fontSize: 16 }} />
+        </ListItemIcon>
+        <ListItemText primary="Gordon College Maps" />
+      </ListItemLink>
+    </List>
+  </>
+);
 
-export default class GordonLinksList extends Component {
-  render() {
-    return (
-      <Typography>
-        <List component="nav" subheader={<ListSubheader component="div">Academics</ListSubheader>}>
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="https://www.gordon.edu"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <img className="gordon" src={GordonIcon} alt="gordon" width="16" height="16" />
-            </ListItemIcon>
-            <ListItemText primary="Gordon College" />
-          </ListItemLink>
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="https://my.gordon.edu"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <img className="mygordon" src={MyGordonIcon} alt="my gordon" width="16" height="16" />
-            </ListItemIcon>
-            <ListItemText primary="My Gordon" />
-          </ListItemLink>
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="https://go.gordon.edu"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <img className="gogordon" src={GOIcon} alt="go gordon" width="16" height="16" />
-            </ListItemIcon>
-            <ListItemText primary="Go Gordon" />
-          </ListItemLink>
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="https://canvas.gordon.edu"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <img className="canvas" src={CanvasIcon} alt="canvas" width="16" height="16" />
-            </ListItemIcon>
-            <ListItemText primary="Canvas" />
-          </ListItemLink>
-        </List>
-        <List
-          component="nav"
-          subheader={<ListSubheader component="div">Information</ListSubheader>}
-        >
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="http://athletics.gordon.edu/"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <LinkIcon style={{ fontSize: 16 }} />
-            </ListItemIcon>
-            <ListItemText primary="Fighting Scots" />
-          </ListItemLink>
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="http://stories.gordon.edu/"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <LinkIcon style={{ fontSize: 16 }} />
-            </ListItemIcon>
-            <ListItemText primary="The Bell" />
-          </ListItemLink>
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="https://www.gordon.edu/titleix"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <LinkIcon style={{ fontSize: 16 }} />
-            </ListItemIcon>
-            <ListItemText primary="Sexual Discrimination and Harassment" />
-          </ListItemLink>
-          <ListItemLink
-            target="_blank"
-            rel="noopener"
-            href="https://www.gordon.edu/map"
-            className="gc360-text-link"
-          >
-            <ListItemIcon>
-              <LinkIcon style={{ fontSize: 16 }} />
-            </ListItemIcon>
-            <ListItemText primary="Gordon College Maps" />
-          </ListItemLink>
-        </List>
-      </Typography>
-    );
-  }
-}
+export default GordonLinksList;
