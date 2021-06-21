031bd3c6ee50d898313c60719275d48c3e70a09d
kevin.chu <kevin.chu@gordon.edu>
2021-06-21 14:01:57 -0400

    Fixed small syntax problem

--- src/components/Profile/components/EmergencyInfoList/index.js
+++ src/components/Profile/components/EmergencyInfoList/index.js
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './index.css';
 import ProfileInfoListItem from '../ProfileInfoListItem';
 import user from 'services/user';
 
 import { Typography, Grid, Card, CardHeader, CardContent, List } from '@material-ui/core';
-import { useEffect } from 'react';
 
 const formatPhone = (phone) => {
   let tele = String(phone);
