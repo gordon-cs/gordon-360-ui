4ec68f0fd62121c4f78d574b80117cb7be943a63
roddy3 <roddy.ngolomingi@gordon.edu>
2021-07-01 11:54:25 -0400

    fixed some built-in erros

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import GordonLoader from 'components/Loader';
-import WellnessQuestion from 'components/WellnessQuestion';
+//import WellnessQuestion from 'components/WellnessQuestion';
 import GuestWelcome from './components/GuestWelcome';
 import Carousel from './components/Carousel';
 import CLWCreditsDaysLeft from './components/CLWCreditsDaysLeft';
@@ -8,43 +8,42 @@ import DaysLeft from './components/DaysLeft';
 import DiningBalance from './components/DiningBalance';
 import NewsCard from './components/NewsCard';
 import user from 'services/user';
-import wellness from 'services/wellness';
-import storage from 'services/storage';
+// import wellness from 'services/wellness';
+// import storage from 'services/storage';
 import './home.css';
 import { Grid } from '@material-ui/core';
-
 const Home = ({ authentication, onLogIn }) => {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(authentication);
   const [personType, setPersonType] = useState(null);
-  const [networkStatus, setNetworkStatus] = useState('online');
-  const [hasAnswered, setHasAnswered] = useState(null);
+  // const [networkStatus, setNetworkStatus] = useState('online');
+  // const [hasAnswered, setHasAnswered] = useState(null);
 
-  useEffect(() => {
-    // Retrieve network status from local storage or default to online
-    try {
-      setNetworkStatus(storage.get('network-status'));
-    } catch (error) {
-      setNetworkStatus('online');
-    }
+  // useEffect(() => {
+  //   // Retrieve network status from local storage or default to online
+  //   try {
+  //     setNetworkStatus(storage.get('network-status'));
+  //   } catch (error) {
+  //     setNetworkStatus('online');
+  //   }
 
-    /* Used to re-render the page when the network connection changes.
-     * The origin of the message is checked to prevent cross-site scripting attacks
-     */
-    window.addEventListener('message', (event) => {
-      setNetworkStatus((prevStatus) => {
-        if (
-          event.origin === window.location.origin &&
-          (event.data === 'online' || event.data === 'offline')
-        ) {
-          return event.data;
-        }
-        return prevStatus;
-      });
-    });
+  //   /* Used to re-render the page when the network connection changes.
+  //    * The origin of the message is checked to prevent cross-site scripting attacks
+  //    */
+  //   window.addEventListener('message', (event) => {
+  //     setNetworkStatus((prevStatus) => {
+  //       if (
+  //         event.origin === window.location.origin &&
+  //         (event.data === 'online' || event.data === 'offline')
+  //       ) {
+  //         return event.data;
+  //       }
+  //       return prevStatus;
+  //     });
+  //   });
 
-    return () => window.removeEventListener('message', () => {});
-  }, []);
+  //   return () => window.removeEventListener('message', () => {});
+  // }, []);
 
   useEffect(() => {
     if (authentication) {
@@ -53,7 +52,7 @@ const Home = ({ authentication, onLogIn }) => {
     } else {
       // Clear out component's person-specific state when authentication becomes false
       // (i.e. user logs out) so that it isn't preserved falsely for the next user
-      setHasAnswered(null);
+      // setHasAnswered(null);
       setPersonType(null);
       setIsAuthenticated(false);
       setLoading(false);
@@ -62,12 +61,12 @@ const Home = ({ authentication, onLogIn }) => {
 
   const loadPage = async () => {
     setLoading(true);
-    const [{ PersonType }, { IsValid }] = await Promise.all([
+    const [{ PersonType } /*, { IsValid }*/] = await Promise.all([
       user.getProfileInfo(),
-      wellness.getStatus(),
+      /*wellness.getStatus(),*/
     ]);
     setPersonType(PersonType);
-    setHasAnswered(IsValid);
+    // setHasAnswered(IsValid);
     setLoading(false);
   };
 
