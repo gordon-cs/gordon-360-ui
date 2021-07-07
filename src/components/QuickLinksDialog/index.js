552813c27951cddc0f7f17eac784516b7d6dca0a
Hyungyu Park <Hyungyu.Park@gordon.edu>
2021-07-07 12:54:27 -0400

    Functional componentgit statusgit statusgit statusgit status

--- src/components/QuickLinksDialog/index.js
+++ src/components/QuickLinksDialog/index.js
@@ -1,45 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import GordonLinksList from './components/LinksList';
 import GordonDialogBox from 'components/GordonDialogBox/index';
 
-export default class GordonQuickLinksDialog extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleLinkClickOpen = this.handleLinkClickOpen.bind(this);
-    this.handleLinkClose = this.handleLinkClose.bind(this);
-
-    this.state = {
-      linkopen: false,
-    };
-  }
-
-  handleLinkClickOpen = () => {
-    this.setState({
-      linkopen: true,
-    });
-  };
-
-  handleLinkClose = () => {
-    this.setState({ linkopen: false });
-  };
-
-  render() {
-    return (
-      <GordonDialogBox
-        onClose={this.props.handleLinkClose}
-        aria-labelledby="useful-links"
-        open={this.props.linkopen}
-        title="Useful Links"
-      >
-        <GordonLinksList onClose={this.props.handleLinkClose} />
-      </GordonDialogBox>
-    );
-  }
-}
+const GordonQuickLinksDialog = (props) => {
+  return (
+    <GordonDialogBox
+      onClose={props.handleLinkClose}
+      aria-labelledby="useful-links"
+      open={props.linkopen}
+      title="Useful Links"
+    >
+      <GordonLinksList onClose={props.handleLinkClose} />
+    </GordonDialogBox>
+  );
+};
 
 GordonQuickLinksDialog.propTypes = {
   handleLinkClickOpen: PropTypes.func.isRequired,
   handleLinkClose: PropTypes.func.isRequired,
+  linkopen: PropTypes.bool,
 };
+
+export default GordonQuickLinksDialog;
