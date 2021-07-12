032e49e5cdfe65345a4ad919ede406e9f660c699
Cameron Abbot <cpabbot1@gmail.com>
2021-07-12 08:54:09 -0400

    regenerated package-lock and updated theme.js import

--- src/components/Profile/components/PersonalInfoList/index.js
+++ src/components/Profile/components/PersonalInfoList/index.js
@@ -189,6 +189,7 @@ const PersonalInfoList = ({
   ) : null;
 
   const cliftonStrengths = CliftonStrengths ? (
+    // <ProfileInfoListItem title="Clifton Strengths:" contentText={<b>test</b>} />
     <ProfileInfoListItem title="Clifton Strengths:" contentText={CliftonStrengths.join(', ')} />
   ) : null;
 
