8049ea8a96d64c832e325bcaea42bee7619a53be
Josh Peters <60206522+josh-peters-2357@users.noreply.github.com>
2021-07-12 09:21:57 -0400

    Update src/views/PeopleSearch/components/PeopleSearchResult/index.js
    Remove console log

--- src/views/PeopleSearch/components/PeopleSearchResult/index.js
+++ src/views/PeopleSearch/components/PeopleSearchResult/index.js
@@ -48,7 +48,6 @@ const PeopleSearchResultBlock = ({ Person, size, lazyImages, inViewport }) => {
   function displayAvatar() {
     console.log(inViewport);
     if (inViewport && !hasBeenRun) {
-      console.log('hey');
       loadAvatar();
     }
     return avatar;
