13ab1b025a3563e1371f9dfc202e16690d5b96b9
Evan Platzer <evan.platzer@gmail.com>
2021-09-23 13:07:54 -0400

    Migrate cmsService to TypeScript

--- src/services/cms.ts
+++ src/services/cms.ts
@@ -1,32 +1,19 @@
-/**
- * CMS
- *
- * @module cms
- */
-
 import http from './http';
 
-/**
- * @global
- * @typedef Slide
- * @property {string} ImagePath Link to image
- * @property {string} AltTag Alternative tag
- * @property {boolean} HasCaption Whether image has a caption
- * @property {string} Title Title
- * @property {string} SubTitle Subtitle
- * @property {string} Action Action
- * @property {string} ActionLink Link of action
- * @property {number} Width Image width
- * @property {number} Height Image height
- * @property {number} SortOrder Order of image in slides
- */
+type Slide = {
+  ImagePath: string;
+  AltTag: string;
+  HasCaption: boolean;
+  Title: string;
+  SubTitle: string;
+  Action: string;
+  ActionLink: string;
+  Width: number;
+  Height: number;
+  SortOrder: number;
+};
 
-/**
- * Get slides
- *
- * @returns {Promise.<Slide[]>} List of slides
- */
-const getSlides = () => http.get('cms/slider');
+const getSlides = (): Promise<Slide[]> => http.get('cms/slider');
 
 const cmsService = {
   getSlides,
