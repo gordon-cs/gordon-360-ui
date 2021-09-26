8e0b6af1a1d9fdc84cd9530699dc3ddadefa43f7
Evan Platzer <evan.platzer@gmail.com>
2021-09-26 12:39:54 -0400

    Migrate storageUtils to TypeScript

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,53 +1,38 @@
-/**
- * Store and retrieve app data locally
- *
- * @description With app storage abstracted in this service, it is possible to change the underlying
- * storage driver from the current `localStorage` to a more featureful storage driver like
- * `IndexedDB` without needing to update any code that depends on this service.
- * @module storage
- */
-
-/**
- * Store a value locally
- *
- * @param {string} key Where to store the value
- * @param {any} value Data to store
- */
-const store = (key, value) => {
+const store = (key: string, value: string) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
 /**
  * Get a value stored locally
  *
- * @param {string} key Where value is stored
- * @returns {any} Stored value
+ * @param key The key to lookup the value
  * @throws Will throw an error when the key is not set in local storage
+ * @returns The value stored, if found
  */
-const get = (key) => {
+const get = <T>(key: string): T => {
   const storedValue = localStorage.getItem(key);
   if (storedValue === null) {
     throw new Error(`Key '${key}' does not exist in local storage`);
   }
 
-  let value;
   try {
-    value = JSON.parse(storedValue);
+    return JSON.parse(storedValue);
   } catch (err) {
     throw new Error(`Could not parse value stored at '${key}'`);
   }
-  return value;
 };
 
-/**
- * Remove a value stored locally
- *
- * @param {string} key Where value is stored
- */
-const remove = (key) => {
+const remove = (key: string) => {
   localStorage.removeItem(key);
 };
 
+/**
+ * Store and retrieve app data locally
+ *
+ * @description With app storage abstracted in this service, it is possible to change the underlying
+ * storage driver from the current `localStorage` to a more featureful storage driver like
+ * `IndexedDB` without needing to update any code that depends on this service.
+ */
 const storageUtils = {
   get,
   remove,
