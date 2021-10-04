2d0f2fa7c7e0a9059f562538f269886f85f73c29
Bennett Forkner <bennett.forkner@gordon.edu>
2021-10-04 13:41:39 -0400

    fixing string interpolation

--- src/components/GordonLimitedAvailability/index.js
+++ src/components/GordonLimitedAvailability/index.js
@@ -2,6 +2,7 @@ import { Grid, Card, CardContent, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 const GordonLimitedAvailability = ({ pageName, backToLocation, backToLink, availableTo }) => {
+  let pageNameFormatted = pageName ? `The ${pageName} page` : 'This page';
   return (
     <Grid container justifyContent="center" spacing="16">
       <Grid item xs={12} md={8}>
@@ -15,7 +16,7 @@ const GordonLimitedAvailability = ({ pageName, backToLocation, backToLink, avail
             <br />
             <h1>{pageName + ' Unavailable'}</h1>
             <h4>
-              `${pageName ? `The ${pageName}` : `This`} page is available for ${availableTo} only.`
+              {pageNameFormatted} is available for {availableTo} only.
             </h4>
             <br />
             <br />
