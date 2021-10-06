83736cf448801524e249c80ba8b46f70a2194fe2
Evan Platzer <evan.platzer@gmail.com>
2021-10-06 12:15:35 -0400

    Refactor hooks for type hints, context hook reuse

--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -1,17 +1,13 @@
 import { useEffect, useState } from 'react';
 
 /**
- * @callback setTitle
- * @param {string} title the title to set the document title to
- */
-
-/**
  * Update the document (i.e. browser tab) title.
+ *
  * @description
  * Can be passed an initial value to change it once,
  * and/or can be used to set the title declaratively via the returned `setTitle` function
  * @param {string} initialTitle The title to set the document to
- * @returns {function(string): void}} A function to change the title again
+ * @returns {(title: string) => void} A function to change the title again
  */
 const useDocumentTitle = (initialTitle = '') => {
   const [title, setTitle] = useState(initialTitle || document.title);
--- src/hooks/useNetworkStatus.js
+++ src/hooks/useNetworkStatus.js
@@ -1,21 +1,11 @@
-import { useContext } from 'react';
 import { NetworkContext } from 'contexts/NetworkContext.js';
+import createUseContext from 'hooks/createUseContext';
 
 /**
- * Custom hook to subscribe to network status.
+ * Subscribe to network status.
  *
  * Value is retrieved from local storage initially and updated by service worker firing events
- *
- * Can be used by any functional component under the NetworkContextProvider in App.js
- *
- * @returns {boolean} true if connected to the network, false otherwise.
  */
-const useNetworkStatus = () => {
-  const context = useContext(NetworkContext);
-  if (context === undefined) {
-    throw new Error(`useNetworkStatus must be called within NetworkContextProvider`);
-  }
+const useNetworkStatus = createUseContext('NetworkStatus', NetworkContext);
 
-  return context;
-};
 export default useNetworkStatus;
