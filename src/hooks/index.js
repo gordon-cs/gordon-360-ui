372b6d45ceca8f54c4355604ea7cca1b84cd9bf3
Evan Platzer <evan.platzer@gmail.com>
2021-10-06 16:38:27 -0400

    Add top-level hooks export

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useDocumentTitle } from './useDocumentTitle';
+export { default as useNetworkStatus } from './useNetworkStatus';
+export { default as useUpdateUser } from './useUpdateUser';
+export { default as useUser } from './useUser';
