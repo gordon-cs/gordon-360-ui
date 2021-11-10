abbc91d11ecef9c472657e52b504725d93f46d8d
Evan Platzer <evan.platzer@gmail.com>
2021-11-10 13:28:46 -0500

    Migrate scheduleService to TypeScritp

---
+++ src/services/schedule.ts
@@ -0,0 +1,95 @@
+import moment from 'moment';
+import http from './http';
+
+type CourseSchedule = {
+  CRS_CDE: string;
+  CRS_TITLE: string;
+  BLDG_CDE: string;
+  ROOM_CDE: string;
+  MONDAY_CDE: string;
+  TUESDAY_CDE: string;
+  WEDNESDAY_CDE: string;
+  THURSDAY_CDE: string;
+  FRIDAY_CDE: string;
+  /** A timespan of the format HH:mm:ss, stringified */
+  BEGIN_TIME: string;
+  /** A timespan of the format HH:mm:ss, stringified */
+  END_TIME: string;
+};
+
+type ScheduleEvent = {
+  id: number;
+  title: string;
+  start: Date;
+  end: Date;
+  resourceId: number;
+};
+
+const getCanReadStudentSchedules = async (): Promise<boolean> => {
+  return http.get(`schedule/canreadstudent/`);
+};
+
+const getSchedule = async (username: string = ''): Promise<CourseSchedule[]> => {
+  return await http.get(`schedule/${username}/`);
+};
+
+function getMeetingDays(course: CourseSchedule): number[] {
+  let dayArray = [];
+
+  if (course.MONDAY_CDE === 'M') {
+    dayArray.push(2);
+  }
+  if (course.TUESDAY_CDE === 'T') {
+    dayArray.push(3);
+  }
+  if (course.WEDNESDAY_CDE === 'W') {
+    dayArray.push(4);
+  }
+  if (course.THURSDAY_CDE === 'R') {
+    dayArray.push(5);
+  }
+  if (course.FRIDAY_CDE === 'F') {
+    dayArray.push(6);
+  }
+
+  return dayArray;
+}
+
+function makeScheduleCourses(schedule: CourseSchedule[]): ScheduleEvent[] {
+  const today = moment();
+  const eventArray = [];
+  let eventId = 0;
+  for (let course of schedule) {
+    course.CRS_CDE = course.CRS_CDE.trim();
+    course.CRS_TITLE = course.CRS_TITLE.trim();
+    const beginTime = moment(course.BEGIN_TIME, 'HH:mm:ss')
+      .set('y', today.year())
+      .set('M', today.month())
+      .set('d', today.day());
+    const endTime = moment(course.END_TIME, 'HH:mm:ss')
+      .set('y', today.year())
+      .set('M', today.month())
+      .set('d', today.day());
+    for (const day of getMeetingDays(course)) {
+      const courseEvent = {
+        id: eventId,
+        title: course.CRS_CDE + ' in ' + course.BLDG_CDE + ' ' + course.ROOM_CDE,
+        start: beginTime.toDate(),
+        end: endTime.toDate(),
+        resourceId: day,
+      };
+      eventArray.push(courseEvent);
+      eventId++;
+    }
+    eventId++;
+  }
+  return eventArray;
+}
+
+const scheduleService = {
+  getSchedule,
+  makeScheduleCourses,
+  getCanReadStudentSchedules,
+};
+
+export default scheduleService;
