cb4c957a867a1a0871f5b011b4d7d4a9b0fa4c35
Evan Platzer <evan.platzer@gmail.com>
2021-11-10 13:29:26 -0500

    Fix overfetching can read student schedule

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -31,7 +31,7 @@ const Profile = ({ profile, myProf }) => {
       setCanReadStudentSchedules(await scheduleService.getCanReadStudentSchedules());
     };
     fetchReadStudentSchedulesPermission();
-  });
+  }, []);
 
   return (
     <Grid container justifyContent="center" spacing={2}>
