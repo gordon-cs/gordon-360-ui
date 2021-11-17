95b3fa400cc17858c04e2e1462f05bc41ba366d3
Evan Platzer <evan.platzer@gmail.com>
2021-11-17 15:55:08 -0500

    Serve Slider images locally like a normal website

--- src/services/cms.js
+++ src/services/cms.js
@@ -31,7 +31,7 @@ const getSlides = () =>
   // http.get('cms/slider');
   Promise.resolve(() => [
     {
-      ImagePath: 'https://i.ibb.co/jTYz32x/1-Chapel-20211115.jpg',
+      ImagePath: '/images/CMS/1Chapel_20211115.jpg',
       AltTag: 'Next Chapel',
       HasCaption: false,
       ActionLink: '',
@@ -40,16 +40,16 @@ const getSlides = () =>
       SortOrder: 1,
     },
     {
-      ImagePath: 'https://i.ibb.co/Wff0b6B/2-Dining-Services-20211122.png',
+      ImagePath: '/images/CMS/2AthleticsCongrats_20211115.png',
       AltTag: 'Dining Services',
       HasCaption: false,
-      ActionLink: 'https://gordon.cafebonappetit.com/',
+      ActionLink: '',
       Width: 1500,
       Height: 600,
       SortOrder: 1,
     },
     {
-      ImagePath: 'https://i.ibb.co/gFPC2DY/3-Athletics-Schedule-20211115.jpg',
+      ImagePath: '/images/CMS/3AthleticsSchedule_20211115.jpg',
       AltTag: 'Athletics Schedule',
       HasCaption: false,
       ActionLink: '',
@@ -58,7 +58,7 @@ const getSlides = () =>
       SortOrder: 3,
     },
     {
-      ImagePath: 'https://i.ibb.co/gS8yHW7/5-Theatre-Shorts-20211115.jpg',
+      ImagePath: '/images/CMS/5TheatreShorts_20211115.jpg',
       AltTag: 'Theatre Shorts',
       HasCaption: false,
       ActionLink: '',
