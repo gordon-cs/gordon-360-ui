22bb0757996196a506a0e4be224001a260e438e2
Evan Platzer <evan.platzer@gmail.com>
2022-03-28 13:38:26 -0400

    Use http.postImage to post images

--- src/services/user.ts
+++ src/services/user.ts
@@ -195,39 +195,11 @@ const getImage = (username: string = ''): Promise<ProfileImages> =>
 
 const resetImage = (): Promise<void> => http.post('/profiles/image/reset', '');
 
-const postIDImage = (dataURI: string): Promise<void> => {
-  const blob = dataURItoBlob(dataURI);
-  const fileType = blob.type.replace('image/', '');
-  const imageData = new FormData();
-  imageData.append('canvasImage', blob, 'canvasImage.' + fileType);
-  return http.post('profiles/IDimage', imageData);
-};
+const postIDImage = (imageDataURI: string): Promise<void> =>
+  http.postImage('profiles/IDimage', imageDataURI);
 
-const postImage = (dataURI: string): Promise<void> => {
-  const blob = dataURItoBlob(dataURI);
-  const fileType = blob.type.replace('image/', '');
-  const imageData = new FormData();
-  imageData.append('canvasImage', blob, 'canvasImage.' + fileType);
-  return http.post('profiles/image', imageData);
-};
-
-function dataURItoBlob(dataURI: string) {
-  // convert base64/URLEncoded data component to raw binary data held in a string
-  let byteString;
-  if (dataURI.split(',')[0].indexOf('base64') >= 0) byteString = atob(dataURI.split(',')[1]);
-  else byteString = unescape(dataURI.split(',')[1]);
-
-  // separate out the mime component
-  let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-  // write the bytes of the string to a typed array
-  let ia = new Uint8Array(byteString.length);
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  return new Blob([ia], { type: mimeString });
-}
+const postImage = (imageDataURI: string): Promise<void> =>
+  http.postImage('profiles/image', imageDataURI);
 
 const getChapelCredits = async (): Promise<CLWCredits | null> => {
   const profile = await getProfile();
