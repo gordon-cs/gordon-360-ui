53b978794f59558a680534bb80c5f86e0cf5aa25
Evan Platzer <evan.platzer@gmail.com>
2022-05-12 17:24:27 -0400

    Implement useDebounce hook to debounce values
    This is especially useful in reactive text fields where we want to update the text field as soon as the user types but we only want to perform some action (e.g. an expensive API request) once per typing interaction.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useAuthGroups } from './useAuthGroups';
+export { default as useDebounce } from './useDebounce';
 export { default as useDocumentTitle } from './useDocumentTitle';
 export { default as useNetworkStatus } from './useNetworkStatus';
 export { default as useUser } from './useUser';
