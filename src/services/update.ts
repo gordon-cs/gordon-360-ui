430f7f2e41adec59f369db625b3c347adee2e2b8
amos-cha <amos.cha98@gmail.com>
2022-06-03 10:59:52 -0400

    removed console.log

--- src/services/update.ts
+++ src/services/update.ts
@@ -1,7 +1,6 @@
 import http from './http';
 
 const requestInfoUpdate = async ( email_content: string ) => {
-  console.log(email_content);
   http.post('profiles/updateRequest/', email_content);
 };
 
