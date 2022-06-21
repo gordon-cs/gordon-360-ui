bce687c4c7694ad9b29e0e7fae378adc34342620
amos-cha <amos.cha98@gmail.com>
2022-06-21 12:21:12 -0400

    somehow the carousel got commented out?

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -47,7 +47,7 @@ const Home = () => {
     return (
       <Grid container justifyContent="center" spacing={2}>
         <Grid item xs={12} md={10}>
-          {/* <Carousel /> */}
+          <Carousel />
         </Grid>
         <Grid item xs={12} md={5}>
           {doughnut}
