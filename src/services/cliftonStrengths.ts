c483f5a59466ad11aa73482c7eb004701f2dd838
Bennett F Forkner <43764277+bennettforkner@users.noreply.github.com>
2022-09-09 09:56:14 -0400

    add method for upload

--- src/services/cliftonStrengths.ts
+++ src/services/cliftonStrengths.ts
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import http from './http';
 
 type CliftonStrengthName =
@@ -154,6 +155,23 @@ export type CliftonStrengths = {
   Private: boolean;
 };
 
+type CliftonStrengthUpload = {
+  Email: string;
+  DateCompleted: DateTime;
+  Theme1: string;
+  Theme2: string;
+  Theme3: string;
+  Theme4: string;
+  Theme5: string;
+  AccessCode: string;
+};
+
+type CliftonStrengthUploadReturn = {
+  Email: string;
+  AccessCode: string;
+  RowState: string;
+};
+
 const strengthDetails = (name: CliftonStrengthName): CliftonStrength => {
   const category = categoryPerStrengthName(name);
   return {
@@ -180,9 +198,14 @@ const getCliftonStrengths = async (username: string): Promise<CliftonStrengths |
 
 const togglePrivacy = async (): Promise<boolean> => http.get<boolean>(`profiles/clifton/privacy`);
 
+const postCliftonStrengths = async (
+  cs: CliftonStrengthUpload[],
+): Promise<CliftonStrengthUploadReturn> => http.post('cliftonstrengths', cs);
+
 const CliftonStrengthsService = {
   getCliftonStrengths,
   togglePrivacy,
+  postCliftonStrengths,
 };
 
 export default CliftonStrengthsService;
