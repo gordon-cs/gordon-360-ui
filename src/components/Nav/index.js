12f6dd3c12746e71e36549cf1fb120fb25606b71
Evan Platzer <evan.platzer@gmail.com>
2022-09-29 06:05:09 -0400

    MUI Upgrade: Remove deprecated `Hidden` component
    Also, simplify nav drawer styles. Many of those styles were cruft left over from an old, defunct design.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,47 +1,26 @@
-import { Divider, Drawer, Hidden } from '@mui/material';
+import { Divider, Drawer } from '@mui/material';
 import GordonNavAvatar from './components/NavAvatar';
 import GordonNavLinks from './components/NavLinks';
 import styles from './Nav.module.css';
 
-const GordonNav = ({ onDrawerToggle, drawerOpen }) => {
-  const drawer = (
-    <>
+const GordonNav = ({ onDrawerToggle, drawerOpen }) => (
+  <section className={styles.gordon_nav}>
+    <Drawer
+      variant="temporary"
+      open={drawerOpen}
+      classes={{
+        paper: styles.gordon_nav_drawer,
+      }}
+      onClose={onDrawerToggle}
+      ModalProps={{
+        keepMounted: true, // Better open performance on mobile.
+      }}
+    >
       <GordonNavAvatar onLinkClick={onDrawerToggle} />
       <Divider />
       <GordonNavLinks onLinkClick={onDrawerToggle} />
-    </>
-  );
-
-  return (
-    <section className={styles.gordon_nav}>
-      <Hidden mdUp>
-        <Drawer
-          variant="temporary"
-          open={drawerOpen}
-          classes={{
-            paper: styles.gordon_nav_drawer,
-          }}
-          onClose={onDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Hidden>
-      <Hidden lgDown implementation="css">
-        <Drawer
-          variant="permanent"
-          open
-          classes={{
-            paper: styles.drawer,
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Hidden>
-    </section>
-  );
-};
+    </Drawer>
+  </section>
+);
 
 export default GordonNav;
