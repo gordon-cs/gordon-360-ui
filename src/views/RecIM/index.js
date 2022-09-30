895f0139bebe561ee1ff8c903a96d416186ca0d4
Cameron Abbot <cpabbot1@gmail.com>
2022-09-30 14:17:24 -0400

    added unauthorized message

--- src/views/RecIM/index.js
+++ src/views/RecIM/index.js
@@ -1,6 +1,7 @@
 import { Grid, Typography } from '@material-ui/core/';
 import { useUser } from 'hooks';
 import GordonLoader from 'components/Loader';
+import GordonUnauthorized from 'components/GordonUnauthorized';
 
 const RecIM = () => {
   const { profile, loading } = useUser();
@@ -17,6 +18,9 @@ const RecIM = () => {
         <Typography variant="subtitle1">Current UserID: {profile.ID}</Typography>
       </Grid>
     );
+  } else {
+    // The user is not logged in
+    return <GordonUnauthorized feature={'the Rec-IM page'} />;
   }
 };
 export default RecIM;
