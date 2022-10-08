01a23161ed10e35fb4b47baaeb0566e5a0e47345
Amos Cha <Amos.Cha@gordon.edu>
2022-10-08 19:44:28 -0400

    Add service request to leagueForm, Adjustments based on @cpabbot's suggestions

--- src/views/RecIM/views/team/index.js
+++ src/views/RecIM/views/team/index.js
@@ -2,7 +2,11 @@ import { useParams } from 'react-router';
 
 const Team = () => {
   let { leagueId, teamId } = useParams();
-  return <p>Here with league id {leagueId} team id {teamId}</p>;
+  return (
+    <p>
+      Here with league id {leagueId} team id {teamId}
+    </p>
+  );
 };
 
 export default Team;
