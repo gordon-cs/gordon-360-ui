299551e3f3a2370301eff69b75af2852b1e36ed9
Cameron Abbot <cpabbot1@gmail.com>
2022-10-31 16:04:53 -0400

    Re-added very much needed proxy in Dev mode

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_URL,
+      changeOrigin: true,
+    }),
+  );
+};
