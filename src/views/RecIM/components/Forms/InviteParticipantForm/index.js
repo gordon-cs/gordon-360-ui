d1e2d03a739c8d6de8b01437693af06488dd6cbf
Silas White <silas.j.white@gmail.com>
2023-01-05 12:24:09 -0500

    Error checking for adding multiple people to invite list

--- src/views/RecIM/components/Forms/InviteParticipantForm/index.js
+++ src/views/RecIM/components/Forms/InviteParticipantForm/index.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import GordonDialogBox from 'components/GordonDialogBox';
 import { ParticipantList } from './../../List';
 import GordonQuickSearch from 'components/Header/components/QuickSearch';
+import GordonSnackbar from 'components/Snackbar';
+import { useCallback } from 'react';
 
 const InviteParticipantForm = ({
   closeWithSnackbar,
@@ -11,12 +13,27 @@ const InviteParticipantForm = ({
 }) => {
   const [disableUpdateButton, setDisableUpdateButton] = useState(true);
   const [inviteList, setInviteList] = useState([]);
+  const [snackbar, setSnackbar] = useState({ message: '', severity: null, open: false });
+
+  const createSnackbar = useCallback((message, severity) => {
+    setSnackbar({ message, severity, open: true });
+  }, []);
 
   useEffect(() => {
     setDisableUpdateButton(!inviteList || !inviteList.length);
   }, [inviteList]);
 
   const onSearchSubmit = (username) => {
+    let containsUsername = false;
+    inviteList.forEach((value) => {
+      if (value.username === username) containsUsername = true;
+    });
+
+    if (containsUsername) {
+      createSnackbar('Participant already in list', 'error');
+      return;
+    }
+
     setInviteList([...inviteList, { username: username }]);
   };
 
@@ -44,30 +61,38 @@ const InviteParticipantForm = ({
   };
 
   return (
-    <GordonDialogBox
-      open={openInviteParticipantForm}
-      title="Invite Teammates"
-      fullWidth
-      maxWidth="sm"
-      buttonClicked={handleSubmit}
-      isButtonDisabled={disableUpdateButton}
-      buttonName="Send Invites"
-      cancelButtonClicked={handleWindowClose}
-      cancelButtonName="cancel"
-    >
-      <Grid container alignItems="center" direction="column" spacing={2} p={2}>
-        <Grid item sx={{ width: '100%' }}>
-          <ParticipantList minimal participants={inviteList} callbackFunction={removeInvite} />
+    <>
+      <GordonDialogBox
+        open={openInviteParticipantForm}
+        title="Invite Teammates"
+        fullWidth
+        maxWidth="sm"
+        buttonClicked={handleSubmit}
+        isButtonDisabled={disableUpdateButton}
+        buttonName="Send Invites"
+        cancelButtonClicked={handleWindowClose}
+        cancelButtonName="cancel"
+      >
+        <Grid container alignItems="center" direction="column" spacing={2} p={2}>
+          <Grid item sx={{ width: '100%' }}>
+            <ParticipantList minimal participants={inviteList} callbackFunction={removeInvite} />
+          </Grid>
+          <Grid item>
+            <GordonQuickSearch
+              customPlaceholderText={'Search for people'}
+              disableLink
+              onSearchSubmit={(selectedUsername) => onSearchSubmit(selectedUsername)}
+            />
+          </Grid>
         </Grid>
-        <Grid item>
-          <GordonQuickSearch
-            customPlaceholderText={'Search for people'}
-            disableLink
-            onSearchSubmit={(selectedUsername) => onSearchSubmit(selectedUsername)}
-          />
-        </Grid>
-      </Grid>
-    </GordonDialogBox>
+        <GordonSnackbar
+          open={snackbar.open}
+          text={snackbar.message}
+          severity={snackbar.severity}
+          onClose={() => setSnackbar((s) => ({ ...s, open: false }))}
+        />
+      </GordonDialogBox>
+    </>
   );
 };
 
