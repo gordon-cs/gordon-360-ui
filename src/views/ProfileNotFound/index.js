149ffaec9235dcccac37420c25e0ea22d45d1015
Evan Platzer <evan.platzer@gmail.com>
2023-01-20 20:58:16 -0500

    Refactor ProfileNotFound to function component

--- src/views/ProfileNotFound/index.js
+++ src/views/ProfileNotFound/index.js
@@ -1,16 +1,14 @@
-import { Component } from 'react';
 import { Typography, Grid } from '@mui/material';
 
-export default class ProfileNotFound extends Component {
-  render() {
-    return (
-      <Grid item>
-        <br />
-        <br />
-        <Typography variant="h4" align="center">
-          No profile exists for this user
-        </Typography>
-      </Grid>
-    );
-  }
-}
+const ProfileNotFound = () =>
+(
+  <Grid item>
+    <br />
+    <br />
+    <Typography variant="h4" align="center">
+      No profile exists for this user
+    </Typography>
+  </Grid>
+);
+
+export default ProfileNotFound
