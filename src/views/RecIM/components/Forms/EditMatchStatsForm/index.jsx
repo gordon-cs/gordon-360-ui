8f165188ccb7877255f4a290640a9847d73ea76a
Silas White <silas.j.white@gmail.com>
2023-03-24 23:01:20 -0400

    Rework edit match stats form to be a typical form

--- src/views/RecIM/components/Forms/EditMatchStatsForm/index.jsx
+++ src/views/RecIM/components/Forms/EditMatchStatsForm/index.jsx
@@ -1,19 +1,13 @@
-import { Grid } from '@mui/material';
 import { useState, useMemo, useEffect } from 'react';
-import GordonDialogBox from 'components/GordonDialogBox';
-import { InformationField } from '../components/InformationField';
-import { ConfirmationWindowHeader } from '../components/ConfirmationHeader';
-import { ConfirmationRow } from '../components/ConfirmationRow';
-import { ContentCard } from '../components/ContentCard';
-import GordonLoader from 'components/Loader';
 import { getMatchTeamStatusTypes, updateMatchStats } from 'services/recim/match';
+import Form from "../Form"
 
 const EditMatchStatsForm = ({
   match,
   targetTeamID,
   closeWithSnackbar,
   openEditMatchStatsForm,
-  setTargetTeamID,
+  setOpenEditMatchStatsForm
 }) => {
   const [errorStatus, setErrorStatus] = useState({
     Score: false,
@@ -39,6 +33,7 @@ const EditMatchStatsForm = ({
       type: 'number',
       error: errorStatus.Score,
       helperText: '*Required',
+      required: true
     },
     {
       label: 'Sportsmanship',
@@ -46,6 +41,7 @@ const EditMatchStatsForm = ({
       type: 'number',
       error: errorStatus.SportsmanshipScore,
       helperText: "*Required & Can't be more than 5",
+      required: true
     },
     {
       label: 'Status',
@@ -56,10 +52,9 @@ const EditMatchStatsForm = ({
       }),
       error: errorStatus.StatusID,
       helperText: '*Required',
+      required: true
     },
   ];
-
-  const allFields = [createMatchStatsField].flat();
 
   const currentInfo = useMemo(() => {
     var targetTeamStats = match.Scores.find((score) => score.TeamID === targetTeamID);
@@ -74,91 +69,18 @@ const EditMatchStatsForm = ({
     };
   }, [match, targetTeamID, matchStatus]);
 
-  const [newInfo, setNewInfo] = useState(currentInfo);
-  const [isSaving, setSaving] = useState(false);
-  const [openConfirmWindow, setOpenConfirmWindow] = useState(false);
-  const [disableUpdateButton, setDisableUpdateButton] = useState(true);
-
-  const handleSetError = (field, condition) => {
-    const getCurrentErrorStatus = (currentValue) => {
-      return {
-        ...currentValue,
-        [field]: condition,
-      };
-    };
-    setErrorStatus(getCurrentErrorStatus);
-  };
-
-  // refresh dropdown select once fetch is complete
-  useEffect(() => {
-    setNewInfo(currentInfo);
-  }, [currentInfo]);
-
-  // Field Validation
-  useEffect(() => {
-    let hasChanges = false;
-    let hasError = false;
-    for (const field in currentInfo) {
-      if (currentInfo[field] !== newInfo[field]) {
-        hasChanges = true;
-      }
-      switch (field) {
-        case 'Sportsmanship':
-          hasError = hasError || newInfo[field] > 5;
-        //fall through
-        case 'Score':
-          hasError = !/^[0-9]+$/.test(newInfo[field]);
-          break;
-        case 'StatusID':
-          hasError = newInfo[field] === '';
-          break;
-        default:
-      }
-      handleSetError(field, hasError);
+  const errorCases = (field, value) => {
+    switch (field) {
+      case 'Sportsmanship':
+        return (value < 0) || (value > 5);
+      case 'Score':
+        return !/^[0-9]+$/.test(value);
+      default:
+        return false;
     }
-
-    setDisableUpdateButton(hasError || !hasChanges);
-  }, [newInfo, currentInfo]);
-
-  const handleChange = (event, src) => {
-    const getNewInfo = (currentValue) => {
-      // datetime pickers return value rather than event,
-      // so we can also manually specify target source and value
-      if (src) {
-        let newValue = event;
-        return {
-          ...currentValue,
-          [src]: newValue,
-        };
-      }
-      return {
-        ...currentValue,
-        [event.target.name]:
-          event.target.type === 'checkbox' ? event.target.checked : event.target.value,
-      };
-    };
-    setNewInfo(getNewInfo);
-  };
-
-  const getFieldLabel = (fieldName) => {
-    const matchingField = allFields.find((field) => field.name === fieldName);
-    return matchingField.label;
-  };
-
-  function getNewFields(currentInfo, newInfo) {
-    const updatedFields = [];
-    Object.entries(newInfo).forEach(([key, value]) => {
-      if (currentInfo[key] !== value)
-        updatedFields.push({
-          Field: key,
-          Value: value,
-          Label: getFieldLabel(key),
-        });
-    });
-    return updatedFields;
   }
 
-  const handleConfirm = () => {
+  const handleConfirm = (newInfo, handleWindowClose, setSaving) => {
     setSaving(true);
     let matchStatsRequest = { ...currentInfo, ...newInfo };
     matchStatsRequest.StatusID = matchStatus.find(
@@ -174,88 +96,20 @@ const EditMatchStatsForm = ({
     });
   };
 
-  const handleWindowClose = () => {
-    setTargetTeamID(null);
-    setOpenConfirmWindow(false);
-    setNewInfo(currentInfo);
-  };
-
-  /**
-   * @param {Array<{name: string, label: string, type: string, menuItems: string[]}>} fields array of objects defining the properties of the input field
-   * @returns JSX correct input for each field based on type
-   */
-  const mapFieldsToInputs = (fields) => {
-    return fields.map((field) => (
-      <InformationField
-        key={field.name}
-        error={field.error}
-        label={field.label}
-        name={field.name}
-        helperText={field.helperText}
-        value={newInfo[field.name]}
-        type={field.type}
-        menuItems={field.menuItems}
-        onChange={handleChange}
-        xs={12}
-        sm={12}
-        md={12}
-        lg={12}
-      />
-    ));
-  };
-
-  let content;
-  if (loading) {
-    content = <GordonLoader />;
-  } else {
-    content = (
-      <>
-        <ContentCard
-          title={`Match Stats for "${match.Team.find((team) => team.ID === targetTeamID).Name}"`}
-        >
-          {mapFieldsToInputs(createMatchStatsField)}
-        </ContentCard>
-        {/* Confirmation Dialog */}
-        <GordonDialogBox
-          open={openConfirmWindow}
-          title="Confirm your Match Stats"
-          buttonClicked={!isSaving && handleConfirm}
-          buttonName="Confirm"
-          // in case you want to authenticate something change isButtonDisabled
-          isButtonDisabled={disableUpdateButton}
-          cancelButtonClicked={!isSaving && handleWindowClose}
-          cancelButtonName="Cancel"
-        >
-          <ConfirmationWindowHeader />
-          <Grid container>
-            {getNewFields(currentInfo, newInfo).map((field) => (
-              <ConfirmationRow key={field} field={field} prevValue={currentInfo[field.Field]} />
-            ))}
-          </Grid>
-          {isSaving && <GordonLoader size={32} />}
-        </GordonDialogBox>
-      </>
-    );
-  }
-
   return (
-    <GordonDialogBox
-      open={openEditMatchStatsForm}
-      title={`Edit ${match.Team.find((team) => team.ID === targetTeamID).Name}'s Stats`}
-      fullWidth
-      maxWidth="sm"
-      buttonClicked={() => setOpenConfirmWindow(true)}
-      isButtonDisabled={disableUpdateButton}
-      buttonName="Submit"
-      cancelButtonClicked={() => {
-        setNewInfo(currentInfo);
-        setTargetTeamID(null);
-      }}
-      cancelButtonName="cancel"
-    >
-      {content}
-    </GordonDialogBox>
-  );
+    <Form
+      formTitle={{ name: 'Match', formType: 'Edit' }}
+      fields={createMatchStatsField}
+      currentInfo={currentInfo}
+      errorCases={errorCases}
+      setErrorStatus={setErrorStatus}
+      loading={loading}
+      setOpenForm={setOpenEditMatchStatsForm}
+      openForm={openEditMatchStatsForm}
+      handleConfirm={handleConfirm}
+    />
+
+  )
 };
 
 export default EditMatchStatsForm;
