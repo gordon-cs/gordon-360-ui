b8eca8830b3488a8f04c6ba2d494cee47a483d6f
Silas White <silas.j.white@gmail.com>
2023-04-10 10:36:10 -0400

    Move tab panel to folder and update imports

--- src/views/RecIM/views/Activity/index.jsx
+++ src/views/RecIM/views/Activity/index.jsx
@@ -34,7 +34,7 @@ import ScheduleList from './components/ScheduleList';
 import { formatDateTimeRange } from '../../components/Helpers';
 import GordonDialogBox from 'components/GordonDialogBox';
 import defaultLogo from 'views/RecIM/recim_logo.png';
-import { TabPanel } from 'views/RecIM/components';
+import { TabPanel } from 'views/RecIM/components/TabPanel';
 import { Box } from '@mui/system';
 import { createTeam } from 'services/recim/team';
 import InviteParticipantForm from 'views/RecIM/components/Forms/InviteParticipantForm';
