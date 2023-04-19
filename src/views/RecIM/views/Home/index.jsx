b91042d1d3d156755635af3ac4c513523abed06a
amos-cha <amos.cha98@gmail.com>
2023-04-19 02:57:30 -0400

    Adjust styling to fit the image

--- src/views/RecIM/views/Home/index.jsx
+++ src/views/RecIM/views/Home/index.jsx
@@ -250,10 +250,10 @@ const Home = () => {
           <GordonLoader />
         ) : (
           <Grid container spacing={2}>
-            <Grid item xs={12} md={7}>
+            <Grid item xs={12} md={6.5}>
               {activitiesCard}
             </Grid>
-            <Grid item xs={12} md={5}>
+            <Grid item xs={12} md={5.5}>
               {myTeamsCard}
             </Grid>
             <ActivityForm
