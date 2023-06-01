5e2503132d5b29dd9b08d663b78a0fc95921a804
andrew-wzj <Andrew.Wang@gordon.edu>
2023-06-01 17:07:35 -0400

    Add Calendar to Event works but still need work on date and time format conversion

--- src/components/EventList/components/EventItem/index.jsx
+++ src/components/EventList/components/EventItem/index.jsx
@@ -1,10 +1,14 @@
-import { CardContent, Collapse, Grid, Typography } from '@mui/material';
+import { Button, CardContent, Collapse, Grid, Typography } from '@mui/material';
 import { useState } from 'react';
 import styles from './EventItem.module.css';
+import { AddToCalendarButton } from 'add-to-calendar-button-react';
 
 const EventItem = ({ event }) => {
   const [expanded, setExpanded] = useState(false);
 
+  
+
+  
   return (
     <Grid
       component="section"
@@ -32,6 +36,18 @@ const EventItem = ({ event }) => {
           <Typography type="caption" className={styles.descriptionText}>
             {event.Description || 'No description available'}
           </Typography>
+          <Button onClick={() => alert(event.date +" " +event.timeRange)}
+          >name</Button>
+        <AddToCalendarButton
+          name= {event.title}
+          options={['Apple','Google']}
+          location= {event.location}
+          startDate="2023-06-04"
+          endDate="2023-06-04"
+          startTime="10:15"
+          endTime="23:30"
+          timeZone="America/Indiana/Indianapolis"
+        ></AddToCalendarButton>
         </CardContent>
       </Collapse>
     </Grid>
