b0c6e0c9a0332d9834411fc3ae0a97b5075a402e
Wenlan Ji <arabella.ji@zephaniah.gordon.edu>
2023-06-12 11:34:01 -0400

    create and call the updateTabHighlight() function fewer times

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -23,8 +23,35 @@ import styles from './Header.module.css';
 
 const ForwardNavLink = forwardRef((props, ref) => <NavLink innerRef={ref} {...props} />);
 
-const GordonHeader = ({ onDrawerToggle }) => {
+// Tab url regular expressions must be listed in the same order as the tabs, since the
+// indices of the elements in the array on the next line are mapped to the indices of the tabs
+const TabUrlPatterns = [
+  /^\/$/,
+  /^\/involvements\/?$|^\/activity/,
+  /^\/events\/?$/,
+  /^\/people$|^\/myprofile|^\/profile/,
+  /^\/timesheets$/,
+];
+
+/**
+ * Update the tab highlight indicator based on the url
+ *
+ * The checks use regular expressions to check for matches in the url.
+ */
+const updateTabHighlight = () => {
+  const location = useLocation();
+  let currentPath = location.pathname;
   const [tabIndex, setTabIndex] = useState(0);
+
+  useEffect(() => {
+    const matchedIndex = TabUrlPatterns.findIndex((pattern) => pattern.test(currentPath));
+    setTabIndex(matchedIndex); // This won't cause an update if the new value is the same as the old value
+  }, [currentPath]);
+
+  return tabIndex;
+};
+
+const GordonHeader = ({ onDrawerToggle }) => {
   const [dialog, setDialog] = useState('');
   const [width] = useWindowSize();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -32,33 +59,9 @@ const GordonHeader = ({ onDrawerToggle }) => {
   const isOnline = useNetworkStatus();
   const setDocumentTitle = useDocumentTitle();
   const isAuthenticated = useIsAuthenticated();
+  const tabIndex = updateTabHighlight();
 
-  /**
-   * Update the tab highlight indicator based on the url
-   *
-   * The checks use regular expressions to check for matches in the url.
-   */
-  const updateTabHighlight = () => {
-    // Tab url regular expressions must be listed in the same order as the tabs, since the
-    // indices of the elements in the array on the next line are mapped to the indices of the tabs
-    const urls = [
-      /^\/$/,
-      /^\/involvements\/?$|^\/activity/,
-      /^\/events\/?$/,
-      /^\/people$|^\/myprofile|^\/profile/,
-      /^\/timesheets$/,
-    ];
-    let currentPath = useLocation().pathname;
-    let tab = false;
-    for (let i = 0; i < urls.length; i++) {
-      if (urls[i].test(currentPath)) {
-        tab = i;
-      }
-    }
-    if (tab !== tabIndex) {
-      setTabIndex(tab);
-    }
-  };
+  console.log('tabIndex: ' + tabIndex);
 
   useEffect(() => {
     if (width < windowBreakWidths.breakMD) {
@@ -138,85 +141,82 @@ const GordonHeader = ({ onDrawerToggle }) => {
   );
 
   return (
-    <>
-      {updateTabHighlight()}
-      <section className={styles.gordon_header}>
-        <AppBar className={styles.app_bar} position="static">
-          <Toolbar>
-            <IconButton
-              className={styles.menu_button}
-              color="primary"
-              aria-label="open drawer"
-              onClick={onDrawerToggle}
-              size="large"
-            >
-              <MenuIcon className={styles.menu_button_icon} />
-            </IconButton>
+    <section className={styles.gordon_header}>
+      <AppBar className={styles.app_bar} position="static">
+        <Toolbar>
+          <IconButton
+            className={styles.menu_button}
+            color="primary"
+            aria-label="open drawer"
+            onClick={onDrawerToggle}
+            size="large"
+          >
+            <MenuIcon className={styles.menu_button_icon} />
+          </IconButton>
 
-            <Typography className={`disable_select ${styles.title}`} variant="h6" color="inherit">
-              <Routes>
-                {routes.map((route) => (
-                  <Route
-                    key={route.path}
-                    path={route.path}
-                    element={() => {
-                      setDocumentTitle(route.name || projectName);
-                      return <span>{route.name || projectName}</span>;
-                    }}
-                  />
-                ))}
-              </Routes>
-            </Typography>
+          <Typography className={`disable_select ${styles.title}`} variant="h6" color="inherit">
+            <Routes>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={() => {
+                    setDocumentTitle(route.name || projectName);
+                    return <span>{route.name || projectName}</span>;
+                  }}
+                />
+              ))}
+            </Routes>
+          </Typography>
 
-            <div className={styles.center_container}>
-              <Tabs textColor="inherit" indicatorColor="secondary" centered value={tabIndex}>
-                <Tab
-                  className={styles.tab}
-                  icon={<HomeIcon />}
-                  label="Home"
-                  component={ForwardNavLink}
-                  to="/"
-                />
-                <Tab
-                  className={styles.tab}
-                  icon={<LocalActivityIcon />}
-                  label="Involvements"
-                  component={ForwardNavLink}
-                  to="/involvements"
-                />
-                <Tab
-                  className={styles.tab}
-                  icon={<EventIcon />}
-                  label="Events"
-                  component={ForwardNavLink}
-                  to="/events"
-                />
-                {requiresAuthTab('People', <PeopleIcon />)}
-                {requiresAuthTab('Timesheets', <WorkIcon />)}
-              </Tabs>
+          <div className={styles.center_container}>
+            <Tabs textColor="inherit" indicatorColor="secondary" centered value={tabIndex}>
+              <Tab
+                className={styles.tab}
+                icon={<HomeIcon />}
+                label="Home"
+                component={ForwardNavLink}
+                to="/"
+              />
+              <Tab
+                className={styles.tab}
+                icon={<LocalActivityIcon />}
+                label="Involvements"
+                component={ForwardNavLink}
+                to="/involvements"
+              />
+              <Tab
+                className={styles.tab}
+                icon={<EventIcon />}
+                label="Events"
+                component={ForwardNavLink}
+                to="/events"
+              />
+              {requiresAuthTab('People', <PeopleIcon />)}
+              {requiresAuthTab('Timesheets', <WorkIcon />)}
+            </Tabs>
+          </div>
+
+          <div className={styles.people_search_container_container}>
+            {/* Width is dynamic */}
+            <div className={styles.people_search_container}>
+              {isAuthenticated ? <GordonQuickSearch /> : loginButton}
             </div>
+          </div>
 
-            <div className={styles.people_search_container_container}>
-              {/* Width is dynamic */}
-              <div className={styles.people_search_container}>
-                {isAuthenticated ? <GordonQuickSearch /> : loginButton}
-              </div>
-            </div>
+          <GordonNavAvatarRightCorner onClick={handleOpenMenu} menuOpened={isMenuOpen} />
 
-            <GordonNavAvatarRightCorner onClick={handleOpenMenu} menuOpened={isMenuOpen} />
+          <GordonNavButtonsRightCorner
+            open={isMenuOpen}
+            openDialogBox={setDialog}
+            anchorEl={anchorElement}
+            onClose={handleCloseMenu}
+          />
 
-            <GordonNavButtonsRightCorner
-              open={isMenuOpen}
-              openDialogBox={setDialog}
-              anchorEl={anchorElement}
-              onClose={handleCloseMenu}
-            />
-
-            {createDialogBox()}
-          </Toolbar>
-        </AppBar>
-      </section>
-    </>
+          {createDialogBox()}
+        </Toolbar>
+      </AppBar>
+    </section>
   );
 };
 
