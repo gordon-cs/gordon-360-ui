e865d0da054ef72e97e5e9e60e48f061009c48b0
Antonia Vontobel <antonia.vontobel@gordon.edu>
2023-06-14 17:21:03 -0400

    implemented the dialog box for the editing office location for staff

--- src/components/Profile/components/OfficeInfoList/UpdateOfficeLocationDialog/index.jsx
+++ src/components/Profile/components/OfficeInfoList/UpdateOfficeLocationDialog/index.jsx
@@ -1,20 +1,21 @@
-import { FormControl, IconButton, Input, InputLabel, Select } from '@mui/material';
+import { FormControl, IconButton, Input, InputLabel, Select, MenuItem } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import GordonDialogBox from 'components/GordonDialogBox';
 import GordonSnackbar from 'components/Snackbar';
-import { forwardRef, useState } from 'react';
+import { forwardRef, useState, useEffect } from 'react';
 import { IMaskInput } from 'react-imask';
 import userService from 'services/user';
+import SearchField from 'views/PeopleSearch/components/SearchFieldList/components/SearchField';
 
 const UpdateOffice = () => {
   const [open, setOpen] = useState(false);
   const [room, setRoom] = useState('');
   const [building, setBuilding] = useState('');
   const [snackbar, setSnackbar] = useState({ message: '', severity: null, open: false });
-  //const [buildings, setBuildings] = useState<string[]>([]);
+  const [buildings, setBuildings] = useState([]);
 
   const handleSubmit = async () => {
-    const values = room + ',' + building;
+    const values = building + ',' + room;
     try {
       await userService.setOfficeLocation(values.toString());
       createSnackbar('Your phone number will update within a couple hours.', 'success');
@@ -27,9 +28,13 @@ const UpdateOffice = () => {
   const createSnackbar = (message, severity) => {
     setSnackbar({ message, severity, open: true });
   };
-  //   console.log(userService.getBuildings()); // I am adding this not working yet
-  //   setBuildings(userService.getBuildings());
-  //console.log(buildings);
+  //console.log(userService.getBuildings()); // I am adding this not working yet
+
+  useEffect(() => {
+    userService.getBuildings().then(setBuildings);
+  }, []);
+
+  console.log(building);
   return (
     <div>
       <IconButton style={{ marginBottom: '0.5rem' }} onClick={() => setOpen(true)} size="large">
@@ -43,31 +48,11 @@ const UpdateOffice = () => {
         buttonClicked={handleSubmit}
         cancelButtonName="CANCEL"
         cancelButtonClicked={() => setOpen(false)}
+        handleSubmit
       >
-        <FormControl>
-          <InputLabel htmlFor="formatted-text-mask-input">Building</InputLabel>
+        <FormControl sx={{ m: 1, minWidth: 200 }}>
+          {/* <InputLabel htmlFor="formatted-text-mask-input">Building</InputLabel> */}
           {/* <Select
-            type="tel"
-            id="building-input"
-            name="building"
-            value={building}
-            onChange={(event) => setbuilding(event.target.value)}
-            required="required"
-            autoFocus
-          >
-            {buildingsDescriptions.map((item) => (
-              <MenuItem
-                value={item.ID}
-                onClick={async () => {
-                  await handleChangeParticipantStatus(item.ID);
-                  setAnchorEl(null);
-                }}
-              >
-                {item.Description}
-              </MenuItem>
-            ))}
-          </Select> */}
-          <Input
             type="tel"
             id="building-input"
             name="building"
@@ -75,7 +60,31 @@ const UpdateOffice = () => {
             onChange={(event) => setBuilding(event.target.value)}
             required="required"
             autoFocus
+          >
+            Building
+            {buildings.map((item) => (
+              <MenuItem value={item.ID} onChange={(event) => setBuilding(event.target.value)}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select> */}
+          <SearchField
+            name="building"
+            value={building}
+            updateValue={(event) => setBuilding(event.target.value)}
+            options={buildings}
+            select
+            size={200}
           />
+          {/* <Input
+            type="tel"
+            id="building-input"
+            name="building"
+            value={building}
+            onChange={(event) => setBuilding(event.target.value)}
+            required="required"
+            autoFocus
+          /> */}
         </FormControl>
         <FormControl>
           <InputLabel htmlFor="formatted-text-mask-input">Room Number</InputLabel>
