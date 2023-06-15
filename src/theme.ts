be7c2140835655b01bc2a90b182e05a99f513157
matthew.jones <matthew.jones@gordon.edu>
2023-06-15 10:03:41 -0400

    Fix contrast text typo

--- src/theme.ts
+++ src/theme.ts
@@ -202,7 +202,7 @@ export const newTheme = extendTheme({
         },
         secondary: {
           main: '#00AEEF' /* Scottie Cyan */,
-          contrastText: '#FFFFFFF' /* white */,
+          contrastText: '#FFFFFF' /* white */,
           50: '#E0F4FD',
           100: '#B0E2F9',
           200: '#7BD0F5',
@@ -217,7 +217,7 @@ export const newTheme = extendTheme({
         error: {
           main: '#B53228' /* old scheme red, good error color */,
           light: '#ff5d53', //Nautical Red
-          contrastText: '#FFFFFFF' /* white */,
+          contrastText: '#FFFFFF' /* white */,
         },
         success: {
           dark: '#006d22' /*La Vida Green*/,
