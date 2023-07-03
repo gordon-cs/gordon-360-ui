402ead92f509dda593cb40c8477858bbbcdea8cc
Wenlan Ji <78650688+ArabellaJi@users.noreply.github.com>
2023-07-03 12:05:06 -0400

    Revert "implemented the dialog box for changing the fac/staff office hours"

--- src/components/Profile/components/OfficeInfoList/index.jsx
+++ src/components/Profile/components/OfficeInfoList/index.jsx
@@ -4,7 +4,6 @@ import styles from './OfficeInfoList.module.css';
 import { gordonColors } from 'theme';
 import UpdateOffice from './UpdateOfficeLocationDialog';
 import GordonTooltip from 'components/GordonTooltip';
-import UpdateOfficeHours from './UpdateOfficeHoursDialog';
 
 const OfficeInfoList = ({
   myProf,
@@ -45,21 +44,7 @@ const OfficeInfoList = ({
   ) : null;
 
   const officeHours = office_hours ? (
-    <ProfileInfoListItem
-      title="Office Hours:"
-      contentText={
-        myProf ? (
-          <Grid container spacing={0} alignItems="center">
-            <Grid item>{office_hours}</Grid>
-            <Grid item>
-              <UpdateOfficeHours />
-            </Grid>
-          </Grid>
-        ) : (
-          `${office_hours}`
-        )
-      }
-    />
+    <ProfileInfoListItem title="Office Hours:" contentText={office_hours} />
   ) : null;
 
   const room =
