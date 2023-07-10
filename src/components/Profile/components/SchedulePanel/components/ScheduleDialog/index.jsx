c525cf0578e9cf3ce7b8cfddee157dc3aedc8b3e
andrew-wzj <Andrew.Wang@gordon.edu>
2023-07-10 11:32:50 -0400

    working on styling

--- src/components/Profile/components/SchedulePanel/components/ScheduleDialog/index.jsx
+++ src/components/Profile/components/SchedulePanel/components/ScheduleDialog/index.jsx
@@ -20,13 +20,13 @@ const ScheduleDialog = (props) => {
           Add Course Schedule to Calendar
         </DialogTitle>
         <DialogContent>
-          <Typography sx={{ fontSize: 'large' }} align="center">
+          <Typography sx={{ fontSize: 'large' }} align="left">
             Course Title: {props.selectedCourseInfo?.title.split('in')[0]}
           </Typography>
-          <Typography sx={{ fontSize: 'large' }} align="center">
+          <Typography sx={{ fontSize: 'large' }} align="left">
             Room: {props.selectedCourseInfo?.title.split('in')[1]}
           </Typography>
-          <Typography sx={{ fontSize: 'large' }} align="center">
+          <Typography sx={{ fontSize: 'large' }} align="left">
             Time Range:
             {format(
               new Date(props.selectedCourseInfo ? props.selectedCourseInfo.start : null),
@@ -37,85 +37,100 @@ const ScheduleDialog = (props) => {
               " - hh:mm aaaaa'm' ",
             )}
           </Typography>
-          <Typography sx={{ fontSize: 'large' }} align="center">
+          <Typography sx={{ fontSize: 'large' }} align="left">
             Meeting Days: {props.recurringDays}
+          </Typography>
+          <Typography sx={{ fontSize: 'large' }} align="left">
+            Course Term:{' '}
+            {format(
+              setDay(
+                new Date(props.firstDay),
+                dayArr.indexOf(props.selectedCourseInfo?.resourceId) + 1,
+              ),
+              'yyyy-MM-dd',
+            )}
           </Typography>
         </DialogContent>
         <DialogActions style={{ overflow: 'hidden', flexDirection: 'column' }}>
           {/* There are two separate add-to-calendar buttons because Google calendar is the only
           calendar that supports recurring events, the other add-to-calendar button is for the other
           options that users can choose and manually set the course as recurring */}
-
-          {props.selectedCourseInfo && (
-            <>
-              <add-to-calendar-button
-                name={props.selectedCourseInfo.title}
-                startDate={format(
-                  setDay(
-                    new Date(props.firstDay),
-                    dayArr.indexOf(props.selectedCourseInfo.resourceId) + 1,
-                  ),
-                  'yyyy-MM-dd',
-                )}
-                startTime={
-                  props.selectedCourseInfo.allDay
-                    ? null
-                    : format(new Date(props.selectedCourseInfo.start), 'HH:mm')
-                }
-                endTime={
-                  props.selectedCourseInfo.allDay
-                    ? null
-                    : format(new Date(props.selectedCourseInfo.end), 'HH:mm')
-                }
-                description={
-                  props.selectedCourseInfo.allDay ? 'Asynchronous Course' : 'Synchronous Course'
-                }
-                Location={props.selectedCourseInfo.title.split('in')[1]}
-                options="'Google'"
-                buttonsList
-                hideTextLabelButton
-                buttonStyle="round"
-                recurrence={
-                  'RRULE:FREQ=WEEKLY;INTERVAL=1;BYDAY=' +
-                  props.recurringDays +
-                  ';UNTIL=' +
-                  format(new Date(props.lastDay), 'yyyyMMdd')
-                }
-                lightMode="bodyScheme"
-                Timezone="currentBrowser"
-              ></add-to-calendar-button>
-              <add-to-calendar-button
-                name={props.selectedCourseInfo.title}
-                startDate={format(
-                  setDay(
-                    new Date(props.firstDay),
-                    dayArr.indexOf(props.selectedCourseInfo.resourceId) + 1,
-                  ),
-                  'yyyy-MM-dd',
-                )}
-                startTime={
-                  props.selectedCourseInfo.allDay
-                    ? null
-                    : format(new Date(props.selectedCourseInfo.start), 'HH:mm')
-                }
-                endTime={
-                  props.selectedCourseInfo.allDay
-                    ? null
-                    : format(new Date(props.selectedCourseInfo.end), 'HH:mm')
-                }
-                description={
-                  props.selectedCourseInfo.allDay ? 'Asynchronous Course' : 'Synchronous Course'
-                }
-                Location={props.selectedCourseInfo.title.split('in')[1]}
-                options="'Outlook.com','MicrosoftTeams','Apple'"
-                buttonsList
-                hideTextLabelButton
-                buttonStyle="round"
-                lightMode="bodyScheme"
-                Timezone="currentBrowser"
-              ></add-to-calendar-button>
-            </>
-          )}
+          <Grid container lg={12}>
+            {props.selectedCourseInfo && (
+              <>
+                <Grid item lg={6}>
+                  <add-to-calendar-button
+                    name={props.selectedCourseInfo.title}
+                    startDate={format(
+                      setDay(
+                        new Date(props.firstDay),
+                        dayArr.indexOf(props.selectedCourseInfo.resourceId) + 1,
+                      ),
+                      'yyyy-MM-dd',
+                    )}
+                    startTime={
+                      props.selectedCourseInfo.allDay
+                        ? null
+                        : format(new Date(props.selectedCourseInfo.start), 'HH:mm')
+                    }
+                    endTime={
+                      props.selectedCourseInfo.allDay
+                        ? null
+                        : format(new Date(props.selectedCourseInfo.end), 'HH:mm')
+                    }
+                    description={
+                      props.selectedCourseInfo.allDay ? 'Asynchronous Course' : 'Synchronous Course'
+                    }
+                    Location={props.selectedCourseInfo.title.split('in')[1]}
+                    options="'Google'"
+                    buttonsList
+                    hideTextLabelButton
+                    buttonStyle="round"
+                    recurrence={
+                      'RRULE:FREQ=WEEKLY;INTERVAL=1;BYDAY=' +
+                      props.recurringDays +
+                      ';UNTIL=' +
+                      format(new Date(props.lastDay), 'yyyyMMdd')
+                    }
+                    lightMode="bodyScheme"
+                    Timezone="currentBrowser"
+                  ></add-to-calendar-button>
+                </Grid>
+                <Grid item lg={6}>
+                  <add-to-calendar-button
+                    name={props.selectedCourseInfo.title}
+                    startDate={format(
+                      setDay(
+                        new Date(props.firstDay),
+                        dayArr.indexOf(props.selectedCourseInfo.resourceId) + 1,
+                      ),
+                      'yyyy-MM-dd',
+                    )}
+                    startTime={
+                      props.selectedCourseInfo.allDay
+                        ? null
+                        : format(new Date(props.selectedCourseInfo.start), 'HH:mm')
+                    }
+                    endTime={
+                      props.selectedCourseInfo.allDay
+                        ? null
+                        : format(new Date(props.selectedCourseInfo.end), 'HH:mm')
+                    }
+                    description={
+                      props.selectedCourseInfo.allDay ? 'Asynchronous Course' : 'Synchronous Course'
+                    }
+                    Location={props.selectedCourseInfo.title.split('in')[1]}
+                    options="'Outlook.com','MicrosoftTeams','Apple'"
+                    buttonsList
+                    hideTextLabelButton
+                    buttonStyle="round"
+                    lightMode="bodyScheme"
+                    Timezone="currentBrowser"
+                  ></add-to-calendar-button>
+                </Grid>
+              </>
+            )}
+          </Grid>
           <Grid style={{ marginTop: '20px' }}>
             <Button onClick={props.handleScheduleDialogClose} variant="contained" size="medium">
               Cancel
