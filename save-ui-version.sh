86d770201a7b81938dcdcf1ad3f0b2bb8c1151c9
Jonathan Senning <jonathan.senning@gmail.com>
2023-07-10 16:54:25 -0400

    Display UI version information on About page.  A "prebuild" script is run that saves the SHA and date of the current branch HEAD, overwriting the src/views/About/version.json file.

--- save-ui-version.sh
+++ save-ui-version.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 GIT_DIR=$(echo $(git rev-parse --git-dir)/../src)
 VERSION_JSON="$GIT_DIR/views/About/version.json"
 
 commit_sha=$(git rev-parse --short HEAD)
-#commit_date=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%Y/%m/%d_%H:%M:%S)
 commit_date=$(git log -1 --format="%at" | xargs -I{} date -u -d @{} +"%Y/%m/%d %H:%M:%S %Z")
 
 cat << EOF > $VERSION_JSON
