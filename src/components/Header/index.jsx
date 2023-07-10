4e1deaa3e4d6303af3481a9c8c86e9f4fba394f4
Russ Tuck <russ.tuck@gordon.edu>
2023-07-10 16:05:40 -0400

    Revert "Add #1885 (new logo on header) to release"

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -151,15 +151,7 @@ const GordonHeader = ({ onDrawerToggle }) => {
           >
             <MenuIcon className={styles.menu_button_icon} />
           </IconButton>
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <Tab //this is an "invisible" button behind the new logo that links back to the home page
-            className={styles.tab}
-            label=" "
-            component={ForwardNavLink}
-            disableRipple={true}
-            to="/"
-            onChange={(event, value) => setTabIndex(value)}
-          />
+
           <Typography className={`disable_select ${styles.title}`} variant="h6" color="inherit">
             <Routes>
               {routes.map((route) => (
@@ -174,6 +166,7 @@ const GordonHeader = ({ onDrawerToggle }) => {
               ))}
             </Routes>
           </Typography>
+
           <div className={styles.center_container}>
             <Tabs
               textColor="inherit"
@@ -207,19 +200,23 @@ const GordonHeader = ({ onDrawerToggle }) => {
               {requiresAuthTab('Timesheets', <WorkIcon />)}
             </Tabs>
           </div>
+
           <div className={styles.people_search_container_container}>
             {/* Width is dynamic */}
             <div className={styles.people_search_container}>
               {isAuthenticated ? <GordonQuickSearch /> : loginButton}
             </div>
           </div>
+
           <GordonNavAvatarRightCorner onClick={handleOpenMenu} menuOpened={isMenuOpen} />
+
           <GordonNavButtonsRightCorner
             open={isMenuOpen}
             openDialogBox={setDialog}
             anchorEl={anchorElement}
             onClose={handleCloseMenu}
           />
+
           {createDialogBox()}
         </Toolbar>
       </AppBar>
