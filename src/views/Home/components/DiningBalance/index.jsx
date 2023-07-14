918805b390dcc76601c65eae4ea5dddae236ef9c
Antonia Vontobel <antonia.vontobel@gordon.edu>
2023-07-14 14:56:48 -0400

    removed comment

--- src/views/Home/components/DiningBalance/index.jsx
+++ src/views/Home/components/DiningBalance/index.jsx
@@ -244,65 +244,6 @@ const DiningBalance = () => {
               </IconButton>
             </Grid>
           </Grid>
-          {/* <Grid container direction="row" alignSelf="right">
-            <Grid item xs={1} align="right">
-              <IconButton
-                title="Change Meal Plan"
-                className={styles.gc360_my_profile_edit_icon}
-                component={Link}
-                href="https://www.gordon.edu/mealplan"
-                size="small"
-              >
-                <EditIcon />
-              </IconButton>
-            </Grid>
-            <Grid item xs={5} align="right">
-              <Button
-                variant="contained"
-                style={{
-                  backgroundColor: 'var(--mui-palette-secondary-main)',
-                  color: 'var(--mui-palette-secondary-contrastText',
-                }}
-                component={Link}
-                href="https://gordon.cafebonappetit.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                TODAY'S MENU
-              </Button>
-            </Grid>
-          </Grid> */}
-          {/* <Grid item xs={7} align="left">
-            <CardHeader title="Dining Balance" />
-          </Grid>
-          <Grid item xs={5} align="right" className={styles.value}>
-            <Grid item>
-              <Button
-                variant="contained"
-                style={{
-                  backgroundColor: 'var(--mui-palette-secondary-main)',
-                  color: 'var(--mui-palette-secondary-contrastText',
-                }}
-                component={Link}
-                href="https://gordon.cafebonappetit.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                TODAY'S MENU
-              </Button>
-            </Grid>
-            <Grid item>
-              <IconButton
-                title="Change Meal Plan"
-                className={styles.gc360_my_profile_edit_icon}
-                component={Link}
-                href="https://www.gordon.edu/mealplan"
-                size="small"
-              >
-                <EditIcon />
-              </IconButton>
-            </Grid>
-          </Grid> */}
         </Grid>
         {content}
       </CardContent>
