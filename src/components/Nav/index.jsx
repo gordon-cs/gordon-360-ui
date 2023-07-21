a76ff99f10d15b057a9f397ff2385951786cd1b7
John Hammond <john.hammond@amos.gordon.edu>
2023-07-21 13:17:38 -0400

    Revert "Remove unnecessary section"
    This reverts commit b1c706409cea04dae64db75cd6d881f7eb8978c1.

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,23 +4,25 @@ import GordonNavLinks from './components/NavLinks';
 import styles from './Nav.module.css';
 
 const GordonNav = ({ onDrawerToggle, drawerOpen }) => (
-  <Drawer
-    variant="temporary"
-    open={drawerOpen}
-    classes={{
-      paper: styles.gordon_nav_drawer,
-    }}
-    onClose={onDrawerToggle}
-    ModalProps={{
-      keepMounted: true, // Better open performance on mobile.
-    }}
-  >
-    <div className={styles.nav_avatar}>
-      <GordonNavAvatar onLinkClick={onDrawerToggle} />
-      <Divider />
-    </div>
-    <GordonNavLinks onLinkClick={onDrawerToggle} />
-  </Drawer>
+  <section className={styles.gordon_nav}>
+    <Drawer
+      variant="temporary"
+      open={drawerOpen}
+      classes={{
+        paper: styles.gordon_nav_drawer,
+      }}
+      onClose={onDrawerToggle}
+      ModalProps={{
+        keepMounted: true, // Better open performance on mobile.
+      }}
+    >
+      <div className={styles.nav_avatar}>
+        <GordonNavAvatar onLinkClick={onDrawerToggle} />
+        <Divider />
+      </div>
+      <GordonNavLinks onLinkClick={onDrawerToggle} />
+    </Drawer>
+  </section>
 );
 
 export default GordonNav;
