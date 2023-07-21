b1c706409cea04dae64db75cd6d881f7eb8978c1
Evan Platzer <evan.platzer@gmail.com>
2023-07-21 12:41:06 -0400

    Remove unnecessary section

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,25 +4,23 @@ import GordonNavLinks from './components/NavLinks';
 import styles from './Nav.module.css';
 
 const GordonNav = ({ onDrawerToggle, drawerOpen }) => (
-  <section className={styles.gordon_nav}>
-    <Drawer
-      variant="temporary"
-      open={drawerOpen}
-      classes={{
-        paper: styles.gordon_nav_drawer,
-      }}
-      onClose={onDrawerToggle}
-      ModalProps={{
-        keepMounted: true, // Better open performance on mobile.
-      }}
-    >
-      <div className={styles.nav_avatar}>
-        <GordonNavAvatar onLinkClick={onDrawerToggle} />
-        <Divider />
-      </div>
-      <GordonNavLinks onLinkClick={onDrawerToggle} />
-    </Drawer>
-  </section>
+  <Drawer
+    variant="temporary"
+    open={drawerOpen}
+    classes={{
+      paper: styles.gordon_nav_drawer,
+    }}
+    onClose={onDrawerToggle}
+    ModalProps={{
+      keepMounted: true, // Better open performance on mobile.
+    }}
+  >
+    <div className={styles.nav_avatar}>
+      <GordonNavAvatar onLinkClick={onDrawerToggle} />
+      <Divider />
+    </div>
+    <GordonNavLinks onLinkClick={onDrawerToggle} />
+  </Drawer>
 );
 
 export default GordonNav;
