a80bcddc13345b1b43664bee1d566e6686add151
Evan Platzer <evan.platzer@gmail.com>
2023-09-15 11:39:15 -0400

    Add library type to MSAL Config and cleanup

--- src/components/Providers/components/AuthProvider/index.tsx
+++ src/components/Providers/components/AuthProvider/index.tsx
@@ -1,25 +1,24 @@
 import { MsalProvider } from '@azure/msal-react';
 import {
   AuthenticationResult,
+  Configuration,
   EventMessage,
   EventType,
   PublicClientApplication,
 } from '@azure/msal-browser';
 
-const msalConfig = {
+const msalConfig: Configuration = {
   auth: {
     clientId: import.meta.env.VITE_AZURE_AD_CLIENT as string,
     authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_AD_TENANT}`,
     postLogoutRedirectUri: window.location.origin,
     redirectUri: window.location.origin,
-    validateAuthority: true,
     // After being redirected to the "redirectUri" page, should user
     // be redirected back to the Url where their login originated from?
     navigateToLoginRequestUrl: true,
   },
   cache: {
     cacheLocation: 'localStorage', // This configures where your cache will be stored
-    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
   },
 };
 
