20ffd791c14c02f7b8282e98fd0799bf1fb54e55
Eric <Eric.Mo2@gordon.edu>
2023-11-06 16:35:47 -0500

    Added add participant and Information Box

---
+++ src/views/HousingLottery/components/StudentApplication/components/InstructionsCard/index.jsx
@@ -0,0 +1,135 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from '@mui/material/';
+import { useEffect, useState } from 'react';
+import housing from 'services/housing';
+// @TODO CSSMODULES - outside directory
+import styles from '../../../../HousingLottery.module.css';
+
+/**
+ * Renders a card displaying the apartment application instructions
+ *
+ * @returns {JSX.Element} JSX Element for the instructions card
+ */
+const InstructionsCard = () => {
+  const [apartmentSelectionDate, setApartmentSelectionDate] = useState();
+  const [thisYear, setThisYear] = useState();
+
+  useEffect(() => {
+    const loadSelectionDate = async () =>
+      setApartmentSelectionDate(await housing.getApartmentSelectionDate());
+
+    loadSelectionDate();
+
+    setThisYear(new Date().getFullYear());
+  }, []);
+
+  const rows = [
+    { description: 'Current Freshman', points: 1 },
+    { description: 'Current Sophomore', points: 2 },
+    { description: 'Current Junior', points: 3 },
+    { description: 'Current Senior', points: 4 },
+    { description: '23+ years old', points: 1 },
+    { description: 'Full-time, off-campus program credit', points: 1 },
+    { description: 'Academic/Chapel probation', points: -1 },
+    { description: 'Possible academic suspension', points: -2 },
+    { description: `${thisYear - 1}-${thisYear} Disciplinary Probation`, points: -3 },
+  ];
+
+  return (
+    <Card>
+      <CardHeader
+        title="On-Campus Apartments"
+        subheader="Information and Guidelines"
+        className={styles.apartment_card_header}
+      />
+      <CardContent className={styles.apartment_instructions}>
+        <Typography variant="body1" paragraph>
+          Apartments provide an alternative to the traditional residence hall setting and offer a
+          unique community experience. To be eligible to live in an apartment, students must be at
+          least 20 years old as of Sept. 1, {thisYear} <strong>or</strong> have junior or senior
+          academic standing. Students who were on disciplinary probation at any time during the{' '}
+          {thisYear - 1}-{thisYear} academic year must also receive approval from the Dean of
+          Student Life or the Assistant Dean of Student Life to be eligible to apply for an
+          apartment. Each applicant must be registered as a full-time student by{' '}
+          {apartmentSelectionDate}.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Each group of students desiring to live in a Tavilla or Bromley apartment or in The
+          Village must submit an application. Your application can include a student who is studying
+          abroad or not enrolled for the Spring {thisYear} semester &ndash; simply list their name
+          on the application.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          <strong>Full-time, off-campus program credit:</strong> Students fulfilling academic
+          program requirements through student teaching or a full-time internship will qualify for
+          the full-time, off-campus program credit. It is the responsibility of applicants to claim
+          this credit on the application.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          <strong>Applications must be for a full apartment:</strong> If applying for a six-person
+          apartment, there must be six people on the application who will be here for the{' '}
+          <strong className={styles.over_emphasized}>fall semester</strong> (four people on a
+          four-person application, etc.). Applications with an incorrect number of applicants will
+          not be considered.
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          <strong>An application is not a guarantee!</strong>
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Due to the large number of applications typically received for apartments, not all
+          applications will be awarded an apartment. If you do not receive an apartment, you will
+          need to secure housing through the housing lottery.
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          <strong>How are apartments awarded?</strong>
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Apartments are awarded in order of point total for each type of apartment (4-person,
+          6-person, etc.). Each individual on an application will have points given/taken away using
+          the following scale:
+        </Typography>
+        <Grid container justifyContent="center" spacing={3}>
+          <Grid item xs={11} lg={9}>
+            <TableContainer component={Paper}>
+              <Table size="small">
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.description}>
+                      <TableCell>{row.description}</TableCell>
+                      <TableCell>{row.points}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+        <br />
+        <Typography variant="subtitle1" gutterBottom>
+          <strong>If You Are Approved...</strong>
+        </Typography>
+        <Typography variant="body1" paragraph>
+          You will be notified of your placement in an apartment/Village{' '}
+          <strong>
+            <em>building</em>
+          </strong>{' '}
+          no later than Apr. 13. Further information about specific apartment/room selection will be
+          communicated in that email.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default InstructionsCard;
--- src/views/HousingLottery/index.jsx
+++ src/views/HousingLottery/index.jsx
@@ -2,10 +2,14 @@ import { Button, Card, CardContent, CardHeader, Grid, TextField } from '@mui/mat
 import { useState } from 'react';
 import housingService from 'services/housing';
 import styles from './HousingLottery.module.css';
+import ApplicantList from './components/StudentApplication/components/ApplicantList/index.jsx';
+import InstructionsCard from './components/StudentApplication/components/InstructionsCard/index.jsx';
+
 
 const HousingLottery = () => {
   const [rank, setRank] = useState('');
   const [hall, setHall] = useState('');
+
 
   const handleRank = (event) => {
     setRank(event.target.value);
@@ -20,14 +24,15 @@ const HousingLottery = () => {
   };
 
   const searchHallTitle = <div align="left">Preferred Halls</div>;
+  // const searchStudentTitle = <div align="left">Student Applicants</div>;
 
   return (
     // <div>
     //   <Input onChange={handleChange} />
     //   <button onClick={handleClick}>Submit</button>
     // </div>
-    <Grid container justifyContent="center">
-      <Grid item xs={12} lg={6}>
+    <Grid container spacing={2} justifyContent="center">
+      <Grid item xs={12} lg={5}>
         <Card>
           <CardHeader title={searchHallTitle} className="gc360_header" />
           <CardContent height="500">
@@ -41,7 +46,7 @@ const HousingLottery = () => {
                   fullWidth
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={8}>
                 <TextField
                   id="standard-basic"
                   variant="standard"
@@ -50,12 +55,24 @@ const HousingLottery = () => {
                   fullWidth
                 />
               </Grid>
-              <Button variant="contained" className={styles.submit_button} onClick={handleClick}>
-                Submit
-              </Button>
+              <Grid>
+                <Button 
+                  variant="contained"
+                  className={styles.submit_button}
+                  onClick={handleClick}
+                  >
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
           </CardContent>
         </Card>
+      </Grid>
+      <Grid item xs={12} lg={5}>
+        <ApplicantList/>
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <InstructionsCard/>
       </Grid>
     </Grid>
   );
