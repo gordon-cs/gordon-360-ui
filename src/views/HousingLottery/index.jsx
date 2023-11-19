a755e7adc3ba889121258cbbcb8a92635f39d3d9
Zhijie Wang <zwrase@gmail.com>
2023-11-18 19:58:00 -0500

    create survey box for sleepying type

--- src/views/HousingLottery/index.jsx
+++ src/views/HousingLottery/index.jsx
@@ -1,21 +1,60 @@
-import { Input } from '@mui/material';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import {
+  Input,
+  Button,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+} from '@mui/material';
 import housingService from 'services/housing';
 
 const HousingLottery = () => {
   const [message, setMessage] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
 
   const handleChange = (event) => {
     setMessage(event.target.value);
   };
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   const handleClick = async () => {
-    await housingService.addRoommate(message);
+    // You can access both the message and selectedOption to submit to your housing service
+    await housingService.addRoommate({ message, selectedOption });
   };
 
   return (
     <div>
-      <Input onChange={handleChange} />
-      <button onClick={handleClick}>Submit</button>
+      <div>
+        <label>
+          Are you a night owl or a morning bird?
+        </label>
+        <RadioGroup
+          aria-label="morning-or-night"
+          name="morning-or-night"
+          value={selectedOption}
+          onChange={handleOptionChange}
+        >
+          <FormControlLabel
+            value="night-owl"
+            control={<Radio />}
+            label="Night Owl"
+          />
+          <FormControlLabel
+            value="morning-bird"
+            control={<Radio />}
+            label="Morning Bird"
+          />
+        </RadioGroup>
+      </div>
+
+      <Input onChange={handleChange} placeholder="Enter your message" />
+
+      <Button variant="contained" onClick={handleClick}>
+        Submit
+      </Button>
     </div>
   );
 };
