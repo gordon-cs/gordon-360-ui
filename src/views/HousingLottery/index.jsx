4d0de9643e04172dd41f436bbbd58801015459f4
“Jay <jinuk.Bae@gordon.edu>
2023-11-19 22:33:43 -0500

    Put survey questions into the box- by Jay Bae

--- src/views/HousingLottery/index.jsx
+++ src/views/HousingLottery/index.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
-import { Input, Button, Radio, RadioGroup, FormControlLabel } from '@mui/material';
+import {
+  Input,
+  Button,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  Card,
+  CardHeader,
+  CardContent,
+  Grid,
+} from '@mui/material';
 import housingService from 'services/housing';
+import styles from './HousingLottery.module.css';
 
 const HousingLottery = () => {
   const [message, setMessage] = useState('');
@@ -25,39 +36,47 @@ const HousingLottery = () => {
   };
 
   return (
-    <div>
-      <div>
-        <label>Are you a night owl or a morning bird?</label>
-        <RadioGroup
-          aria-label="morning-or-night"
-          name="morning-or-night"
-          value={morningOrNight}
-          onChange={handleMorningOrNightChange}
-        >
-          <FormControlLabel value="night-owl" control={<Radio />} label="Night Owl" />
-          <FormControlLabel value="morning-bird" control={<Radio />} label="Morning Bird" />
-        </RadioGroup>
-      </div>
+    <Grid container justifyContent="center">
+      <Grid item xs={12} lg={6} style={{ maxWidth: '500px' }}>
+        <Card>
+          <CardHeader title="Preferences" className={styles.apartment_card_header} />
+          <CardContent>
+            <div>
+              <label>Are you a night owl or a morning bird?</label>
+              <RadioGroup
+                aria-label="morning-or-night"
+                name="morning-or-night"
+                value={morningOrNight}
+                onChange={handleMorningOrNightChange}
+              >
+                <FormControlLabel value="night-owl" control={<Radio />} label="Night Owl" />
+                <FormControlLabel value="morning-bird" control={<Radio />} label="Morning Bird" />
+              </RadioGroup>
+            </div>
 
-      <div>
-        <label>Do you consider yourself quiet or loud in the dorm?</label>
-        <RadioGroup
-          aria-label="loud-or-quiet"
-          name="loud-or-quiet"
-          value={loudOrQuiet}
-          onChange={handleLoudOrQuietChange}
-        >
-          <FormControlLabel value="quiet" control={<Radio />} label="Quiet" />
-          <FormControlLabel value="loud" control={<Radio />} label="Loud" />
-        </RadioGroup>
-      </div>
-
-      <Input onChange={handleChange} placeholder="Enter your message" />
-
-      <Button variant="contained" onClick={handleClick}>
-        Submit
-      </Button>
-    </div>
+            <div>
+              <label>Do you consider yourself quiet or loud in the dorm?</label>
+              <RadioGroup
+                aria-label="loud-or-quiet"
+                name="loud-or-quiet"
+                value={loudOrQuiet}
+                onChange={handleLoudOrQuietChange}
+              >
+                <FormControlLabel value="quiet" control={<Radio />} label="Quiet" />
+                <FormControlLabel value="loud" control={<Radio />} label="Loud" />
+              </RadioGroup>
+            </div>
+          </CardContent>
+          <Grid container justifyContent="flex-end" style={{ padding: '0 16px 16px 0' }}>
+            <Grid item>
+              <Button variant="contained" onClick={handleClick}>
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
