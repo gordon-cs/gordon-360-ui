798b607c82175d962203e184e7a46374bb20b0a6
Eric <Eric.Mo2@gordon.edu>
2023-11-28 01:44:38 -0500

    Added applicantFields as number of max roommate

---
+++ src/views/HousingLottery/studentView/StudentApplicants/ApplicantFields/index.jsx
@@ -0,0 +1,53 @@
+import { TextField } from '@mui/material';
+import styles from '../../../HousingLottery.module.css';
+
+// Applicant input fields component
+const ApplicantFields = ({ applicant, onApplicantChange, index }) => {
+  const handleFieldChange = (field, value) => {
+    onApplicantChange(index, { ...applicant, [field]: value });
+  };
+
+  return (
+    <>
+      <TextField
+        type="text"
+        variant='outlined'
+        color='secondary'
+        label="First Name"
+        onChange={(e) => handleFieldChange('firstName', e.target.value)}
+        value={applicant.firstName}
+        fullWidth
+        required
+        className={styles.applicant_name}
+        helperText={'*Required'}
+      />
+      <TextField
+        type="text"
+        variant='outlined'
+        color='secondary'
+        label="Last Name"
+        onChange={(e) => handleFieldChange('lastName', e.target.value)}
+        value={applicant.lastName}
+        fullWidth
+        required
+        className={styles.applicant_name}
+        helperText={'*Required'}
+      />
+      <TextField
+        type="email"
+        variant='outlined'
+        color='secondary'
+        label="Email"
+        onChange={(e) => handleFieldChange('email', e.target.value)}
+        value={applicant.email}
+        required
+        error={!applicant.email.endsWith('@gordon.edu')}
+        helperText={!applicant.email.endsWith('@gordon.edu') ? 'Not a Valid Gordon Email' : '*Required'}
+        className={styles.applicant_email}
+      />
+    </>
+  );
+};
+
+export default ApplicantFields;
+
--- src/views/HousingLottery/studentView/StudentApplicants/index.jsx
+++ src/views/HousingLottery/studentView/StudentApplicants/index.jsx
@@ -1,82 +1,62 @@
-import { Button, Card, CardContent, CardHeader, Container, Grid, List, Stack, TextField } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Divider, Grid, TextField } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-// eslint-disable-next-line no-unused-vars
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'; // eslint disabled because it doesn't recognise type imports that ARE used in JSDoc comments
-// @TODO CSSMODULES - outside directory
+import { useState } from 'react';
+import ApplicantFields from './ApplicantFields';
 import styles from '../../HousingLottery.module.css';
 
-/**
- * Renders a list of selection boxes to choosing preferred halls
- *
- * @param {Object} props The React component props
- * @param {boolean} props.disabled boolean to disable the interactive elements of this list item
- */
-const StudentApplicants = ({
-  disabled
-}) => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState(false);
 
-  const handleEmailChange = (e) => {
-    const emailValue = e.target.value;
-    setEmail(emailValue);
-    setEmailError(!emailValue.endsWith('@gordon.edu'));
+const StudentApplicants = () => {
+  const [applicants, setApplicants] = useState([{ firstName: '', lastName: '', email: '' }]);
+
+  const handleApplicantChange = (index, updatedApplicant) => {
+    const newApplicants = [...applicants];
+    newApplicants[index] = updatedApplicant;
+    setApplicants(newApplicants);
   };
-  
+
+  const addApplicant = () => {
+    setApplicants([...applicants, { firstName: '', lastName: '', email: '' }]);
+  };
 
   return (
     <Card>
       <CardHeader title="Student Applicants" className={styles.applicants_card_header} />
       <CardContent>
         <Grid container justifyContent="space-between" spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              type="text"
-              variant='outlined'
-              color='secondary'
-              label="First Name"
-              onChange={e => setFirstName(e.target.value)}
-              value={firstName}
-              fullWidth
-              required
-              className={styles.applicants_name}
-              helperText={'*Required'}
-            />
-            <TextField
-              type="text"
-              variant='outlined'
-              color='secondary'
-              label="Last Name"
-              onChange={e => setLastName(e.target.value)}
-              value={lastName}
-              fullWidth
-              required
-              className={styles.applicants_name}
-              helperText={'*Required'}
-            />
-            <TextField
-              type="email"
-              variant='outlined'
-              color='secondary'
-              label="Email"
-              onChange={handleEmailChange}
-              value={email}
-              required
-              error={emailError}
-              helperText={emailError ? 'Not a Valid Gordon Email' : '*Required' }
-              className={styles.applicants_email}
-            />
-          </Grid>
+          {applicants.map((applicant, index) => (
+            <Grid item xs={12} key={index}>
+              <div className={styles.applicant_field} >
+                <ApplicantFields
+                  applicant={applicant}
+                  onApplicantChange={handleApplicantChange}
+                  index={index}
+                />
+              </div>
+              <Divider/>
+              <div className={styles.applicant_field} >
+                <ApplicantFields
+                  applicant={applicant}
+                  onApplicantChange={handleApplicantChange}
+                  index={index}
+                />
+              </div>
+              <Divider/>
+              <div className={styles.applicant_field} >
+                <ApplicantFields
+                  applicant={applicant}
+                  onApplicantChange={handleApplicantChange}
+                  index={index}
+                />
+              </div>
+            </Grid>
+          ))}
           <Grid item xs={12}>
             <Button
               style={{ margin: 'auto', display: 'flex' }}
-              // disabled={disabled || apartmentChoices?.length >= 5}
               variant="outlined"
               color="primary"
               startIcon={<AddIcon fontSize="inherit" />}
-              // onClick={onHallAdd}
+              onClick={addApplicant}
             >
               Add Applicant
             </Button>
