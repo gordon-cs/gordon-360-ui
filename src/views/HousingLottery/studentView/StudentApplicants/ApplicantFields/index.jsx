b96b151f219b6742c732a38206de04657622b164
“Jay <jinuk.Bae@gordon.edu>
2024-04-07 23:37:40 -0400

    Remove first and last name boxes in Student Applicants section

--- src/views/HousingLottery/studentView/StudentApplicants/ApplicantFields/index.jsx
+++ src/views/HousingLottery/studentView/StudentApplicants/ApplicantFields/index.jsx
@@ -9,39 +9,19 @@ const ApplicantFields = ({ applicant, onApplicantChange, index }) => {
   return (
     <>
       <TextField
-        type="text"
-        variant='outlined'
-        color='secondary'
-        label="First Name"
-        onChange={(e) => handleFieldChange('firstName', e.target.value)}
-        value={applicant.firstName}
-        fullWidth
-        required
-        className={styles.applicant_name}
-        helperText={'*Required'}
-      />
-      <TextField
-        type="text"
-        variant='outlined'
-        color='secondary'
-        label="Last Name"
-        onChange={(e) => handleFieldChange('lastName', e.target.value)}
-        value={applicant.lastName}
-        fullWidth
-        required
-        className={styles.applicant_name}
-        helperText={'*Required'}
-      />
-      <TextField
         type="email"
-        variant='outlined'
-        color='secondary'
+        variant="outlined"
+        color="secondary"
         label="Email"
         onChange={(e) => handleFieldChange('email', e.target.value)}
         value={applicant.email}
         required
         error={applicant.email !== '' && !applicant.email.endsWith('@gordon.edu')}
-        helperText={applicant.email === '' || applicant.email.endsWith('@gordon.edu') ? '*Required' : 'Not a Valid Gordon Email'}
+        helperText={
+          applicant.email === '' || applicant.email.endsWith('@gordon.edu')
+            ? '*Required'
+            : 'Not a Valid Gordon Email'
+        }
         className={styles.applicant_email}
       />
     </>
@@ -49,4 +29,3 @@ const ApplicantFields = ({ applicant, onApplicantChange, index }) => {
 };
 
 export default ApplicantFields;
-
