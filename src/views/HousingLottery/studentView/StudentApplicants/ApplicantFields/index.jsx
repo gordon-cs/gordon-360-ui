fe0064d1df342a5edc5f8c2052bfeb0d67be793b
“Jay <jinuk.Bae@gordon.edu>
2024-04-07 21:40:02 -0400

    Remove '*Required' label and disable input for the first applicant field

--- src/views/HousingLottery/studentView/StudentApplicants/ApplicantFields/index.jsx
+++ src/views/HousingLottery/studentView/StudentApplicants/ApplicantFields/index.jsx
@@ -6,47 +6,54 @@ const ApplicantFields = ({ applicant, onApplicantChange, index }) => {
     onApplicantChange(index, { ...applicant, [field]: value });
   };
 
+  const disabled = index === 0;
+  const disabledClass = disabled ? styles.applicant_disabled : '';
+
   return (
     <>
       <TextField
         type="text"
-        variant='outlined'
-        color='secondary'
+        variant="outlined"
+        color="secondary"
         label="First Name"
         onChange={(e) => handleFieldChange('firstName', e.target.value)}
         value={applicant.firstName}
         fullWidth
+        disabled={disabled}
         required
-        className={styles.applicant_name}
-        helperText={'*Required'}
+        className={`${styles.applicant_name} ${disabledClass}`}
       />
       <TextField
         type="text"
-        variant='outlined'
-        color='secondary'
+        variant="outlined"
+        color="secondary"
         label="Last Name"
         onChange={(e) => handleFieldChange('lastName', e.target.value)}
         value={applicant.lastName}
         fullWidth
+        disabled={disabled}
         required
-        className={styles.applicant_name}
-        helperText={'*Required'}
+        className={`${styles.applicant_name} ${disabledClass}`}
       />
       <TextField
         type="email"
-        variant='outlined'
-        color='secondary'
+        variant="outlined"
+        color="secondary"
         label="Email"
         onChange={(e) => handleFieldChange('email', e.target.value)}
         value={applicant.email}
+        disabled={disabled}
         required
         error={applicant.email !== '' && !applicant.email.endsWith('@gordon.edu')}
-        helperText={applicant.email === '' || applicant.email.endsWith('@gordon.edu') ? '*Required' : 'Not a Valid Gordon Email'}
-        className={styles.applicant_email}
+        helperText={
+          applicant.email === '' || applicant.email.endsWith('@gordon.edu')
+            ? ''
+            : 'Not a Valid Gordon Email'
+        }
+        className={`${styles.applicant_email} ${disabledClass}`}
       />
     </>
   );
 };
 
 export default ApplicantFields;
-
