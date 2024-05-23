7b7e3210ac96317df943f9ca5f42b791570cc803
jinuk Bae <jaybae9903@gmail.com>
2024-05-23 17:25:18 -0400

    Added pagination text code

--- src/components/EventList/index.jsx
+++ src/components/EventList/index.jsx
@@ -6,6 +6,8 @@ import useWindowSize from 'hooks/useWindowSize';
 import { List, Grid, Typography, Card, CardHeader } from '@mui/material';
 import styles from './EventList.module.css';
 
+import { useState } from 'react';
+
 const smallHeader = (
   <Typography variant="h5" className={styles.header_text}>
     Events
@@ -60,6 +62,30 @@ const EventList = ({ events }) => {
 
   const header = width < breakpointWidth ? smallHeader : fullHeader;
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 5;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const records = events.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(events.length / recordsPerPage);
+  const numbers = [...Array(npage + 1).keys()].slice(1);
+
+  function prePage() {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  function changeCPage(id) {
+    setCurrentPage(id);
+  }
+
+  function nextPage() {
+    if (currentPage !== npage) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
   return (
     <Card>
       <CardHeader title={header} className={styles.header} />
@@ -67,8 +93,29 @@ const EventList = ({ events }) => {
         <List className="gc360_event_list" disablePadding>
           {events?.length < 1
             ? noEvents
-            : events.map((event) => <EventItem event={event} key={event.Event_ID} />)}
+            : records.map((event) => <EventItem event={event} key={event.Event_ID} />)}
         </List>
+      </Grid>
+      <Grid>
+        <ul className="pagination">
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={prePage}>
+              Prev
+            </a>
+          </li>
+          {numbers.map((n, i) => (
+            <li className={"page-item ${currentPage === n ? 'active' : ''}"} key={i}>
+              <a href="#" className="page-item" onClick={changeCPage(n)}>
+                {n}
+              </a>
+            </li>
+          ))}
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={nextPage}>
+              Next
+            </a>
+          </li>
+        </ul>
       </Grid>
     </Card>
   );
