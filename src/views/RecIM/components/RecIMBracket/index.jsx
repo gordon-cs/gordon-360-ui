0d09337af886d8c5196b77915b301ed135ebb3b8
amos-cha <amos.cha98@gmail.com>
2024-07-03 15:30:21 -0400

    Add rudamentary bracket component

--- src/views/RecIM/components/RecIMBracket/index.jsx
+++ src/views/RecIM/components/RecIMBracket/index.jsx
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getBracketInfo } from 'services/recim/series';
 import { useWindowSize } from 'hooks';
 
@@ -15,16 +15,19 @@ import {
   SVGViewer,
   createTheme,
 } from '@g-loot/react-tournament-brackets';
+import GordonLoader from 'components/Loader';
 //import './styles.css';
 
-export const SingleElimination = () => {
+export const SingleElimination = ({ data }) => {
   const [width, height] = useWindowSize();
   const finalWidth = Math.max(width - 50, 500);
   const finalHeight = Math.max(height - 100, 500);
+
+  if (data.length === 0) return null;
   return (
     <SingleEliminationBracket
-      theme={GlootTheme}
-      matches={simpleSmallBracket}
+      theme={Theme}
+      matches={data}
       matchComponent={Match}
       svgWrapper={({ children, ...props }) => (
         <SVGViewer
@@ -43,7 +46,7 @@ export const SingleElimination = () => {
   );
 };
 
-const GlootTheme = createTheme({
+const Theme = createTheme({
   textColor: { main: '#000000', highlighted: '#F4F2FE', dark: '#707582' },
   matchBackground: { wonColor: '#2D2D59', lostColor: '#1B1D2D' },
   score: {
@@ -64,127 +67,26 @@ const GlootTheme = createTheme({
   overflow: 'visible',
 });
 
-export const simpleSmallBracket = [
-  {
-    id: 19753,
-    nextMatchId: null,
-    tournamentRoundText: '3',
-    startTime: '2021-05-30',
-    state: 'SCHEDULED',
-    participants: [],
-  },
-  {
-    id: 19754,
-    nextMatchId: 19753,
-    tournamentRoundText: '2',
-    startTime: '2021-05-30',
-    state: 'SCHEDULED',
-    participants: [
-      {
-        id: '14754a1a-932c-4992-8dec-f7f94a339960',
-        resultText: null,
-        isWinner: false,
-        status: null,
-        name: 'CoKe BoYz',
-        picture: 'teamlogos/client_team_default_logo',
-      },
-    ],
-  },
-  {
-    id: 19755,
-    nextMatchId: 19754,
-    tournamentRoundText: '1',
-    startTime: '2021-05-30',
-    state: 'SCORE_DONE',
-    participants: [
-      {
-        id: '14754a1a-932c-4992-8dec-f7f94a339960',
-        resultText: 'Won',
-        isWinner: true,
-        status: 'PLAYED',
-        name: 'CoKe BoYz',
-        picture: 'teamlogos/client_team_default_logo',
-      },
-      {
-        id: 'd16315d4-7f2d-427b-ae75-63a1ae82c0a8',
-        resultText: 'Lost',
-        isWinner: false,
-        status: 'PLAYED',
-        name: 'Aids Team',
-        picture: 'teamlogos/client_team_default_logo',
-      },
-    ],
-  },
-  {
-    id: 19756,
-    nextMatchId: 19754,
-    tournamentRoundText: '1',
-    startTime: '2021-05-30',
-    state: 'RUNNING',
-    participants: [
-      {
-        id: 'd8b9f00a-0ffa-4527-8316-da701894768e',
-        resultText: null,
-        isWinner: false,
-        status: null,
-        name: 'Art of kill',
-        picture: 'teamlogos/client_team_default_logo',
-      },
-    ],
-  },
-  {
-    id: 19757,
-    nextMatchId: 19753,
-    tournamentRoundText: '2',
-    startTime: '2021-05-30',
-    state: 'SCHEDULED',
-    participants: [],
-  },
-  {
-    id: 19758,
-    nextMatchId: 19757,
-    tournamentRoundText: '1',
-    startTime: '2021-05-30',
-    state: 'SCHEDULED',
-    participants: [
-      {
-        id: '9397971f-4b2f-44eb-a094-722eb286c59b',
-        resultText: null,
-        isWinner: false,
-        status: null,
-        name: 'Crazy Pepes',
-        picture: 'teamlogos/client_team_default_logo',
-      },
-    ],
-  },
-  {
-    id: 19759,
-    nextMatchId: 19757,
-    tournamentRoundText: '1',
-    startTime: '2021-05-30',
-    state: 'SCHEDULED',
-    participants: [
-      {
-        id: '42fecd89-dc83-4821-80d3-718acb50a30c',
-        resultText: null,
-        isWinner: false,
-        status: null,
-        name: 'BLUEJAYS',
-        picture: 'teamlogos/client_team_default_logo',
-      },
-      {
-        id: 'df01fe2c-18db-4190-9f9e-aa63364128fe',
-        resultText: null,
-        isWinner: false,
-        status: null,
-        name: 'Bosphorus',
-        picture: 'teamlogos/r7zn4gr8eajivapvjyzd',
-      },
-    ],
-  },
-];
-
 const RecIMBracket = ({ series }) => {
-  return <SingleElimination />;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const load = async () => {
+      setLoading(true);
+      setData(await getBracketInfo(series.ID));
+      setLoading(false);
+    };
+    load();
+  }, []);
+  console.log(data);
+
+  if (loading) return <GordonLoader />;
+
+  return (
+    <>
+      {/* <SingleElimination data={simpleSmallBracket} /> */}
+      <SingleElimination data={data} />
+    </>
+  );
 };
 export default RecIMBracket;
