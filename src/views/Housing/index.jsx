1d98a046474b67ae5150daa1ebb241e37242c894
Jason Asonye <jasonasonye@gmail.com>
2024-10-12 23:28:09 -0400

    Created more components for ResidentView

--- src/views/Housing/index.jsx
+++ src/views/Housing/index.jsx
@@ -1,14 +1,52 @@
-import { Card, Grid } from '@mui/material';
+import { Box, Card, Grid } from '@mui/material';
+import { useAuthGroups } from 'hooks';
+import { AuthGroup } from 'services/auth';
+
 import RDView from './components/RDView';
-import { isFacStaff as checkIsFacStaff, isFacStaff } from 'services/user';
+
+// Components for Resident View
+import MyHall from './components/ResidentView/MyHall';
+import MyRA from './components/ResidentView/MyRA';
+import OnDuty from './components/ResidentView/OnDuty';
+import Resources from './components/ResidentView/Resources';
 
 const Housing = () => {
-  if (isFacStaff) {
+  const isFaculty = useAuthGroups(AuthGroup.Faculty);
+  const isStudent = useAuthGroups(AuthGroup.Student);
+
+  if (isFaculty) {
     return (
       <Grid container>
         <Card>
           <RDView className="jsx" />
         </Card>
+      </Grid>
+    );
+  } else if (isStudent) {
+    return (
+      <Grid container>
+        <Box ml={5} /> {/* margin-left for all elements */}
+        <Grid item xs={12} md={4} padding={1}>
+          <Card>
+            <OnDuty className="jsx" />
+          </Card>
+          <Box mt={5} /> {/* margin-top between OnDuty and MyHall cards */}
+          <Card>
+            <MyHall className="jsx" />
+          </Card>
+        </Grid>
+        <Box mr={35} /> {/* margin-top of 5 between My Hall and MyRA cards */}
+        <Grid item xs={12} md={4} padding={1}>
+          <Card>
+            <MyRA className="jsx" />
+          </Card>
+        </Grid>
+        <Box mt={5} /> {/* margin-top between MyRA and Resources cards */}
+        <Grid item xs={12} md={4} padding={1}>
+          <Card>
+            <Resources className="jsx" />
+          </Card>
+        </Grid>
       </Grid>
     );
   } else {
