035e09a451dbdf9e5dc30475783505caa8cd7767
matthew.jones <matthew.jones@gordon.edu>
2024-10-14 20:13:32 -0400

    Added 404 page to Campus Safety Page routing

--- src/views/CampusSafety/index.tsx
+++ src/views/CampusSafety/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import SafetyPage from './views/SafetyPage';
+import Page404 from 'views/Page404';
 
 // Routing between Campus Safety App pages
 const CampusSafetyApp = () => {
   return (
     <Routes>
       <Route path="" element={<SafetyPage />}></Route>
+      <Route path="*" element={<Page404 />}></Route>
     </Routes>
   );
 };
