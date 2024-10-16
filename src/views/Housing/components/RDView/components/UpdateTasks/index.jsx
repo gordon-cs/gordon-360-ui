02f14a0dc42efb8af7d5f20e72ab4aa6c99a7b99
Ross Clark <78284383+RossClark01@users.noreply.github.com>
2024-10-16 11:24:21 -0400

    Added call to use user profile
    imported hook to use user so we can pull hall information from the signed in user. added the profile photo option on the MyHall component. This is temporarily populated with the user image

--- src/views/Housing/components/RDView/components/UpdateTasks/index.jsx
+++ src/views/Housing/components/RDView/components/UpdateTasks/index.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import React, { useState } from 'react'; // Import useState for managing open state
 import { IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import GordonDialogBox from 'components/GordonDialogBox';
 
 const UpdateTasks = () => {
   const [open, setOpen] = useState(false);
+
   return (
     <>
       <IconButton onClick={() => setOpen(true)} size="large" aria-label="Update RA/AC Tasks">
--- src/views/Housing/components/ResidentView/MyHall/index.jsx
+++ src/views/Housing/components/ResidentView/MyHall/index.jsx
@@ -1,6 +1,14 @@
-import { Card, CardContent, CardHeader, Grid, Link, Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Grid, Typography, Avatar } from '@mui/material';
+import { useUser } from 'hooks';
 
 const MyHall = () => {
+  const { profile, images } = useUser();
+  const profileImage = images?.pref || images?.def;
+
+  if (!profile) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Card>
       <CardHeader
@@ -14,15 +22,42 @@ const MyHall = () => {
         className="gc360_header"
       />
       <CardContent>
-        <strong>Hall:</strong>
-      </CardContent>
+        <Grid container>
+          <Grid item xs={8}>
+            <Typography variant="h6">
+              <strong>Hall:</strong> {profile.BuildingDescription ?? profile.Hall}
+            </Typography>
 
-      <CardContent>
-        <strong>Room #:</strong>
-      </CardContent>
+            <Typography variant="h6">
+              <strong>Room #:</strong> {profile.OnCampusRoom}
+            </Typography>
 
-      <CardContent>
-        <strong>RD:</strong>
+            <Typography variant="h6">
+              <strong>RD:</strong> {profile.fullName}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={4}
+            container
+            alignItems="center"
+            justifyContent="center"
+            direction="column"
+          >
+            {profileImage ? (
+              <Avatar
+                src={`data:image/jpg;base64,${profileImage}`}
+                alt={profile.fullName}
+                sx={{ width: 100, height: 100 }}
+              />
+            ) : (
+              <Avatar sx={{ width: 100, height: 100 }}>
+                {profile.FirstName?.[0]}
+                {profile.LastName?.[0]}
+              </Avatar>
+            )}
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
