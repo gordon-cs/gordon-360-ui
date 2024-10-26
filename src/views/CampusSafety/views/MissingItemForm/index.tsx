5b9fe46d8a44b91bead9b3e17f24e8f5ed62bbda
Ben Myers <benjamin.myers@gordon.edu>
2024-10-25 20:37:59 -0400

    added card with a few input boxes

--- src/views/CampusSafety/views/MissingItemForm/index.tsx
+++ src/views/CampusSafety/views/MissingItemForm/index.tsx
@@ -1,9 +1,31 @@
-import { Typography, Card, CardContent, CardHeader, Grid, Link, Box, Button } from '@mui/material';
+import { Card, CardContent, CardHeader, Grid, TextField } from '@mui/material';
 import Header from '../../components/Header';
 import styles from './MissingItemForm.module.scss';
 
 const MissingItemForm = () => {
-  return <Header></Header>;
+  return (
+    <Card className={styles.form_card}>
+      <CardHeader
+        title="Missing Item Report"
+        titleTypographyProps={{ align: 'center' }}
+        className="gc360_header"
+      />
+      <CardContent>
+        <Grid container justifyContent={'center'}>
+          <Grid item xs={12} sm={6}>
+            <Grid item margin={3}>
+              <TextField fullWidth variant="filled">
+                "Test"
+              </TextField>
+            </Grid>
+            <Grid item margin={3}>
+              <TextField fullWidth variant="filled"></TextField>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default MissingItemForm;
