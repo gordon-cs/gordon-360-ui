f7cbe538a599bf061c984f651af275931e9a1bbf
dli505 <100231490+dli505@users.noreply.github.com>
2024-11-07 02:52:35 -0500

    more work

--- src/views/Housing/components/RAView/index.jsx
+++ src/views/Housing/components/RAView/index.jsx
@@ -9,6 +9,7 @@ import {
   FormGroup,
   FormControl,
   FormControlLabel,
+  FormLabel,
   Grid,
   Typography,
 } from '@mui/material';
@@ -19,16 +20,43 @@ import { ListItemIcon, ListItemText, ListSubheader, List, ListItem, Link } from 
 import GordonDialogBox from 'components/GordonDialogBox';
 
 const RAView = () => {
-  const [isCheckedIn, updateCheckedIn] = useState(false);
+  const [isCheckedIn, setCheckedIn] = useState(false);
   const [open, setOpen] = useState(false);
-  const handleSubmit = () => {
-    updateCheckedIn(true);
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const hallName = 'testing';
+
+  const handleConfirm = (event) => {
+    setConfirmOpen(true);
+    for (var i = 0; i < hallState.length; i++) {
+      console.log(hallState[i]);
+      if (hallState[i].value && hallName == '') {
+        hallName.concat(hallState[i]).capitalize().string();
+        console.log(hallName);
+      } else if (hallState[i] && hallName != '') {
+        hallName.concat(', ', hallState[i]).capitalize();
+        console.log(hallName);
+      }
+    }
+  };
+
+  const handleSubmit = (event) => {
+    setCheckedIn({ ...isCheckedIn, [event.target.name]: event.target.value });
+    // isCheckedIn will need to be updated by the API later...
+    setConfirmOpen(false);
     setOpen(false);
   };
 
   const [hallState, setHallState] = useState({
+    bromley: false,
+    chase: false,
+    evans: false,
+    ferrin: false,
+    fulton: false,
+    nyland: false,
+    tavilla: false,
+    wilson: false,
     village: false,
-    conrad: false,
   });
 
   const handleHallChecked = (event) => {
@@ -38,7 +66,7 @@ const RAView = () => {
     });
   };
 
-  const { village, conrad } = hallState;
+  const { bromley, chase, evans, ferrin, fulton, nyland, tavilla, wilson, village } = hallState;
 
   return (
     <Grid container>
@@ -64,7 +92,7 @@ const RAView = () => {
 
       <Grid item xs={12} md={4} padding={1}>
         <Button variant="contained" onClick={() => setOpen(true)} disabled={isCheckedIn}>
-          {isCheckedIn ? 'You Are Checked In To Your Shift' : 'Check-In To Your Shift'}
+          {isCheckedIn ? 'You Are Checked In To Your Shift' : 'Check In To Your Shift'}
         </Button>
         <Grid item xs={12} md={4} padding={1}>
           <GordonDialogBox
@@ -72,39 +100,96 @@ const RAView = () => {
             onClose={() => setOpen(false)}
             title={'Choose Which Hall to Check Into'}
             buttonName="Check In"
-            buttonClicked={handleSubmit}
+            buttonClicked={handleConfirm}
             cancelButtonName="CANCEL"
             cancelButtonClicked={() => setOpen(false)}
           >
             <Grid item>
-              <FormGroup>
-                <FormControlLabel control={<Checkbox />} label="Bromley" />
-                <FormControlLabel control={<Checkbox />} label="Chase" />
-                <FormControlLabel control={<Checkbox />} label="Evans" />
-                <FormControlLabel control={<Checkbox />} label="Ferrin" />
-                <FormControlLabel control={<Checkbox />} label="Fulton" />
-                <FormControlLabel control={<Checkbox />} label="Nyland" />
-                <FormControlLabel control={<Checkbox />} label="Tavilla" />
-                <FormControlLabel control={<Checkbox />} label="Wilson" />
-                <FormControlLabel
-                  checked={village}
-                  control={<Checkbox />}
-                  onChange={handleHallChecked}
-                  label="The Village"
-                  name="village"
-                />
-                <FormControlLabel
-                  checked={village ? village : conrad}
-                  control={<Checkbox />}
-                  onChange={handleHallChecked}
-                  label="Conrad"
-                  name="conrad"
-                />
-                <FormControlLabel checked={village} control={<Checkbox />} label="Grace" />
-                <FormControlLabel checked={village} control={<Checkbox />} label="Hilton" />
-                <FormControlLabel checked={village} control={<Checkbox />} label="Rider" />
-                <FormControlLabel checked={village} control={<Checkbox />} label="McInnis" />
-              </FormGroup>
+              <FormControl required={true}>
+                <FormLabel error>Select a Hall</FormLabel>
+                <FormGroup>
+                  <FormControlLabel
+                    checked={bromley}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Bromley"
+                    name="bromley"
+                  />
+                  <FormControlLabel
+                    checked={chase}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Chase"
+                    name="chase"
+                  />
+                  <FormControlLabel
+                    checked={evans}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Evans"
+                    name="evans"
+                  />
+                  <FormControlLabel
+                    checked={ferrin}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Ferrin"
+                    name="ferrin"
+                  />
+                  <FormControlLabel
+                    checked={fulton}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Fulton"
+                    name="fulton"
+                  />
+                  <FormControlLabel
+                    checked={nyland}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Nyland"
+                    name="nyland"
+                  />
+                  <FormControlLabel
+                    checked={tavilla}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Tavilla"
+                    name="tavilla"
+                  />
+                  <FormControlLabel
+                    checked={wilson}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="Wilson"
+                    name="wilson"
+                  />
+                  <FormControlLabel
+                    checked={village}
+                    control={<Checkbox />}
+                    onChange={handleHallChecked}
+                    label="The Village"
+                    name="village"
+                  />
+                </FormGroup>
+              </FormControl>
+            </Grid>
+          </GordonDialogBox>
+        </Grid>
+        <Grid item xs={12} md={4} padding={1}>
+          <GordonDialogBox
+            open={confirmOpen}
+            onClose={() => setConfirmOpen(false)}
+            title={'Confirmation'}
+            buttonName="Yes"
+            buttonClicked={handleSubmit}
+            cancelButtonName="No"
+            cancelButtonClicked={() => setConfirmOpen(false)}
+          >
+            <Grid item>
+              <Typography>
+                NOTE: You are checking into {hallName} to be on duty. Is this what you meant to do?
+              </Typography>
             </Grid>
           </GordonDialogBox>
         </Grid>
