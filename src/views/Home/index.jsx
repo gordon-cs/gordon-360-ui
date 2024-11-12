39fb78beb3432dae96ac4e9831ad30feaab5dda6
Luke Hart <luke.hart@gordon.edu>
2024-11-12 16:48:38 -0500

    Home Page Lost and Found Widget

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -7,6 +7,7 @@ import Carousel from './components/Carousel';
 import DiningBalance from './components/DiningBalance';
 import GuestWelcome from './components/GuestWelcome';
 import NewsCard from './components/NewsCard';
+import LostAndFoundCard from './components/LostAndFound';
 import DaysLeft from './components/DaysLeft';
 const Home = () => {
   const { profile, loading } = useUser();
@@ -28,6 +29,9 @@ const Home = () => {
           <DiningBalance />
         </Grid>
         <Grid item xs={12} md={5}>
+          <LostAndFoundCard />
+        </Grid>
+        <Grid item xs={12} md={10}>
           <NewsCard />
         </Grid>
       </Grid>
