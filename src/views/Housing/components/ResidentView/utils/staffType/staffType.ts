d6f3d4efe129f672f1df5c1d4968f87b00d7556c
Jason Asonye <jasonasonye@gmail.com>
2024-12-09 04:22:24 -0500

    cleaned up ResidentView UI to push to sp-branch

---
+++ src/views/Housing/components/ResidentView/utils/staffType/staffType.ts
@@ -0,0 +1,15 @@
+// Determine if RA or AC based on building
+export const staffType = {
+  BRO: 'AC',
+  CHA: 'RA',
+  EVN: 'RA',
+  FER: 'RA',
+  FUL: 'RA',
+  NYL: 'RA',
+  TAV: 'AC',
+  WIL: 'RA',
+  CON: 'AC',
+  GRA: 'AC',
+  MCI: 'AC',
+  RID: 'AC',
+};
