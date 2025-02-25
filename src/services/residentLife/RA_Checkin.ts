c0526cf098c4a3e92c4722a30c8d4aa9dae2f5b4
Mya Randolph <72843238+Mrand03@users.noreply.github.com>
2025-02-25 17:07:21 -0500

    Update RA_Checkin.ts

--- src/services/residentLife/RA_Checkin.ts
+++ src/services/residentLife/RA_Checkin.ts
@@ -1,18 +1,15 @@
 import http from '../http';
 
 const checkIfCheckedIn = async (raId: string): Promise<boolean> => {
-  //const response = await http.get<{ IsOnCall: boolean }>(`Housing/ras/${raId}/is-on-call/`);
-  const response = await http.get<{ IsOnCall: boolean }>(`Housing/ras/${'50223925'}/is-on-call/`);
+  const response = await http.get<{ IsOnCall: boolean }>(`Housing/ras/${raId}/is-on-call/`);
   return response.IsOnCall;
 };
 
 const submitCheckIn = async (raId: string, hallIds: string[]): Promise<void> => {
-  //await http.post(`Housing/ras/${raId}/checkin`, hallIds);
-  await http.post(`Housing/ras/${'50223925'}/checkin`, hallIds);
+  await http.post(`Housing/ras/${raId}/checkin`, hallIds);
 };
 
 const getRACurrentHalls = (userName: string): Promise<String[]> =>
-  //http.get(`Housing/halls/on-calls/${userName}/locations`);
-  http.get(`Housing/halls/on-calls/${'Daniel.Fagerland'}/locations`);
+  http.get(`Housing/halls/on-calls/${userName}/locations`);
 
 export { checkIfCheckedIn, submitCheckIn, getRACurrentHalls };
