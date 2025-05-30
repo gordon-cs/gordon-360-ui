ac118a00c5d4f5ea067f6976e93a574734b6e1da
Josh Miller <joshuamiller1@gmail.com>
2025-05-30 12:27:39 -0400

    Enlarges poster when clicked on

--- src/views/InvolvementProfile/components/ClubPosters/index.jsx
+++ src/views/InvolvementProfile/components/ClubPosters/index.jsx
@@ -13,6 +13,8 @@ import {
   Typography,
 } from '@mui/material';
 import DATA from 'views/Posters/dummy-posters/dummyposters.jsx';
+import React from 'react';
+import Dialog from '@mui/material/Dialog';
 
 const ClubPosters = ({ clubName, clubCode }) => {
   let pizzaSlice = [];
@@ -22,56 +24,96 @@ const ClubPosters = ({ clubName, clubCode }) => {
     }
   }
 
+  const [open, setOpen] = React.useState(false);
+  const [selectedPoster, setSelectedPoster] = React.useState(null);
+
+  const handleOpen = (poster) => {
+    setSelectedPoster(poster);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedPoster(null);
+  };
+
   return (
-    <Card elevation={0}>
-      <CardHeader
-        title={`Posters from ${clubName}`}
-        className="gc360_header"
-        style={{ marginLeft: 0, width: '125%' }}
-      />
-      <CardContent>
-        <Grid container direction="row" spacing={4} className={'test1'}>
-          {pizzaSlice?.length > 0 ? (
-            pizzaSlice.map((item) => (
-              <Grid item xs={6} sm={4} md={3} lg={3} key={item.key}>
-                <Card variant="outlined" elevation={0}>
-                  <CardActionArea component={Link} to="/Page404">
-                    <CardMedia
-                      loading="lazy"
-                      component="img"
-                      alt={item.alt}
-                      src={item.image}
-                      title={item.title}
-                      description={item.desc}
-                    />
-                    <CardContent>
-                      <Typography className={'Poster Title'} variant="h6" fontWeight="bold">
-                        {item.title}
-                      </Typography>
-                      <Typography
-                        className={'Poster Description'}
-                        variant="body2"
-                        fontStyle="italic"
-                        fontWeight="light"
-                      >
-                        {item.desc}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
+    <>
+      <Card elevation={0}>
+        <CardHeader
+          title={`Posters from ${clubName}`}
+          className="gc360_header"
+          style={{ marginLeft: 0, width: '125%' }}
+        />
+        <CardContent>
+          <Grid container direction="row" spacing={4} className={'test1'}>
+            {pizzaSlice?.length > 0 ? (
+              pizzaSlice.map((item) => (
+                <Grid item xs={6} sm={4} md={3} lg={3} key={item.key}>
+                  <Card variant="outlined" elevation={0}>
+                    <CardActionArea onClick={() => handleOpen(item)}>
+                      <CardMedia
+                        loading="lazy"
+                        component="img"
+                        alt={item.alt}
+                        src={item.image}
+                        title={item.title}
+                        description={item.desc}
+                      />
+                      <CardContent>
+                        <Typography className={'Poster Title'} variant="h6" fontWeight="bold">
+                          {item.title}
+                        </Typography>
+                        <Typography
+                          className={'Poster Description'}
+                          variant="body2"
+                          fontStyle="italic"
+                          fontWeight="light"
+                        >
+                          {item.desc}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              ))
+            ) : (
+              <Grid item>
+                <Typography variant="h5" align="center">
+                  {'No Posters to Show for this involvement'}
+                </Typography>
               </Grid>
-            ))
-          ) : (
-            <Grid item>
-              <Typography variant="h5" align="center">
-                {'No Posters to Show for this involvement'}
+            )}
+          </Grid>
+        </CardContent>
+      </Card>
+      {selectedPoster && (
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          maxWidth="md"
+          style={{ backgroundColor: '#ffffff00' }}
+        >
+          <Card>
+            <CardMedia
+              component="img"
+              alt={selectedPoster.alt}
+              src={selectedPoster.image}
+              title={selectedPoster.title}
+              style={{ maxHeight: '80vh', objectFit: 'contain' }}
+            />
+            <CardContent>
+              <Typography variant="h5" fontWeight="bold" textAlign={'center'}>
+                {selectedPoster.title}
               </Typography>
-            </Grid>
-          )}
-        </Grid>
-      </CardContent>
-    </Card>
-    //Gonna need to filter for strictly individual club events
+              <Typography variant="body1" fontStyle="italic" textAlign={'center'}>
+                {selectedPoster.desc}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Dialog>
+      )}
+    </>
   );
 };
 
