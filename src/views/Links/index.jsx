857cb6b0144c918f4902fe31a07626849f0a26a8
JasonWang432 <Jason.Wang2@gordon.edu>
2025-06-06 09:27:04 -0400

    made the icon for metz in the link tab transparent

--- src/views/Links/index.jsx
+++ src/views/Links/index.jsx
@@ -23,10 +23,9 @@ import PsychologyIcon from '@mui/icons-material/Psychology';
 import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
 import CallIcon from '@mui/icons-material/Call';
 import ReportIcon from '@mui/icons-material/Report';
-import metzIcon from '/src/views/Links/images//metz.jpeg';
+import metzIcon from '/src/views/Links/images//metz.png';
 import styles from '/src/views/Links/Links.module.scss';
 import { useUser } from 'hooks';
-import { Tooltip } from '@mui/material';
 
 const Links = () => {
   const { profile } = useUser();
@@ -168,22 +167,20 @@ const Links = () => {
                 <ListItemText primary="iAttended" />
               </Link>
             </ListItem>
-            <Tooltip title="ඞ">
-              <ListItem>
-                <ListItemIcon>
-                  <img src={metzIcon} alt={'MetzIcon Logo'} className={styles.icon_style} />
-                </ListItemIcon>
-                <Link
-                  href="https://www.gordonmetz.com/"
-                  underline="hover"
-                  className={`gc360_text_link`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItemText primary="Metz" />
-                </Link>
-              </ListItem>
-            </Tooltip>
+            <ListItem>
+              <ListItemIcon>
+                <img src={metzIcon} alt={'MetzIcon Logo'} className={styles.icon_style} />
+              </ListItemIcon>
+              <Link
+                href="https://www.gordonmetz.com/"
+                underline="hover"
+                className={`gc360_text_link`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ListItemText primary="Metz" />
+              </Link>
+            </ListItem>
           </List>
         </Typography>
       </CardContent>
