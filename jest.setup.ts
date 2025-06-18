b7af54d1fbca93b2c5aaab92a24d86230f21a202
Woobensky Pierre <woobensky.pierre@zechariah.gordon.edu>
2025-06-18 14:57:35 -0400

    add tests for dinning balance

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+jest.mock('services/storage', () => ({
+  get: jest.fn((key) => {
+    if (key === 'network-status') return 'online';
+    return null;
+  }),
+  set: jest.fn(),
+}));
+
