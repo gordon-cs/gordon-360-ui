ca18a18c5f9977169389561c3c1935e7413a3202
JasonWang432 <Jason.Wang2@gordon.edu>
2025-06-25 10:49:03 -0400

    not needed

--- src/views/Links/index.jsx
+++ src/views/Links/index.jsx
@@ -23,8 +23,6 @@ import PsychologyIcon from '@mui/icons-material/Psychology';
 import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
 import CallIcon from '@mui/icons-material/Call';
 import ReportIcon from '@mui/icons-material/Report';
-import metzIcon from '/src/views/Links/images//metz.png';
-import CashnetIcon from '/src/views/Links/images//Cashnet.png';
 import styles from '/src/views/Links/Links.module.scss';
 import { useUser } from 'hooks';
 
@@ -166,34 +164,6 @@ const Links = () => {
                 target="_blank"
               >
                 <ListItemText primary="iAttended" />
-              </Link>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <img src={CashnetIcon} alt={'Cashnet Logo'} className={styles.icon_style} />
-              </ListItemIcon>
-              <Link
-                href="https://commerce.cashnet.com/cashnetg/static/epayment/gordonpay/login"
-                underline="hover"
-                className={`gc360_text_link`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <ListItemText primary="CashNet" />
-              </Link>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <img src={metzIcon} alt={'MetzIcon Logo'} className={styles.icon_style} />
-              </ListItemIcon>
-              <Link
-                href="https://www.gordonmetz.com/"
-                underline="hover"
-                className={`gc360_text_link`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <ListItemText primary="Metz" />
               </Link>
             </ListItem>
           </List>
