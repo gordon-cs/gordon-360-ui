908fba3a04532f91a7d8ffafc8b5f2832d3e84b1
Evan Platzer <evan.platzer@gmail.com>
2026-02-10 15:24:53 -0500

    ApartApp: Update instructions and agreements for 2026

--- src/views/ApartmentApp/components/StudentApplication/components/Agreements/index.jsx
+++ src/views/ApartmentApp/components/StudentApplication/components/Agreements/index.jsx
@@ -11,7 +11,8 @@ import {
   FormLabel,
 } from '@mui/material/';
 import { Fragment, useEffect, useState } from 'react';
-import housing from 'services/housing';
+import { ApplicationProcessDates } from 'services/housing';
+import { format } from 'date-fns';
 // @TODO CSSMODULES - outside directory
 import styles from '../../../../ApartmentApp.module.css';
 
@@ -28,7 +29,6 @@ const Agreements = ({ deleting, onChange }) => {
 
   const loadAgreements = async () => {
     const currentYear = new Date().getFullYear();
-    const selectionDate = await housing.getApartmentSelectionDate();
 
     const newCheckboxes = [
       {
@@ -42,7 +42,8 @@ const Agreements = ({ deleting, onChange }) => {
       },
       {
         checked: false,
-        label: 'Each individual on this application appears ONLY on this application',
+        label:
+          'Each individual on this application appears ONLY on this application and not on any other applications',
       },
       {
         checked: false,
@@ -51,18 +52,48 @@ const Agreements = ({ deleting, onChange }) => {
       },
       {
         checked: false,
-        label: `Any individual on this application who has been on disciplinary probation at any point during the ${
+        label: `Each individual on this application who has been on disciplinary probation at any point during the ${
           currentYear - 1
-        }-${currentYear} academic year has been approved to apply by the Dean of Student Life or Assistant Dean of Student Life`,
+        }-${currentYear} academic year has been approved by the Care and Conduct Coordinator to apply for an apartment`,
       },
       {
         checked: false,
-        label: `Each individual on this application intends to register as a full-time student by ${selectionDate}`,
+        label: `Each individual on this application intends to register for classes by ${format(ApplicationProcessDates.approvalNotificationBy, 'MMMM do, y')}`,
       },
       {
         checked: false,
-        label: `We understand that if any member of our application fails to register as a full-time student by ${selectionDate}, our application could be disqualified`,
+        label: `We understand that if any member of our application fails to register for classes by ${format(ApplicationProcessDates.approvalNotificationBy, 'MMMM do, y')}, our application could be disqualified`,
       },
+      {
+        checked: false,
+        label:
+          'Each individual on this application has submitted or is working with Student Financial Services to submit their housing deposit',
+      },
+      {
+        checked: false,
+        label:
+          "We understand that if any member of our application has not submitted their housing deposits by the date of this application's submission, our application could be disqualified",
+      },
+      {
+        checked: false,
+        label: (
+          <>
+            We have completed{' '}
+            <a
+              className="gc360_text_link"
+              href="https://gordoncollegestudent-ma.safecolleges.com/login"
+            >
+              our awareness education modules in Vector Solutions
+            </a>{' '}
+          </>
+        ),
+      },
+      {
+        checked: false,
+        label:
+          'We understand that we will not be eligible to apply for housing if our awareness education modules are incomplete',
+      },
+
       {
         checked: false,
         label:
@@ -72,6 +103,16 @@ const Agreements = ({ deleting, onChange }) => {
         checked: false,
         label:
           'We certify that all information provided on this application is accurate, to the best of our knowledge',
+      },
+      {
+        checked: false,
+        label:
+          'We confirm that we will have a full apartment in both the fall and spring semesters. We understand that if this changes at any point, our apartment/roommate status may be changed at the discretion of the Housing Office.',
+      },
+      {
+        checked: false,
+        label:
+          'We understand that if the number of residents in our apartment changes at any point, the Housing Office reserves the right to reassign our apartment to a different room prior to move-in (based on bed stock) and/or assign a new roommate as necessary throughout the year',
       },
       {
         checked: false,
--- src/views/ApartmentApp/components/StudentApplication/components/InstructionsCard/index.jsx
+++ src/views/ApartmentApp/components/StudentApplication/components/InstructionsCard/index.jsx
@@ -11,8 +11,8 @@ import {
   TableRow,
   Typography,
 } from '@mui/material/';
-import { useEffect, useState } from 'react';
-import housing from 'services/housing';
+import { ApplicationProcessDates } from 'services/housing';
+import { format } from 'date-fns';
 // @TODO CSSMODULES - outside directory
 import styles from '../../../../ApartmentApp.module.css';
 
@@ -22,17 +22,7 @@ import styles from '../../../../ApartmentApp.module.css';
  * @returns {JSX.Element} JSX Element for the instructions card
  */
 const InstructionsCard = () => {
-  const [apartmentSelectionDate, setApartmentSelectionDate] = useState();
-  const [thisYear, setThisYear] = useState();
-
-  useEffect(() => {
-    const loadSelectionDate = async () =>
-      setApartmentSelectionDate(await housing.getApartmentSelectionDate());
-
-    loadSelectionDate();
-
-    setThisYear(new Date().getFullYear());
-  }, []);
+  const thisYear = new Date().getFullYear();
 
   const rows = [
     { description: 'Current Freshman', points: 1 },
@@ -54,50 +44,49 @@ const InstructionsCard = () => {
         className={styles.apartment_card_header}
       />
       <CardContent className={styles.apartment_instructions}>
+        <Typography variant="h4">Review the following notices before applying</Typography>
         <Typography variant="body1" paragraph>
           Apartments provide an alternative to the traditional residence hall setting and offer a
           unique community experience. To be eligible to live in an apartment, students must be at
-          least 20 years old as of Sept. 1, {thisYear} <strong>or</strong> have junior or senior
-          academic standing. Students who were on disciplinary probation at any time during the{' '}
-          {thisYear - 1}-{thisYear} academic year must also receive approval from the Dean of
-          Student Life or the Assistant Dean of Student Life to be eligible to apply for an
-          apartment. Each applicant must be registered as a full-time student by{' '}
-          {apartmentSelectionDate}.
+          least 20 years old <strong>or</strong> have junior or senior academic standing as of Sept.
+          1, {thisYear}. Students who were on disciplinary probation at any time during the{' '}
+          {thisYear - 1}-{thisYear} academic year must also receive approval from the Care and
+          Conduct Coordinator, Porter Sprigg, to be eligible to apply for an apartment. Each
+          applicant must be registered as a full-time student by{' '}
+          {format(ApplicationProcessDates.registrationDeadline, 'MMMM do, y')}.
         </Typography>
         <Typography variant="body1" paragraph>
           Each group of students desiring to live in a Tavilla or Bromley apartment or in The
-          Village must submit an application. Your application can include a student who is studying
-          abroad or not enrolled for the Spring {thisYear} semester &ndash; simply list their name
-          on the application.
+          Village must submit an application. Students can apply in groups of 4 to live near one
+          another in The Village.
         </Typography>
         <Typography variant="body1" paragraph>
-          <strong>Full-time, off-campus program credit:</strong> Students fulfilling academic
-          program requirements through student teaching or a full-time internship will qualify for
-          the full-time, off-campus program credit. It is the responsibility of applicants to claim
-          this credit on the application.
+          Full-time and part-time students are allowed to apply for an apartment, but extra points
+          will be awarded to full-time students.
         </Typography>
         <Typography variant="body1" paragraph>
-          <strong>Applications must be for a full apartment:</strong> If applying for a six-person
-          apartment, there must be six people on the application who will be here for the{' '}
-          <strong className={styles.over_emphasized}>fall semester</strong> (four people on a
-          four-person application, etc.). Applications with an incorrect number of applicants will
-          not be considered.
+          Students fulfilling academic program requirements through student teaching or a full-time
+          internship will qualify for the full-time, off-campus program credit. It is the
+          responsibility of applicants to claim this credit on the application.
         </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          <strong>An application is not a guarantee!</strong>
+        <Typography variant="h5">Applications must be for a full apartment</Typography>
+        <Typography variant="body1" paragraph>
+          If applying for a six-person apartment, there must be six people on the application who
+          will be here for the <strong className={styles.over_emphasized}>fall semester</strong>{' '}
+          (four people on a four-person application, etc.). Applications with an incorrect number of
+          applicants will not be considered.
         </Typography>
+        <Typography variant="h5">An Application is Not a Guarantee!</Typography>
         <Typography variant="body1" paragraph>
           Due to the large number of applications typically received for apartments, not all
           applications will be awarded an apartment. If you do not receive an apartment, you will
           need to secure housing through the housing lottery.
         </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          <strong>How are apartments awarded?</strong>
-        </Typography>
+        <Typography variant="h5">How are apartments awarded?</Typography>
         <Typography variant="body1" paragraph>
-          Apartments are awarded in order of point total for each type of apartment (4-person,
-          6-person, etc.). Each individual on an application will have points given/taken away using
-          the following scale:
+          Apartments are awarded in priority order, based first on point total for each type of
+          apartment (4-person, 6-person, etc.) and then on application submission time. Each
+          individual on an application will have points given/taken away using the following scale:
         </Typography>
         <Grid container justifyContent="center" spacing={3}>
           <Grid item xs={11} lg={9}>
@@ -116,16 +105,19 @@ const InstructionsCard = () => {
           </Grid>
         </Grid>
         <br />
-        <Typography variant="subtitle1" gutterBottom>
-          <strong>If You Are Approved...</strong>
-        </Typography>
+        <Typography variant="h5">If You Are Approved</Typography>
         <Typography variant="body1" paragraph>
-          You will be notified of your placement in an apartment/Village{' '}
-          <strong>
-            <em>building</em>
-          </strong>{' '}
-          no later than Apr. 13. Further information about specific apartment/room selection will be
-          communicated in that email.
+          You will be notified of your approval for an apartment by{' '}
+          {format(ApplicationProcessDates.approvalNotificationBy, 'MMMM do, y')}. Once approved, you
+          will receive a “time slot” to attend Housing Selection Night and choose which specific
+          building/apartment you would like to live in! Further information about specific
+          apartment/room selection will be communicated in that email.
+        </Typography>
+        <Typography variant="h5">If You Are Not Approved</Typography>
+        <Typography variant="body1" paragraph>
+          You will need to enter the General Housing Selection process and let the Housing Office
+          know if you would prefer to live in a double/triple/quad and who you would like to be your
+          roommate(s). Further information about specifics will be communicated in that email.
         </Typography>
       </CardContent>
     </Card>
