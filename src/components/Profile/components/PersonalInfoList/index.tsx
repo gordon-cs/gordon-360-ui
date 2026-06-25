a5faa39c7eae4cff633d68dc77440c397ecddf8e
Jonathan Senning <jonathan.senning@gordon.edu>
2026-06-25 13:57:11 -0400

    Undo erroneous change (originally suggested by github?) made last summer

--- src/components/Profile/components/PersonalInfoList/index.tsx
+++ src/components/Profile/components/PersonalInfoList/index.tsx
@@ -197,7 +197,7 @@ const PersonalInfoList = ({ myProf, profile, isOnline, createSnackbar }: Props) 
         }
       }
     }
-  }, [isStudent, myProf, graduationInfo, setPlannedGradDate, profPlannedGradYear, createSnackbar]);
+  }, [isStudent, myProf, graduationInfo, profPlannedGradYear, createSnackbar]);
 
   const handleChangeCliftonStrengthsPrivacy = async () => {
     try {
